6cb409f9d02ebf34e2c65454609f99a9449bb141
steviez <steven@anza.xyz>
2024-03-06 00:50:49 -0600

    Name the threadpool used for new epoch processing

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -1483,7 +1483,10 @@ impl Bank {
         let epoch = self.epoch();
         let slot = self.slot();
         let (thread_pool, thread_pool_time) = measure!(
-            ThreadPoolBuilder::new().build().unwrap(),
+            ThreadPoolBuilder::new()
+                .thread_name(|i| format!("solBnkNewEpch{i:02}"))
+                .build()
+                .unwrap(),
             "thread_pool_creation",
         );
 
