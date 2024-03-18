b6bdbac5229d6ecb094fd8ee05720425e10f216a
steviez <steven@anza.xyz>
2024-03-18 12:40:52 -0500

    Move argument strings to modules

--- validator/src/cli/thread_args.rs
+++ validator/src/cli/thread_args.rs
@@ -32,22 +32,22 @@ pub struct NumThreadConfig {
 pub fn thread_args<'a>(defaults: &DefaultThreadArgs) -> Vec<Arg<'_, 'a>> {
     // Do not let any threadpool size scale over the number of threads
     vec![
-        Arg::with_name("replay_forks_threads")
-            .long("replay-forks-threads")
+        Arg::with_name(replay_forks_threads::NAME)
+            .long(replay_forks_threads::LONG_ARG)
             .takes_value(true)
             .value_name("NUMBER")
             .default_value(&defaults.replay_forks_threads)
             .validator(|num| is_within_range(num, 1..=MAX_CONCURRENT_FORKS_TO_REPLAY))
             .hidden(hidden_unless_forced())
-            .help("Number of threads to use for replay of blocks on different forks"),
-        Arg::with_name("replay_transactions_threads")
-            .long("replay-transactions-threads")
+            .help(replay_forks_threads::HELP),
+        Arg::with_name(replay_transactions_threads::NAME)
+            .long(replay_transactions_threads::LONG_ARG)
             .takes_value(true)
             .value_name("NUMBER")
             .default_value(&defaults.replay_transactions_threads)
             .validator(|num| is_within_range(num, 1..=get_max_thread_count()))
             .hidden(hidden_unless_forced())
-            .help("Number of threads to use for transaction replay"),
+            .help(replay_transactions_threads::HELP),
     ]
 }
 
@@ -56,12 +56,24 @@ pub fn parse_num_threads_args(matches: &ArgMatches) -> NumThreadConfig {
         replay_forks_threads: if matches.is_present("replay_slots_concurrently") {
             NonZeroUsize::new(4).expect("4 is non-zero")
         } else {
-            value_t_or_exit!(matches, "replay_forks_threads", NonZeroUsize)
+            value_t_or_exit!(matches, replay_forks_threads::NAME, NonZeroUsize)
         },
         replay_transactions_threads: value_t_or_exit!(
             matches,
-            "replay_transactions_threads",
+            replay_transactions_threads::NAME,
             NonZeroUsize
         ),
     }
+}
+
+mod replay_forks_threads {
+    pub const NAME: &str = "replay_forks_threads";
+    pub const LONG_ARG: &str = "replay-forks-threads";
+    pub const HELP: &str = "Number of threads to use for replay of blocks on different forks";
+}
+
+mod replay_transactions_threads {
+    pub const NAME: &str = "replay_transactions_threads";
+    pub const LONG_ARG: &str = "replay-transactions-threads";
+    pub const HELP: &str = "Number of threads to use for transaction replay";
 }
