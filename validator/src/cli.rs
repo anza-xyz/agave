49b17bc3d19b59a3f3fbc29fb79f6f603d96d757
brooks <brooks@anza.xyz>
2024-03-22 08:52:57 -0400

    fix help for --incremental-snapshots
    OLD:
    ```
            --incremental-snapshots
                Enable incremental snapshots by setting this flag.  When enabled, --snapshot-interval-slots will set the
                incremental snapshot interval. To set the
                                 full snapshot interval, use --full-snapshot-interval-slots.
    ```

--- validator/src/cli.rs
+++ validator/src/cli.rs
@@ -2042,8 +2042,8 @@ fn deprecated_arguments() -> Vec<DeprecatedArg> {
         .help("Enable incremental snapshots")
         .long_help(
             "Enable incremental snapshots by setting this flag.  When enabled, \
-             --snapshot-interval-slots will set the incremental snapshot interval. To set the
-                 full snapshot interval, use --full-snapshot-interval-slots.",
+             --snapshot-interval-slots will set the incremental snapshot interval. To set the \
+             full snapshot interval, use --full-snapshot-interval-slots.",
         ));
     add_arg!(Arg::with_name("minimal_rpc_api")
         .long("minimal-rpc-api")
