1a3245db79436fe6e8753bebe0b5cf3de3c4e3cf
yihau <yihau.chen@icloud.com>
2024-04-03 21:34:35 +0800

    remove InetAddr from streamer/src/sendmmsg.rs

--- streamer/src/sendmmsg.rs
+++ streamer/src/sendmmsg.rs
@@ -1,9 +1,6 @@
 //! The `sendmmsg` module provides sendmmsg() API implementation
 
 #[cfg(target_os = "linux")]
-#[allow(deprecated)]
-use nix::sys::socket::InetAddr;
-#[cfg(target_os = "linux")]
 use {
     itertools::izip,
     libc::{iovec, mmsghdr, sockaddr_in, sockaddr_in6, sockaddr_storage},
@@ -76,21 +73,41 @@ fn mmsghdr_for_packet(
     hdr.msg_hdr.msg_iovlen = 1;
     hdr.msg_hdr.msg_name = addr as *mut _ as *mut _;
 
-    #[allow(deprecated)]
-    match InetAddr::from_std(dest) {
-        InetAddr::V4(dest) => {
+    match *dest {
+        SocketAddr::V4(ref socket_addr_v4) => {
             unsafe {
-                std::ptr::write(addr as *mut _ as *mut _, dest);
+                std::ptr::write(
+                    addr as *mut _ as *mut _,
+                    libc::sockaddr_in {
+                        sin_family: libc::AF_INET as u16,
+                        sin_port: socket_addr_v4.port().to_be(),
+                        sin_addr: libc::in_addr {
+                            s_addr: u32::from_ne_bytes(socket_addr_v4.ip().octets()),
+                        },
+                        ..std::mem::zeroed()
+                    },
+                );
             }
             hdr.msg_hdr.msg_namelen = SIZE_OF_SOCKADDR_IN as u32;
         }
-        InetAddr::V6(dest) => {
+        SocketAddr::V6(ref socket_addr_v6) => {
             unsafe {
-                std::ptr::write(addr as *mut _ as *mut _, dest);
+                std::ptr::write(
+                    addr as *mut _ as *mut _,
+                    libc::sockaddr_in6 {
+                        sin6_family: libc::AF_INET6 as u16,
+                        sin6_port: socket_addr_v6.port().to_be(),
+                        sin6_addr: libc::in6_addr {
+                            s6_addr: socket_addr_v6.ip().octets(),
+                        },
+                        sin6_flowinfo: socket_addr_v6.flowinfo(),
+                        sin6_scope_id: socket_addr_v6.scope_id(),
+                    },
+                );
             }
             hdr.msg_hdr.msg_namelen = SIZE_OF_SOCKADDR_IN6 as u32;
         }
-    };
+    }
 }
 
 #[cfg(target_os = "linux")]
