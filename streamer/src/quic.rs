79df0b811c7ad3772294f45a58c710c02529f510
HaoranYi <haoran.yi@solana.com>
2024-04-05 22:30:34 +0000

    fix merge conflicts

--- streamer/src/quic.rs
+++ streamer/src/quic.rs
@@ -179,15 +179,10 @@ pub struct StreamStats {
     pub(crate) stream_load_ema: AtomicUsize,
     pub(crate) stream_load_ema_overflow: AtomicUsize,
     pub(crate) stream_load_capacity_overflow: AtomicUsize,
-<<<<<<< HEAD
-=======
-    pub(crate) process_sampled_packets_us_hist: Mutex<histogram::Histogram>,
-    pub(crate) perf_track_overhead_us: AtomicU64,
     pub(crate) total_staked_packets_sent_for_batching: AtomicUsize,
     pub(crate) total_unstaked_packets_sent_for_batching: AtomicUsize,
     pub(crate) throttled_staked_streams: AtomicUsize,
     pub(crate) throttled_unstaked_streams: AtomicUsize,
->>>>>>> b443cfb0c7 (Show staked vs nonstaked packets sent down/throttled (#600))
 }
 
 impl StreamStats {
@@ -450,8 +445,6 @@ impl StreamStats {
                 self.stream_load_capacity_overflow.load(Ordering::Relaxed),
                 i64
             ),
-<<<<<<< HEAD
-=======
             (
                 "throttled_unstaked_streams",
                 self.throttled_unstaked_streams.swap(0, Ordering::Relaxed),
@@ -462,39 +455,6 @@ impl StreamStats {
                 self.throttled_staked_streams.swap(0, Ordering::Relaxed),
                 i64
             ),
-            (
-                "process_sampled_packets_us_90pct",
-                process_sampled_packets_us_hist
-                    .percentile(90.0)
-                    .unwrap_or(0),
-                i64
-            ),
-            (
-                "process_sampled_packets_us_min",
-                process_sampled_packets_us_hist.minimum().unwrap_or(0),
-                i64
-            ),
-            (
-                "process_sampled_packets_us_max",
-                process_sampled_packets_us_hist.maximum().unwrap_or(0),
-                i64
-            ),
-            (
-                "process_sampled_packets_us_mean",
-                process_sampled_packets_us_hist.mean().unwrap_or(0),
-                i64
-            ),
-            (
-                "process_sampled_packets_count",
-                process_sampled_packets_us_hist.entries(),
-                i64
-            ),
-            (
-                "perf_track_overhead_us",
-                self.perf_track_overhead_us.swap(0, Ordering::Relaxed),
-                i64
-            ),
->>>>>>> b443cfb0c7 (Show staked vs nonstaked packets sent down/throttled (#600))
         );
     }
 }
