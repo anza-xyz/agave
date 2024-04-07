9a83ec51b9f4941ef73e2ea63d53e2d7626d9127
HaoranYi <haoran.yi@solana.com>
2024-04-07 02:53:34 +0000

    fix gossip spy port

--- gossip/src/main.rs
+++ gossip/src/main.rs
@@ -236,11 +236,16 @@ fn process_spy(matches: &ArgMatches, socket_addr_space: SocketAddrSpace) -> std:
     let gossip_addr = SocketAddr::new(
         gossip_host,
         value_t!(matches, "gossip_port", u16).unwrap_or_else(|_| {
-            solana_net_utils::find_available_port_in_range(
-                IpAddr::V4(Ipv4Addr::UNSPECIFIED),
-                (0, 1),
-            )
-            .expect("unable to find an available gossip port")
+            // find an available port.
+            let get_port = || -> Result<_, std::io::Error> {
+                let t = solana_net_utils::bind_with_any_port(bind_address)?;
+                let addr = t.local_addr()?;
+                Ok(addr.port())
+            };
+            get_port().unwrap_or_else(|err| {
+                eprintln!("Unable to find an available gossip port: {err}");
+                exit(1);
+            })
         }),
     );
     let discover_timeout = Duration::from_secs(timeout.unwrap_or(u64::MAX));
