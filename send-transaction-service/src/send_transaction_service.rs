solana-labs/solana

Added rpc-send-transaction-tpu-peer subcommand.

send-transaction-service/src/send_transaction_service.rs
@@ -115,7 +115,7 @@ pub struct Config {
     pub batch_send_rate_ms: u64,
     /// When the retry pool exceeds this max size, new transactions are dropped after their first broadcast attempt
     pub retry_pool_max_size: usize,
-    pub tpu_peers: Option<Vec<SocketAddr>>,
+    pub tpu_peers: Arc<RwLock<Vec<SocketAddr>>>,
 }
 
 impl Default for Config {
@@ -128,7 +128,7 @@ impl Default for Config {
             batch_size: DEFAULT_TRANSACTION_BATCH_SIZE,
             batch_send_rate_ms: DEFAULT_BATCH_SEND_RATE_MS,
             retry_pool_max_size: MAX_TRANSACTION_RETRY_POOL_SIZE,
-            tpu_peers: None,
+            tpu_peers: Default::default(),
         }
     }
 }
@@ -568,11 +568,18 @@ impl SendTransactionService {
         stats: &SendTransactionServiceStats,
     ) {
         // Processing the transactions in batch
-        let mut addresses = config
+        let addresses = config
             .tpu_peers
-            .as_ref()
-            .map(|addrs| addrs.iter().map(|a| (a, 0)).collect::<Vec<_>>())
-            .unwrap_or_default();
+            .read()
+            .unwrap()
+            .iter()
+            .cloned()
+            .map(|addr| (addr, 0))
+            .collect::<Vec<_>>();
+        let mut addresses = addresses
+            .iter()
+            .map(|(addr, slot)| (addr, *slot))
+            .collect::<Vec<(&SocketAddr, Slot)>>();
         let leader_addresses = Self::get_tpu_addresses_with_slots(
             tpu_address,
             leader_info,
@@ -710,11 +717,7 @@ impl SendTransactionService {
 
             let iter = wire_transactions.chunks(config.batch_size);
             for chunk in iter {
-                let mut addresses = config
-                    .tpu_peers
-                    .as_ref()
-                    .map(|addrs| addrs.iter().collect::<Vec<_>>())
-                    .unwrap_or_default();
+                let mut addresses = config.tpu_peers.read().unwrap().clone();
                 let mut leader_info_provider = leader_info_provider.lock().unwrap();
                 let leader_info = leader_info_provider.get_leader_info();
                 let leader_addresses = Self::get_tpu_addresses(

validator/src/bin/solana-test-validator.rs
@@ -405,6 +405,7 @@ fn main() {
             validator_exit: genesis.validator_exit.clone(),
             authorized_voter_keypairs: genesis.authorized_voter_keypairs.clone(),
             staked_nodes_overrides: genesis.staked_nodes_overrides.clone(),
+            tpu_peers: Default::default(),
             post_init: admin_service_post_init,
             tower_storage: tower_storage.clone(),
             rpc_to_plugin_manager_sender,

validator/src/main.rs
@@ -708,6 +708,35 @@ pub fn main() {
                 });
             return;
         }
+        ("rpc-send-transaction-tpu-peer", Some(subcommand_matches)) => {
+            if !subcommand_matches.is_present("tpu-peers") {
+                println!("rpc-send-transaction-tpu-peer requires at least one HOST:PORT");
+                exit(1);
+            }
+
+            let tpu_peers = subcommand_matches.values_of("tpu-peers").unwrap();
+
+            let admin_client = admin_rpc_service::connect(&ledger_path);
+            admin_rpc_service::runtime()
+                .block_on(async move {
+                    admin_client
+                        .await?
+                        .set_rpc_send_transaction_tpu_peers(tpu_peers
+                                                            .into_iter()
+                                                            .map(solana_net_utils::parse_host_port)
+                                                            .collect::<Result<Vec<SocketAddr>, String>>()
+                                                            .unwrap_or_else(|e| {
+                                                                eprintln!("failed to parse rpc send-transaction-service tpu peer address: {e}");
+                                                                exit(1);
+                                                            }))
+                        .await
+                })
+                .unwrap_or_else(|err| {
+                    println!("setStakedNodesOverrides request failed: {err}");
+                    exit(1);
+                });
+            return;
+        }
         ("set-identity", Some(subcommand_matches)) => {
             let require_tower = subcommand_matches.is_present("require_tower");
 
@@ -1331,6 +1360,10 @@ pub fn main() {
 
     let full_api = matches.is_present("full_rpc_api");
 
+    let tpu_peers = Arc::new(RwLock::new(
+        rpc_send_transaction_tpu_peers.unwrap_or_default(),
+    ));
+
     let mut validator_config = ValidatorConfig {
         require_tower: matches.is_present("require_tower"),
         tower_storage,
@@ -1439,7 +1472,7 @@ pub fn main() {
                 "rpc_send_transaction_retry_pool_max_size",
                 usize
             ),
-            tpu_peers: rpc_send_transaction_tpu_peers,
+            tpu_peers: tpu_peers.clone(),
         },
         no_poh_speed_test: matches.is_present("no_poh_speed_test"),
         no_os_memory_stats_reporting: matches.is_present("no_os_memory_stats_reporting"),
@@ -1764,6 +1797,7 @@ pub fn main() {
             post_init: admin_service_post_init.clone(),
             tower_storage: validator_config.tower_storage.clone(),
             staked_nodes_overrides,
+            tpu_peers,
             rpc_to_plugin_manager_sender,
         },
     );

anza-xyz/agave

send-transaction-service/src/send_transaction_service.rs
@@ -115,7 +115,7 @@ pub struct Config {
     pub batch_send_rate_ms: u64,
     /// When the retry pool exceeds this max size, new transactions are dropped after their first broadcast attempt
     pub retry_pool_max_size: usize,
-    pub tpu_peers: Option<Vec<SocketAddr>>,
+    pub tpu_peers: Arc<RwLock<Vec<SocketAddr>>>,
 }
 
 impl Default for Config {
@@ -128,7 +128,7 @@ impl Default for Config {
             batch_size: DEFAULT_TRANSACTION_BATCH_SIZE,
             batch_send_rate_ms: DEFAULT_BATCH_SEND_RATE_MS,
             retry_pool_max_size: MAX_TRANSACTION_RETRY_POOL_SIZE,
-            tpu_peers: None,
+            tpu_peers: Arc::default(),
         }
     }
 }
@@ -568,11 +568,18 @@ impl SendTransactionService {
         stats: &SendTransactionServiceStats,
     ) {
         // Processing the transactions in batch
-        let mut addresses = config
+        let addresses = config
             .tpu_peers
-            .as_ref()
-            .map(|addrs| addrs.iter().map(|a| (a, 0)).collect::<Vec<_>>())
-            .unwrap_or_default();
+            .read()
+            .unwrap()
+            .iter()
+            .cloned()
+            .map(|addr| (addr, 0))
+            .collect::<Vec<_>>();
+        let mut addresses = addresses
+            .iter()
+            .map(|(addr, slot)| (addr, *slot))
+            .collect::<Vec<(&SocketAddr, Slot)>>();
         let leader_addresses = Self::get_tpu_addresses_with_slots(
             tpu_address,
             leader_info,
@@ -710,11 +717,7 @@ impl SendTransactionService {
 
             let iter = wire_transactions.chunks(config.batch_size);
             for chunk in iter {
-                let mut addresses = config
-                    .tpu_peers
-                    .as_ref()
-                    .map(|addrs| addrs.iter().collect::<Vec<_>>())
-                    .unwrap_or_default();
+                let mut addresses = config.tpu_peers.read().unwrap().clone();
                 let mut leader_info_provider = leader_info_provider.lock().unwrap();
                 let leader_info = leader_info_provider.get_leader_info();
                 let leader_addresses = Self::get_tpu_addresses(

validator/src/bin/solana-test-validator.rs
@@ -405,6 +405,7 @@ fn main() {
             validator_exit: genesis.validator_exit.clone(),
             authorized_voter_keypairs: genesis.authorized_voter_keypairs.clone(),
             staked_nodes_overrides: genesis.staked_nodes_overrides.clone(),
+            tpu_peers: Arc::default(),
             post_init: admin_service_post_init,
             tower_storage: tower_storage.clone(),
             rpc_to_plugin_manager_sender,

validator/src/main.rs
@@ -711,6 +711,35 @@ pub fn main() {
                 });
             return;
         }
+        ("rpc-send-transaction-tpu-peer", Some(subcommand_matches)) => {
+            if !subcommand_matches.is_present("tpu-peers") {
+                println!("rpc-send-transaction-tpu-peer requires at least one HOST:PORT");
+                exit(1);
+            }
+
+            let tpu_peers = subcommand_matches.values_of("tpu-peers").unwrap();
+
+            let admin_client = admin_rpc_service::connect(&ledger_path);
+            admin_rpc_service::runtime()
+                .block_on(async move {
+                    admin_client
+                        .await?
+                        .set_rpc_send_transaction_tpu_peers(tpu_peers
+                                                            .into_iter()
+                                                            .map(solana_net_utils::parse_host_port)
+                                                            .collect::<Result<Vec<SocketAddr>, String>>()
+                                                            .unwrap_or_else(|e| {
+                                                                eprintln!("failed to parse rpc send-transaction-service tpu peer address: {e}");
+                                                                exit(1);
+                                                            }))
+                        .await
+                })
+                .unwrap_or_else(|err| {
+                    println!("setSendTransactionTpuPeer request failed: {err}");
+                    exit(1);
+                });
+            return;
+        }
         ("set-identity", Some(subcommand_matches)) => {
             let require_tower = subcommand_matches.is_present("require_tower");
 
@@ -1341,6 +1370,10 @@ pub fn main() {
         tvu_receive_threads,
     } = cli::thread_args::parse_num_threads_args(&matches);
 
+    let tpu_peers = Arc::new(RwLock::new(
+        rpc_send_transaction_tpu_peers.unwrap_or_default(),
+    ));
+
     let mut validator_config = ValidatorConfig {
         require_tower: matches.is_present("require_tower"),
         tower_storage,
@@ -1449,7 +1482,7 @@ pub fn main() {
                 "rpc_send_transaction_retry_pool_max_size",
                 usize
             ),
-            tpu_peers: rpc_send_transaction_tpu_peers,
+            tpu_peers: tpu_peers.clone(),
         },
         no_poh_speed_test: matches.is_present("no_poh_speed_test"),
         no_os_memory_stats_reporting: matches.is_present("no_os_memory_stats_reporting"),
@@ -1778,6 +1811,7 @@ pub fn main() {
             post_init: admin_service_post_init.clone(),
             tower_storage: validator_config.tower_storage.clone(),
             staked_nodes_overrides,
+            tpu_peers,
             rpc_to_plugin_manager_sender,
         },
     );