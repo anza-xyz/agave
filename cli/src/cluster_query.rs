608e9b60d5d3548beb2efada1f7b7aa96df43f24
kubanemil <emil3toktobekov@gmail.com>
2024-04-20 08:23:04 +0500

    review fix

--- cli/src/cluster_query.rs
+++ cli/src/cluster_query.rs
@@ -57,7 +57,7 @@ use {
         signature::Signature,
         slot_history,
         stake::{self, state::StakeStateV2},
-        system_instruction,
+        system_instruction::{self, MAX_PERMITTED_DATA_LENGTH},
         sysvar::{
             self,
             slot_history::SlotHistory,
@@ -2181,8 +2181,8 @@ pub fn process_calculate_rent(
     data_length: usize,
     use_lamports_unit: bool,
 ) -> ProcessResult {
-    if data_length > 10_000_000 {
-        println!("Warning: Account's maximum size is 10 MB".to_string());
+    if data_length > MAX_PERMITTED_DATA_LENGTH.try_into().unwrap() {
+        eprintln!("Warning: Maximum account size is {MAX_PERMITTED_DATA_LENGTH} bytes, {data_length} provided");
     }
     let rent_account = rpc_client.get_account(&sysvar::rent::id())?;
     let rent: Rent = rent_account.deserialize_data()?;
