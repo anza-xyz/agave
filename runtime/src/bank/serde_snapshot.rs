ec9d55553d46d5817c185f709012558ca7e92ff7
Tyera Eulberg <tyera@anza.xyz>
2024-05-08 13:19:53 -0600

    Update frozen_abi hash

--- runtime/src/bank/serde_snapshot.rs
+++ runtime/src/bank/serde_snapshot.rs
@@ -544,7 +544,7 @@ mod tests {
 
         // This some what long test harness is required to freeze the ABI of
         // Bank's serialization due to versioned nature
-        #[frozen_abi(digest = "ENUEoDA5CYu9NLLCuaj23qpfKBZzCvxK3T5VsUFA9sti")]
+        #[frozen_abi(digest = "8pZwgyMdvxExLgN9GMKnCdofb5CQJgsZ8Dt88hfVd9bf")]
         #[derive(Serialize, AbiExample)]
         pub struct BankAbiTestWrapperNewer {
             #[serde(serialize_with = "wrapper_newer")]
