4ce3751121641b39a6c7fbe90a33071ff4cd685d
yihau <yihau.chen@icloud.com>
2024-05-10 20:57:32 +0800

    Revert "XXX hack buildkite pipeline"
    This reverts commit effb72e78fa1a46e6e7f9b8a3791e8cc8dc1903e.

--- ci/buildkite-pipeline.sh
+++ ci/buildkite-pipeline.sh
@@ -140,115 +140,115 @@ wait_step() {
 }
 
 all_test_steps() {
-  # command_step checks1 "ci/docker-run-default-image.sh ci/test-checks.sh" 20 check
-  # command_step checks2 "ci/docker-run-default-image.sh ci/test-dev-context-only-utils.sh check-bins" 15 check
-  # command_step checks3 "ci/docker-run-default-image.sh ci/test-dev-context-only-utils.sh check-all-targets" 15 check
-  # command_step miri "ci/docker-run-default-image.sh ci/test-miri.sh" 5 check
-  # wait_step
+  command_step checks1 "ci/docker-run-default-image.sh ci/test-checks.sh" 20 check
+  command_step checks2 "ci/docker-run-default-image.sh ci/test-dev-context-only-utils.sh check-bins" 15 check
+  command_step checks3 "ci/docker-run-default-image.sh ci/test-dev-context-only-utils.sh check-all-targets" 15 check
+  command_step miri "ci/docker-run-default-image.sh ci/test-miri.sh" 5 check
+  wait_step
 
-#   # Full test suite
-#   .buildkite/scripts/build-stable.sh >> "$output_file"
+  # Full test suite
+  .buildkite/scripts/build-stable.sh >> "$output_file"
 
-#   # Docs tests
-#   if affects \
-#              .rs$ \
-#              Cargo.lock$ \
-#              Cargo.toml$ \
-#              ^ci/rust-version.sh \
-#              ^ci/test-docs.sh \
-#       ; then
-#     command_step doctest "ci/docker-run-default-image.sh ci/test-docs.sh" 15
-#   else
-#     annotate --style info --context test-docs \
-#       "Docs skipped as no .rs files were modified"
-#   fi
-#   wait_step
+  # Docs tests
+  if affects \
+             .rs$ \
+             Cargo.lock$ \
+             Cargo.toml$ \
+             ^ci/rust-version.sh \
+             ^ci/test-docs.sh \
+      ; then
+    command_step doctest "ci/docker-run-default-image.sh ci/test-docs.sh" 15
+  else
+    annotate --style info --context test-docs \
+      "Docs skipped as no .rs files were modified"
+  fi
+  wait_step
 
-#   # SBF test suite
-#   if affects \
-#              .rs$ \
-#              Cargo.lock$ \
-#              Cargo.toml$ \
-#              ^ci/rust-version.sh \
-#              ^ci/test-stable-sbf.sh \
-#              ^ci/test-stable.sh \
-#              ^ci/test-local-cluster.sh \
-#              ^core/build.rs \
-#              ^fetch-perf-libs.sh \
-#              ^programs/ \
-#              ^sdk/ \
-#              cargo-build-bpf$ \
-#              cargo-test-bpf$ \
-#              cargo-build-sbf$ \
-#              cargo-test-sbf$ \
-#       ; then
-#     cat >> "$output_file" <<"EOF"
-#   - command: "ci/docker-run-default-image.sh ci/test-stable-sbf.sh"
-#     name: "stable-sbf"
-#     timeout_in_minutes: 35
-#     artifact_paths: "sbf-dumps.tar.bz2"
-#     agents:
-#       queue: "solana"
-# EOF
-#   else
-#     annotate --style info \
-#       "Stable-SBF skipped as no relevant files were modified"
-#   fi
+  # SBF test suite
+  if affects \
+             .rs$ \
+             Cargo.lock$ \
+             Cargo.toml$ \
+             ^ci/rust-version.sh \
+             ^ci/test-stable-sbf.sh \
+             ^ci/test-stable.sh \
+             ^ci/test-local-cluster.sh \
+             ^core/build.rs \
+             ^fetch-perf-libs.sh \
+             ^programs/ \
+             ^sdk/ \
+             cargo-build-bpf$ \
+             cargo-test-bpf$ \
+             cargo-build-sbf$ \
+             cargo-test-sbf$ \
+      ; then
+    cat >> "$output_file" <<"EOF"
+  - command: "ci/docker-run-default-image.sh ci/test-stable-sbf.sh"
+    name: "stable-sbf"
+    timeout_in_minutes: 35
+    artifact_paths: "sbf-dumps.tar.bz2"
+    agents:
+      queue: "solana"
+EOF
+  else
+    annotate --style info \
+      "Stable-SBF skipped as no relevant files were modified"
+  fi
 
-#   # Downstream backwards compatibility
-#   if affects \
-#              .rs$ \
-#              Cargo.lock$ \
-#              Cargo.toml$ \
-#              ^ci/rust-version.sh \
-#              ^ci/test-stable-perf.sh \
-#              ^ci/test-stable.sh \
-#              ^ci/test-local-cluster.sh \
-#              ^core/build.rs \
-#              ^fetch-perf-libs.sh \
-#              ^programs/ \
-#              ^sdk/ \
-#              cargo-build-bpf$ \
-#              cargo-test-bpf$ \
-#              cargo-build-sbf$ \
-#              cargo-test-sbf$ \
-#              ^ci/downstream-projects \
-#              .buildkite/scripts/build-downstream-projects.sh \
-#       ; then
-#     .buildkite/scripts/build-downstream-projects.sh >> "$output_file"
-#   else
-#     annotate --style info \
-#       "downstream-projects skipped as no relevant files were modified"
-#   fi
+  # Downstream backwards compatibility
+  if affects \
+             .rs$ \
+             Cargo.lock$ \
+             Cargo.toml$ \
+             ^ci/rust-version.sh \
+             ^ci/test-stable-perf.sh \
+             ^ci/test-stable.sh \
+             ^ci/test-local-cluster.sh \
+             ^core/build.rs \
+             ^fetch-perf-libs.sh \
+             ^programs/ \
+             ^sdk/ \
+             cargo-build-bpf$ \
+             cargo-test-bpf$ \
+             cargo-build-sbf$ \
+             cargo-test-sbf$ \
+             ^ci/downstream-projects \
+             .buildkite/scripts/build-downstream-projects.sh \
+      ; then
+    .buildkite/scripts/build-downstream-projects.sh >> "$output_file"
+  else
+    annotate --style info \
+      "downstream-projects skipped as no relevant files were modified"
+  fi
 
-#   # Wasm support
-#   if affects \
-#              ^ci/test-wasm.sh \
-#              ^ci/test-stable.sh \
-#              ^sdk/ \
-#       ; then
-#     command_step wasm "ci/docker-run-default-image.sh ci/test-wasm.sh" 20
-#   else
-#     annotate --style info \
-#       "wasm skipped as no relevant files were modified"
-#   fi
+  # Wasm support
+  if affects \
+             ^ci/test-wasm.sh \
+             ^ci/test-stable.sh \
+             ^sdk/ \
+      ; then
+    command_step wasm "ci/docker-run-default-image.sh ci/test-wasm.sh" 20
+  else
+    annotate --style info \
+      "wasm skipped as no relevant files were modified"
+  fi
 
-#   # Benches...
-#   if affects \
-#              .rs$ \
-#              Cargo.lock$ \
-#              Cargo.toml$ \
-#              ^ci/rust-version.sh \
-#              ^ci/test-coverage.sh \
-#              ^ci/test-bench.sh \
-#              ^ci/bench \
-#              .buildkite/scripts/build-bench.sh \
-#       ; then
-#     .buildkite/scripts/build-bench.sh >> "$output_file"
-#   else
-#     annotate --style info --context test-bench \
-#       "Bench skipped as no .rs files were modified"
-#   fi
+  # Benches...
+  if affects \
+             .rs$ \
+             Cargo.lock$ \
+             Cargo.toml$ \
+             ^ci/rust-version.sh \
+             ^ci/test-coverage.sh \
+             ^ci/test-bench.sh \
+             ^ci/bench \
+             .buildkite/scripts/build-bench.sh \
+      ; then
+    .buildkite/scripts/build-bench.sh >> "$output_file"
+  else
+    annotate --style info --context test-bench \
+      "Bench skipped as no .rs files were modified"
+  fi
 
   # Coverage...
   if affects \
@@ -267,46 +267,46 @@ all_test_steps() {
 }
 
 pull_or_push_steps() {
-  # command_step sanity "ci/test-sanity.sh" 5 check
-  # wait_step
+  command_step sanity "ci/test-sanity.sh" 5 check
+  wait_step
 
-  # # Check for any .sh file changes
-  # if affects \
-  #             .sh$ \
-  #             ^.buildkite/hooks \
-  #     ; then
-  #   command_step shellcheck "ci/shellcheck.sh" 5 check
-  #   wait_step
-  # fi
+  # Check for any .sh file changes
+  if affects \
+              .sh$ \
+              ^.buildkite/hooks \
+      ; then
+    command_step shellcheck "ci/shellcheck.sh" 5 check
+    wait_step
+  fi
 
-  # # Version bump PRs are an edge case that can skip most of the CI steps
-  # if affects .toml$ && affects .lock$ && ! affects_other_than .toml$ .lock$; then
-  #   optional_old_version_number=$(git diff origin/"$BUILDKITE_PULL_REQUEST_BASE_BRANCH"..HEAD validator/Cargo.toml | \
-  #     grep -e "^-version" | sed  's/-version = "\(.*\)"/\1/')
-  #   echo "optional_old_version_number: ->$optional_old_version_number<-"
-  #   new_version_number=$(grep -e  "^version = " validator/Cargo.toml | sed 's/version = "\(.*\)"/\1/')
-  #   echo "new_version_number: ->$new_version_number<-"
+  # Version bump PRs are an edge case that can skip most of the CI steps
+  if affects .toml$ && affects .lock$ && ! affects_other_than .toml$ .lock$; then
+    optional_old_version_number=$(git diff origin/"$BUILDKITE_PULL_REQUEST_BASE_BRANCH"..HEAD validator/Cargo.toml | \
+      grep -e "^-version" | sed  's/-version = "\(.*\)"/\1/')
+    echo "optional_old_version_number: ->$optional_old_version_number<-"
+    new_version_number=$(grep -e  "^version = " validator/Cargo.toml | sed 's/version = "\(.*\)"/\1/')
+    echo "new_version_number: ->$new_version_number<-"
 
-  #   # Every line in a version bump diff will match one of these patterns. Since we're using grep -v the output is the
-  #   # lines that don't match. Any diff that produces output here is not a version bump.
-  #   # | cat is a no-op. If this pull request is a version bump then grep will output no lines and have an exit code of 1.
-  #   # Piping the output to cat prevents that non-zero exit code from exiting this script
-  #   diff_other_than_version_bump=$(git diff origin/"$BUILDKITE_PULL_REQUEST_BASE_BRANCH"..HEAD | \
-  #     grep -vE "^ |^@@ |^--- |^\+\+\+ |^index |^diff |^-( \")?solana.*$optional_old_version_number|^\+( \")?solana.*$new_version_number|^-version|^\+version"|cat)
-  #   echo "diff_other_than_version_bump: ->$diff_other_than_version_bump<-"
+    # Every line in a version bump diff will match one of these patterns. Since we're using grep -v the output is the
+    # lines that don't match. Any diff that produces output here is not a version bump.
+    # | cat is a no-op. If this pull request is a version bump then grep will output no lines and have an exit code of 1.
+    # Piping the output to cat prevents that non-zero exit code from exiting this script
+    diff_other_than_version_bump=$(git diff origin/"$BUILDKITE_PULL_REQUEST_BASE_BRANCH"..HEAD | \
+      grep -vE "^ |^@@ |^--- |^\+\+\+ |^index |^diff |^-( \")?solana.*$optional_old_version_number|^\+( \")?solana.*$new_version_number|^-version|^\+version"|cat)
+    echo "diff_other_than_version_bump: ->$diff_other_than_version_bump<-"
 
-  #   if [ -z "$diff_other_than_version_bump" ]; then
-  #     echo "Diff only contains version bump."
-  #     command_step checks "ci/docker-run-default-image.sh ci/test-checks.sh" 20
-  #     exit 0
-  #   fi
-  # fi
+    if [ -z "$diff_other_than_version_bump" ]; then
+      echo "Diff only contains version bump."
+      command_step checks "ci/docker-run-default-image.sh ci/test-checks.sh" 20
+      exit 0
+    fi
+  fi
 
   # Run the full test suite by default, skipping only if modifications are local
   # to some particular areas of the tree
-  # if affects_other_than ^.mergify .md$ ^docs/ ^.gitbook; then
+  if affects_other_than ^.mergify .md$ ^docs/ ^.gitbook; then
     all_test_steps
-  # fi
+  fi
 
   # docs changes run on Github actions...
 }
