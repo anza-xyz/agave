8fa1aebe9e1ca3bc0b1ea9593ef1d9222c07d77b
steviez <steven@anza.xyz>
2024-06-14 14:56:58 -0500

    ledger-tool: Add structs for load and process output
    The struct with named fields will allow the number of fields in output
    to grow more cleanly

--- ledger-tool/src/ledger_utils.rs
+++ ledger-tool/src/ledger_utils.rs
@@ -54,6 +54,11 @@ use {
     thiserror::Error,
 };
 
+pub(crate) struct LoadAndProcessLedgerOutput {
+    pub bank_forks: Arc<RwLock<BankForks>>,
+    pub starting_snapshot_hashes: Option<StartingSnapshotHashes>,
+}
+
 const PROCESS_SLOTS_HELP_STRING: &str =
     "The starting slot is either the latest found snapshot slot, or genesis (slot 0) if the \
      --no-snapshot flag was specified or if no snapshots were found. \
@@ -99,7 +104,7 @@ pub fn load_and_process_ledger_or_exit(
     blockstore: Arc<Blockstore>,
     process_options: ProcessOptions,
     transaction_status_sender: Option<TransactionStatusSender>,
-) -> (Arc<RwLock<BankForks>>, Option<StartingSnapshotHashes>) {
+) -> LoadAndProcessLedgerOutput {
     load_and_process_ledger(
         arg_matches,
         genesis_config,
@@ -119,7 +124,7 @@ pub fn load_and_process_ledger(
     blockstore: Arc<Blockstore>,
     process_options: ProcessOptions,
     transaction_status_sender: Option<TransactionStatusSender>,
-) -> Result<(Arc<RwLock<BankForks>>, Option<StartingSnapshotHashes>), LoadAndProcessLedgerError> {
+) -> Result<LoadAndProcessLedgerOutput, LoadAndProcessLedgerError> {
     let bank_snapshots_dir = if blockstore.is_primary_access() {
         blockstore.ledger_path().join("snapshot")
     } else {
@@ -402,7 +407,10 @@ pub fn load_and_process_ledger(
         None, // Maybe support this later, though
         &accounts_background_request_sender,
     )
-    .map(|_| (bank_forks, starting_snapshot_hashes))
+    .map(|_| LoadAndProcessLedgerOutput {
+        bank_forks,
+        starting_snapshot_hashes,
+    })
     .map_err(LoadAndProcessLedgerError::ProcessBlockstoreFromRoot);
 
     exit.store(true, Ordering::Relaxed);
