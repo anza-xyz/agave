bd1e624fb27016a05d67731441bf699cd32082f5
Huzaifa696 <huzaifa.rehman696@gmail.com>
2024-06-26 05:32:11 +0000

    incorporated review changes
    - changed name of entry level locks lookup table
    - write locks also track the num of outstanding locks
    - simplied lock_account function flow
    - formatted code with cargo fmt
    - cleared some nits

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -3245,7 +3245,8 @@ impl Bank {
             .collect::<Result<Vec<_>>>()?;
         let tx_account_lock_limit = self.get_transaction_account_lock_limit();
         let feature_set: Arc<FeatureSet> = self.feature_set.clone();
-        let allow_self_conflicting_txns = feature_set.is_active(&feature_set::allow_self_conflicting_entries::id());
+        let allow_self_conflicting_txns =
+            feature_set.is_active(&feature_set::allow_self_conflicting_entries::id());
         let (lock_results, _self_conflicting_batch) = self.rc.accounts.lock_accounts(
             sanitized_txs.iter(),
             tx_account_lock_limit,
@@ -3265,7 +3266,8 @@ impl Bank {
     ) -> (TransactionBatch<'a, 'b>, bool) {
         let tx_account_lock_limit = self.get_transaction_account_lock_limit();
         let feature_set: Arc<FeatureSet> = self.feature_set.clone();
-        let allow_self_conflicting_txns = feature_set.is_active(&feature_set::allow_self_conflicting_entries::id());
+        let allow_self_conflicting_txns =
+            feature_set.is_active(&feature_set::allow_self_conflicting_entries::id());
 
         let (lock_results, self_conflicting_batch) = self.rc.accounts.lock_accounts(
             txs.iter(),
@@ -3288,7 +3290,8 @@ impl Bank {
         // this lock_results could be: Ok, AccountInUse, WouldExceedBlockMaxLimit or WouldExceedAccountMaxLimit
         let tx_account_lock_limit = self.get_transaction_account_lock_limit();
         let feature_set: Arc<FeatureSet> = self.feature_set.clone();
-        let allow_self_conflicting_txns = feature_set.is_active(&feature_set::allow_self_conflicting_entries::id());
+        let allow_self_conflicting_txns =
+            feature_set.is_active(&feature_set::allow_self_conflicting_entries::id());
 
         let lock_results = self.rc.accounts.lock_accounts_with_results(
             transactions.iter(),
@@ -6717,7 +6720,8 @@ impl Bank {
             .map(SanitizedTransaction::from_transaction_for_tests)
             .collect::<Vec<_>>();
         let feature_set: Arc<FeatureSet> = self.feature_set.clone();
-        let allow_self_conflicting_txns = feature_set.is_active(&feature_set::allow_self_conflicting_entries::id());
+        let allow_self_conflicting_txns =
+            feature_set.is_active(&feature_set::allow_self_conflicting_entries::id());
 
         let (lock_results, _) = self.rc.accounts.lock_accounts(
             sanitized_txs.iter(),
