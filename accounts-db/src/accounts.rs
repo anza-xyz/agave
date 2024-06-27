ffb37c505d352a99236e80234e87febb4bcc2585
Huzaifa696 <huzaifa.rehman696@gmail.com>
2024-06-27 05:40:19 +0000

    incorporated review changes
    - added comments for clarity
    - changed mis-leading variable name

--- accounts-db/src/accounts.rs
+++ accounts-db/src/accounts.rs
@@ -67,6 +67,7 @@ impl BatchAccountLocks {
 #[cfg_attr(feature = "frozen-abi", derive(AbiExample))]
 #[derive(Debug, Default)]
 pub struct AccountLocks {
+    // A key can have multiple outstanding write locks in the case of a self-conflicting batch.
     write_locks: AHashMap<Pubkey, u64>,
     readonly_locks: AHashMap<Pubkey, u64>,
 }
@@ -604,7 +605,7 @@ impl Accounts {
         batch_account_locks: &mut BatchAccountLocks,
         allow_self_conflicting_entries: bool,
     ) -> (Result<()>, bool) {
-        let mut self_conflicting_tx = false;
+        let mut self_conflicting_batch = false;
         for k in writable_keys.iter() {
             if account_locks.is_locked_write(k) || account_locks.is_locked_readonly(k) {
                 if allow_self_conflicting_entries {
@@ -614,7 +615,7 @@ impl Accounts {
                         debug!("Write-only account in use: {:?}", k);
                         return (Err(TransactionError::AccountInUse), false);
                     }
-                    self_conflicting_tx = true;
+                    self_conflicting_batch = true;
                 } else {
                     return (Err(TransactionError::AccountInUse), false);
                 }
@@ -627,7 +628,7 @@ impl Accounts {
                         debug!("Read-only account in use: {:?}", k);
                         return (Err(TransactionError::AccountInUse), false);
                     }
-                    self_conflicting_tx = true;
+                    self_conflicting_batch = true;
                 } else {
                     return (Err(TransactionError::AccountInUse), false);
                 }
@@ -648,7 +649,7 @@ impl Accounts {
             }
         }
 
-        (Ok(()), self_conflicting_tx)
+        (Ok(()), self_conflicting_batch)
     }
 
     fn unlock_account(
@@ -1206,8 +1207,10 @@ mod tests {
             instructions,
         );
         let tx = new_sanitized_tx(&[&keypair0], message, Hash::default());
-        let (results0, _) = accounts.lock_accounts([tx.clone()].iter(), MAX_TX_ACCOUNT_LOCKS, true);
+        let (results0, self_conflicting_batch) =
+            accounts.lock_accounts([tx.clone()].iter(), MAX_TX_ACCOUNT_LOCKS, true);
 
+        assert_eq!(self_conflicting_batch, false);
         assert_eq!(results0, vec![Ok(())]);
         assert_eq!(
             *accounts
@@ -1296,7 +1299,10 @@ mod tests {
         );
         let tx1 = new_sanitized_tx(&[&keypair1], message, Hash::default());
         let txs = vec![tx0, tx1];
-        let (results1, _) = accounts.lock_accounts(txs.iter(), MAX_TX_ACCOUNT_LOCKS, true);
+        let (results1, self_conflicting_batch) =
+            accounts.lock_accounts(txs.iter(), MAX_TX_ACCOUNT_LOCKS, true);
+
+        assert_eq!(self_conflicting_batch, true);
         assert_eq!(
             results1,
             vec![
@@ -1310,8 +1316,6 @@ mod tests {
             .unwrap()
             .write_locks
             .contains_key(&keypair1.pubkey()));
-
-        // accounts.unlock_accounts(txs.iter().zip(&results1));
 
         // batch write-write conflict. outstanding (read/write lock)
         let instructions = vec![CompiledInstruction::new(2, &(), vec![0, 1])];
@@ -1335,7 +1339,11 @@ mod tests {
         );
         let tx1 = new_sanitized_tx(&[&keypair0], message, Hash::default());
         let txs2 = vec![tx0, tx1];
-        let (results2, _) = accounts.lock_accounts(txs2.iter(), MAX_TX_ACCOUNT_LOCKS, true);
+        let (results2, self_conflicting_batch) =
+            accounts.lock_accounts(txs2.iter(), MAX_TX_ACCOUNT_LOCKS, true);
+
+        assert_eq!(self_conflicting_batch, false);
+        // the first tx conflicts with the previous batch
         assert_eq!(
             results2,
             vec![
@@ -1387,7 +1395,10 @@ mod tests {
         );
         let tx1 = new_sanitized_tx(&[&keypair1], message, Hash::default());
         let txs = vec![tx0, tx1];
-        let (results1, _) = accounts.lock_accounts(txs.iter(), MAX_TX_ACCOUNT_LOCKS, true);
+        let (results1, self_conflicting_batch) =
+            accounts.lock_accounts(txs.iter(), MAX_TX_ACCOUNT_LOCKS, true);
+
+        assert_eq!(self_conflicting_batch, true);
         assert_eq!(
             results1,
             vec![
