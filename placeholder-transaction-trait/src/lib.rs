a831bc6d00a64e6a88144a2df81b871bdc08c321
Andrew Fitzgerald <apfitzge@gmail.com>
2024-06-28 10:17:45 -0500

    MessageAddressTableLookup

--- placeholder-transaction-trait/src/lib.rs
+++ placeholder-transaction-trait/src/lib.rs
@@ -1,14 +1,3 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+mod message_address_table_lookup;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub use message_address_table_lookup::*;
