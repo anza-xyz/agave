0deeb14b9905556f372ddac876ac3d0d551ae090
HaoranYi <219428+HaoranYi@users.noreply.github.com>
2024-07-02 12:15:59 -0500

    Update accounts-db/accounts-hash-cache-tool/src/main.rs
    Co-authored-by: Brooks <brooks@prumo.org>

--- accounts-db/accounts-hash-cache-tool/src/main.rs
+++ accounts-db/accounts-hash-cache-tool/src/main.rs
@@ -460,7 +460,11 @@ fn do_diff_dirs(
         }
         if then_diff_files {
             for (file1, file2) in &mismatches {
-                println!("Differences between '{}' and '{}':", file1.0.0.display(), file2.0.0.display());
+                println!(
+                    "Differences between '{}' and '{}':",
+                    file1.0 .0.display(),
+                    file2.0 .0.display(),
+                );
                 if let Err(err) = do_diff_files(&file1.0 .0, &file2.0 .0) {
                     eprintln!("Error: failed to diff files: {err}");
                 }
