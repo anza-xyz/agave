0f17b94cad170f0f5691c1839b5ef24dc84e561c
Andrew Fitzgerald <apfitzge@gmail.com>
2024-07-02 10:01:38 -0500

    rename/move files

--- svm-transaction/src/lib.rs
+++ svm-transaction/src/lib.rs
@@ -1,8 +1,6 @@
 mod instruction;
 mod message_address_table_lookup;
-mod solana_message;
-mod solana_transaction;
+mod svm_message;
+mod svm_transaction;
 
-pub use {
-    instruction::*, message_address_table_lookup::*, solana_message::*, solana_transaction::*,
-};
+pub use {instruction::*, message_address_table_lookup::*, svm_message::*, svm_transaction::*};
