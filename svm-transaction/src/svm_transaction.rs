d131feac238aab1d8cce9b9484b6f8ea654e4c32
Andrew Fitzgerald <apfitzge@gmail.com>
2024-07-11 13:41:15 -0500

    remove extra features from SVMTransaction

--- svm-transaction/src/svm_transaction.rs
+++ svm-transaction/src/svm_transaction.rs
@@ -1,7 +1,4 @@
-use {
-    crate::svm_message::SVMMessage,
-    solana_sdk::{hash::Hash, signature::Signature, transaction::VersionedTransaction},
-};
+use {crate::svm_message::SVMMessage, solana_sdk::signature::Signature};
 
 mod sanitized_transaction;
 
@@ -11,16 +8,4 @@ pub trait SVMTransaction: SVMMessage {
 
     /// Get all the signatures of the message.
     fn signatures(&self) -> &[Signature];
-
-    /// Returns the message hash.
-    // TODO: consider moving this to Message
-    fn message_hash(&self) -> &Hash;
-
-    /// Returns true if the transaction is a simple vote transaction.
-    // TODO: consider moving this to Message
-    fn is_simple_vote_transaction(&self) -> bool;
-
-    /// Make a versioned transaction copy of the transaction.
-    // TODO: get rid of this once PoH supports generic transactions
-    fn to_versioned_transaction(&self) -> VersionedTransaction;
 }
--- svm-transaction/src/svm_transaction/sanitized_transaction.rs
+++ svm-transaction/src/svm_transaction/sanitized_transaction.rs
@@ -1,10 +1,6 @@
 use {
     crate::svm_transaction::SVMTransaction,
-    solana_sdk::{
-        hash::Hash,
-        signature::Signature,
-        transaction::{SanitizedTransaction, VersionedTransaction},
-    },
+    solana_sdk::{signature::Signature, transaction::SanitizedTransaction},
 };
 
 impl SVMTransaction for SanitizedTransaction {
@@ -14,17 +10,5 @@ impl SVMTransaction for SanitizedTransaction {
 
     fn signatures(&self) -> &[Signature] {
         SanitizedTransaction::signatures(self)
-    }
-
-    fn message_hash(&self) -> &Hash {
-        SanitizedTransaction::message_hash(self)
-    }
-
-    fn is_simple_vote_transaction(&self) -> bool {
-        SanitizedTransaction::is_simple_vote_transaction(self)
-    }
-
-    fn to_versioned_transaction(&self) -> VersionedTransaction {
-        SanitizedTransaction::to_versioned_transaction(self)
     }
 }
