afde92f2944496098664599ef3c32442b4c63624
Lucas <lucas.tnagel@gmail.com>
2024-07-30 15:45:55 -0300

    Rename functions and add comments

--- svm/tests/concurrent_tests.rs
+++ svm/tests/concurrent_tests.rs
@@ -15,7 +15,7 @@ use {
 
 mod mock_bank;
 
-fn program_cache_execution() {
+fn program_cache_execution(threads: usize) {
     let mut mock_bank = MockBankCallback::default();
     let batch_processor = TransactionBatchProcessor::<MockForkGraph>::new(5, 5, HashSet::new());
     let fork_graph = Arc::new(RwLock::new(MockForkGraph {}));
@@ -33,7 +33,7 @@ fn program_cache_execution() {
         .map(|(idx, key)| (*key, idx as u64))
         .collect();
 
-    let ths: Vec<_> = (0..4)
+    let ths: Vec<_> = (0..threads)
         .map(|_| {
             let local_bank = mock_bank.clone();
             let processor = TransactionBatchProcessor::new_from(
@@ -61,29 +61,39 @@ fn program_cache_execution() {
     }
 }
 
+// Shuttle has its own internal scheduler and the following tests change the way it operates to
+// increase the efficiency in finding problems in the program cache's concurrent code.
+
+// This test leverages the probabilistic concurrency testing algorithm
+// (https://www.microsoft.com/en-us/research/wp-content/uploads/2016/02/asplos277-pct.pdf).
+// It bounds the numbers of preemptions to explore (five in this test) for the four
+// threads we use. We run it for 300 iterations.
 #[test]
-fn probabilistic_concurrency_test() {
+fn test_program_cache_with_probabilistic_scheduler() {
     shuttle::check_pct(
         move || {
-            program_cache_execution();
+            program_cache_execution(4);
         },
         300,
         5,
     );
 }
 
+// In this case, the scheduler is random and may preempt threads at any point and any time.
 #[test]
-fn random_concurrency_test() {
-    shuttle::check_random(move || program_cache_execution(), 300);
+fn test_program_cache_with_random_scheduler() {
+    shuttle::check_random(move || program_cache_execution(4), 300);
 }
 
+// This test explores all the possible thread scheduling patterns that might affect the program
+// cache. There is a limitation to run only 500 iterations to avoid consuming too much CI time.
 #[test]
-fn dfs_concurrency_test() {
+fn test_program_cache_with_exhaustive_scheduler() {
     // The DFS (shuttle::check_dfs) test is only complete when we do not generate random
     // values in a thread.
     // Since this is not the case for the execution of jitted program, we can still run the test
     // but with decreased accuracy.
-    let scheduler = shuttle::scheduler::DfsScheduler::new(Some(300), true);
+    let scheduler = shuttle::scheduler::DfsScheduler::new(Some(500), true);
     let runner = Runner::new(scheduler, Default::default());
-    runner.run(move || program_cache_execution());
+    runner.run(move || program_cache_execution(4));
 }
