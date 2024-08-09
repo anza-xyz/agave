9b898ec494c5845d8c12c18f2c04887ab3aa2223
behzad nouri <behzadnouri@gmail.com>
2024-08-09 16:07:55 -0500

    customizes override logic for gossip ContactInfo
    If there are two running instances of the same node, we want the one
    with more recent start time to be propagated through gossip regardless
    of wallclocks.
    
    The commit adds custom override logic for ContactInfo to first compare
    by outset timestamp.

--- gossip/src/crds.rs
+++ gossip/src/crds.rs
@@ -196,12 +196,19 @@ impl Default for Crds {
 // Both values should have the same key/label.
 fn overrides(value: &CrdsValue, other: &VersionedCrdsValue) -> bool {
     assert_eq!(value.label(), other.value.label(), "labels mismatch!");
-    // Node instances are special cased so that if there are two running
-    // instances of the same node, the more recent start is propagated through
-    // gossip regardless of wallclocks.
+    // Contact-infos and node instances are special cased so that if there are
+    // two running instances of the same node, the more recent start is
+    // propagated through gossip regardless of wallclocks.
     if let CrdsData::NodeInstance(value) = &value.data {
         if let Some(out) = value.overrides(&other.value) {
             return out;
+        }
+    }
+    if let CrdsData::ContactInfo(value) = &value.data {
+        if let CrdsData::ContactInfo(other) = &other.value.data {
+            if let Some(out) = value.overrides(other) {
+                return out;
+            }
         }
     }
     match value.wallclock().cmp(&other.value.wallclock()) {
