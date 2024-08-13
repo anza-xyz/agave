2be74828512d5f2f3c1b108b4367f6f0381f9a93
Tao Zhu <tao@anza.xyz>
2024-08-13 16:26:34 -0500

    rename bench group

--- runtime-transaction/benches/process_compute_budget_instructions.rs
+++ runtime-transaction/benches/process_compute_budget_instructions.rs
@@ -27,9 +27,9 @@ fn build_sanitized_transaction(
 }
 
 fn bench_process_compute_budget_instructions_empty(c: &mut Criterion) {
-    c.benchmark_group(format!("bench_size_{NUM_TRANSACTIONS_PER_ITER}"))
+    c.benchmark_group("bench_process_compute_budget_instructions_empty")
         .throughput(Throughput::Elements(NUM_TRANSACTIONS_PER_ITER as u64))
-        .bench_function("No instructions", |bencher| {
+        .bench_function("0 instructions", |bencher| {
             let tx = build_sanitized_transaction(&Keypair::new(), &[]);
             bencher.iter(|| {
                 (0..NUM_TRANSACTIONS_PER_ITER).for_each(|_| {
@@ -43,30 +43,38 @@ fn bench_process_compute_budget_instructions_empty(c: &mut Criterion) {
 }
 
 fn bench_process_compute_budget_instructions_no_builtins(c: &mut Criterion) {
-    c.benchmark_group(format!("bench_size_{NUM_TRANSACTIONS_PER_ITER}"))
+    let num_instructions = 4;
+    c.benchmark_group("bench_process_compute_budget_instructions_no_builtins")
         .throughput(Throughput::Elements(NUM_TRANSACTIONS_PER_ITER as u64))
-        .bench_function("No builtins", |bencher| {
-            let ixs: Vec<_> = (0..4)
-                .map(|_| {
-                    Instruction::new_with_bincode(DUMMY_PROGRAM_ID.parse().unwrap(), &(), vec![])
-                })
-                .collect();
-            let tx = build_sanitized_transaction(&Keypair::new(), &ixs);
-            bencher.iter(|| {
-                (0..NUM_TRANSACTIONS_PER_ITER).for_each(|_| {
-                    assert!(process_compute_budget_instructions(black_box(
-                        tx.message().program_instructions_iter()
-                    ))
-                    .is_ok())
-                })
-            });
-        });
+        .bench_function(
+            format!("{num_instructions} dummy Instructions"),
+            |bencher| {
+                let ixs: Vec<_> = (0..num_instructions)
+                    .map(|_| {
+                        Instruction::new_with_bincode(
+                            DUMMY_PROGRAM_ID.parse().unwrap(),
+                            &(),
+                            vec![],
+                        )
+                    })
+                    .collect();
+                let tx = build_sanitized_transaction(&Keypair::new(), &ixs);
+                bencher.iter(|| {
+                    (0..NUM_TRANSACTIONS_PER_ITER).for_each(|_| {
+                        assert!(process_compute_budget_instructions(black_box(
+                            tx.message().program_instructions_iter()
+                        ))
+                        .is_ok())
+                    })
+                });
+            },
+        );
 }
 
 fn bench_process_compute_budget_instructions_compute_budgets(c: &mut Criterion) {
-    c.benchmark_group(format!("bench_size_{NUM_TRANSACTIONS_PER_ITER}"))
+    c.benchmark_group("bench_process_compute_budget_instructions_compute_budgets")
         .throughput(Throughput::Elements(NUM_TRANSACTIONS_PER_ITER as u64))
-        .bench_function("Only compute-budget instructions", |bencher| {
+        .bench_function("4 compute-budget instructions", |bencher| {
             let ixs = vec![
                 ComputeBudgetInstruction::request_heap_frame(40 * 1024),
                 ComputeBudgetInstruction::set_compute_unit_limit(u32::MAX),
@@ -86,9 +94,9 @@ fn bench_process_compute_budget_instructions_compute_budgets(c: &mut Criterion) 
 }
 
 fn bench_process_compute_budget_instructions_builtins(c: &mut Criterion) {
-    c.benchmark_group(format!("bench_size_{NUM_TRANSACTIONS_PER_ITER}"))
+    c.benchmark_group("bench_process_compute_budget_instructions_builtins")
         .throughput(Throughput::Elements(NUM_TRANSACTIONS_PER_ITER as u64))
-        .bench_function("Only builtins", |bencher| {
+        .bench_function("4 dummy builtins", |bencher| {
             let ixs = vec![
                 Instruction::new_with_bincode(solana_sdk::bpf_loader::id(), &(), vec![]),
                 Instruction::new_with_bincode(solana_sdk::secp256k1_program::id(), &(), vec![]),
@@ -112,33 +120,41 @@ fn bench_process_compute_budget_instructions_builtins(c: &mut Criterion) {
 }
 
 fn bench_process_compute_budget_instructions_mixed(c: &mut Criterion) {
-    c.benchmark_group(format!("bench_size_{NUM_TRANSACTIONS_PER_ITER}"))
+    let num_instructions = 1024;
+    c.benchmark_group("bench_process_compute_budget_instructions_mixed")
         .throughput(Throughput::Elements(NUM_TRANSACTIONS_PER_ITER as u64))
-        .bench_function("Mixed instructions", |bencher| {
-            let payer_keypair = Keypair::new();
-            let mut ixs: Vec<_> = (0..128)
-                .map(|_| {
-                    Instruction::new_with_bincode(DUMMY_PROGRAM_ID.parse().unwrap(), &(), vec![])
-                })
-                .collect();
-            ixs.extend(vec![
-                ComputeBudgetInstruction::request_heap_frame(40 * 1024),
-                ComputeBudgetInstruction::set_compute_unit_limit(u32::MAX),
-                ComputeBudgetInstruction::set_compute_unit_price(u64::MAX),
-                ComputeBudgetInstruction::set_loaded_accounts_data_size_limit(u32::MAX),
-                system_instruction::transfer(&payer_keypair.pubkey(), &Pubkey::new_unique(), 1),
-            ]);
-            let tx = build_sanitized_transaction(&payer_keypair, &ixs);
+        .bench_function(
+            format!("{num_instructions} mixed instructions"),
+            |bencher| {
+                let payer_keypair = Keypair::new();
+                let mut ixs: Vec<_> = (0..num_instructions)
+                    .map(|_| {
+                        Instruction::new_with_bincode(
+                            DUMMY_PROGRAM_ID.parse().unwrap(),
+                            &(),
+                            vec![],
+                        )
+                    })
+                    .collect();
+                ixs.extend(vec![
+                    ComputeBudgetInstruction::request_heap_frame(40 * 1024),
+                    ComputeBudgetInstruction::set_compute_unit_limit(u32::MAX),
+                    ComputeBudgetInstruction::set_compute_unit_price(u64::MAX),
+                    ComputeBudgetInstruction::set_loaded_accounts_data_size_limit(u32::MAX),
+                    system_instruction::transfer(&payer_keypair.pubkey(), &Pubkey::new_unique(), 1),
+                ]);
+                let tx = build_sanitized_transaction(&payer_keypair, &ixs);
 
-            bencher.iter(|| {
-                (0..NUM_TRANSACTIONS_PER_ITER).for_each(|_| {
-                    assert!(process_compute_budget_instructions(black_box(
-                        tx.message().program_instructions_iter()
-                    ))
-                    .is_ok())
-                })
-            });
-        });
+                bencher.iter(|| {
+                    (0..NUM_TRANSACTIONS_PER_ITER).for_each(|_| {
+                        assert!(process_compute_budget_instructions(black_box(
+                            tx.message().program_instructions_iter()
+                        ))
+                        .is_ok())
+                    })
+                });
+            },
+        );
 }
 
 criterion_group!(
