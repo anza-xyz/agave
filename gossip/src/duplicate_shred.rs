73b8496044f0ee21372fb469e96a2fd8a8aeccfa
Ashwin Sekar <ashwin@solana.com>
2024-08-27 14:18:03 +0000

    gossip: demote invalid duplicate proof errors to info

--- gossip/src/duplicate_shred.rs
+++ gossip/src/duplicate_shred.rs
@@ -90,6 +90,22 @@ pub enum Error {
     UnknownSlotLeader(Slot),
 }
 
+impl Error {
+    /// Errors indicating that the initial node submitted an invalid duplicate proof case
+    pub(crate) fn is_non_critical(&self) -> bool {
+        matches!(
+            self,
+            Self::SlotMismatch
+                | Self::InvalidShredVersion(_)
+                | Self::InvalidSignature
+                | Self::ShredTypeMismatch
+                | Self::InvalidDuplicateShreds
+                | Self::InvalidLastIndexConflict
+                | Self::InvalidErasureMetaConflict
+        )
+    }
+}
+
 /// Check that `shred1` and `shred2` indicate a valid duplicate proof
 ///     - Must be for the same slot
 ///     - Must match the expected shred version
