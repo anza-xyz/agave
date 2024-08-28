8c8891d03407ea1632fe39c7cc5f338a8b60d9e1
WillHickey <will.hickey@anza.xyz>
2024-08-28 10:00:35 -0500

    Remove echos to minimize the risk of leaking sensitive information

--- .github/scripts/add-team-to-ghsa.sh
+++ .github/scripts/add-team-to-ghsa.sh
@@ -5,8 +5,6 @@ team_to_add_slug="security-incident-response"
 github_org="anza-xyz"
 github_repo="agave"
 
-echo "Adding $team_to_add_slug"
-
 # Note: This will get all the GHSAs even if there are more than the per_page value
 # from gh api --help
 # --paginate    Make additional HTTP requests to fetch all pages of results
@@ -17,17 +15,14 @@ ghsa_json=$(gh api \
 
 # Get a list of GHSAs that don't have the $team_to_add_slug in collaborating_teams
 ghsa_without_team=$( jq -r '[ .[] | select(all(.collaborating_teams.[]; .slug != "'$team_to_add_slug'")) | .ghsa_id ] | sort | .[] ' <<< "$ghsa_json" )
-echo "ghsa_without_team: $ghsa_without_team"
 
 # Iterate through the teams
 while IFS= read -r ghsa_id; do
-    echo "ghsa_id: $ghsa_id"
     # PATCH updates the value. If we just set -f "collaborating_teams[]=$team_to_add_slug" it
     # will overwrite any existing collaborating_teams. So we get the list of teams that are already
     # added to this GHSA and format them as parameters for gh api like:
     #    -f collaborating_teams[]=ghsa-testing-1
     original_collaborating_team_slugs=$( jq -r '[ .[] | select(.ghsa_id == "'$ghsa_id'") | .collaborating_teams ] | "-f collaborating_teams[]=" + .[][].slug ' <<< "$ghsa_json" )
-    echo "original_collaborating_team_slugs: $original_collaborating_team_slugs"
     
     # Update the team list
     gh api \
