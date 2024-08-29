34935f21347cff3de44976643a94d2971fc5a48f
WillHickey <will.hickey@anza.xyz>
2024-08-29 11:02:19 -0500

    shellcheck

--- .github/scripts/add-team-to-ghsa.sh
+++ .github/scripts/add-team-to-ghsa.sh
@@ -14,7 +14,7 @@ ghsa_json=$(gh api \
     /repos/$github_org/$github_repo/security-advisories?per_page=100 --paginate )
 
 # Get a list of GHSAs that don't have the $team_to_add_slug in collaborating_teams
-ghsa_without_team=$( jq -r '[ .[] | select(all(.collaborating_teams.[]; .slug != "'$team_to_add_slug'")) | .ghsa_id ] | sort | .[] ' <<< "$ghsa_json" )
+ghsa_without_team=$( jq -r '[ .[] | select(all(.collaborating_teams.[]; .slug != "'"$team_to_add_slug"'")) | .ghsa_id ] | sort | .[] ' <<< "$ghsa_json" )
 
 # Iterate through the teams
 while IFS= read -r ghsa_id; do
@@ -22,7 +22,7 @@ while IFS= read -r ghsa_id; do
     # will overwrite any existing collaborating_teams. So we get the list of teams that are already
     # added to this GHSA and format them as parameters for gh api like:
     #    -f collaborating_teams[]=ghsa-testing-1
-    original_collaborating_team_slugs=$( jq -r '[ .[] | select(.ghsa_id == "'$ghsa_id'") | .collaborating_teams ] | "-f collaborating_teams[]=" + .[][].slug ' <<< "$ghsa_json" )
+    original_collaborating_team_slugs=$( jq -r '[ .[] | select(.ghsa_id == "'"$ghsa_id"'") | .collaborating_teams ] | "-f collaborating_teams[]=" + .[][].slug ' <<< "$ghsa_json" )
 
     # Update the team list
     gh api \
@@ -30,6 +30,6 @@ while IFS= read -r ghsa_id; do
         -H "Accept: application/vnd.github+json" \
         -H "X-GitHub-Api-Version: 2022-11-28" \
         "/repos/$github_org/$github_repo/security-advisories/$ghsa_id" \
-        -f "collaborating_teams[]=$team_to_add_slug" $original_collaborating_team_slugs \
+        -f "collaborating_teams[]=$team_to_add_slug" "$original_collaborating_team_slugs" \
         > /dev/null 2>&1
 done <<< "$ghsa_without_team"
