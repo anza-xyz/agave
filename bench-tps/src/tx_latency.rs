39a11a970b52129be42c0509321fe623a6df092c
Artem Vitae <15745003+artemii235@users.noreply.github.com>
2024-10-16 11:29:24 +0500

    Remove WS measurement as it doesn't work as expected.

--- bench-tps/src/tx_latency.rs
+++ bench-tps/src/tx_latency.rs
@@ -7,20 +7,14 @@ use {
     solana_transaction_status::TransactionConfirmationStatus,
     std::{
         sync::Arc,
-        thread::{sleep, Builder, JoinHandle},
+        thread::{Builder, JoinHandle, sleep},
         time::{Duration, Instant},
     },
 };
 
-pub(crate) enum HttpOrWs {
-    Http,
-    Ws,
-}
-
-pub(crate) fn measure_tx_latency<T: TpsClient + Send + Sync + ?Sized + 'static>(
+pub(crate) fn measure_tx_latency_thread<T: TpsClient + Send + Sync + ?Sized + 'static>(
     receiver: Receiver<(Signature, Instant)>,
-    http_or_ws: HttpOrWs,
-    http_sleep_ms: u64,
+    latency_sleep_ms: u64,
     client: Arc<T>,
 ) -> JoinHandle<()> {
     Builder::new()
@@ -30,51 +24,44 @@ pub(crate) fn measure_tx_latency<T: TpsClient + Send + Sync + ?Sized + 'static>(
                 Ok(s) => s,
                 Err(_) => break,
             };
-            match http_or_ws {
-                HttpOrWs::Http => {
-                    let mut current_status = None;
+            let mut current_status = None;
 
-                    loop {
-                        let mut statuses = match client.get_signature_statuses(&[signature]) {
-                            Ok(statuses) => statuses,
-                            Err(e) => {
-                                error!("Failed to get status of signature {signature}: {e}");
-                                sleep(Duration::from_millis(http_sleep_ms));
-                                continue;
-                            }
-                        };
+            loop {
+                let mut statuses = match client.get_signature_statuses(&[signature]) {
+                    Ok(statuses) => statuses,
+                    Err(e) => {
+                        error!("Failed to get status of signature {signature}: {e}");
+                        sleep(Duration::from_millis(latency_sleep_ms));
+                        continue;
+                    }
+                };
 
-                        match (
-                            current_status,
-                            statuses.remove(0).map(|status| status.confirmation_status),
-                        ) {
-                            (None, Some(Some(TransactionConfirmationStatus::Confirmed))) => {
-                                current_status = Some(TransactionConfirmationStatus::Confirmed);
-                                datapoint_info!(
-                                    "ext-tx-latency-confirmed",
-                                    ("signature", signature.to_string(), String),
-                                    ("latency", sent_at.elapsed().as_millis(), i64)
-                                );
-                            }
-                            (_, Some(Some(TransactionConfirmationStatus::Finalized))) => {
-                                current_status = Some(TransactionConfirmationStatus::Finalized);
-                                datapoint_info!(
-                                    "ext-tx-latency-finalized",
-                                    ("signature", signature.to_string(), String),
-                                    ("latency", sent_at.elapsed().as_millis(), i64)
-                                );
-                                break;
-                            }
-                            (_, _) => {
-                                // do nothing here
-                            }
-                        }
-                        sleep(Duration::from_millis(http_sleep_ms));
+                match (
+                    current_status,
+                    statuses.remove(0).map(|status| status.confirmation_status),
+                ) {
+                    (None, Some(Some(TransactionConfirmationStatus::Confirmed))) => {
+                        current_status = Some(TransactionConfirmationStatus::Confirmed);
+                        datapoint_info!(
+                            "ext-tx-latency-confirmed",
+                            ("signature", signature.to_string(), String),
+                            ("latency", sent_at.elapsed().as_millis(), i64)
+                        );
+                    }
+                    (_, Some(Some(TransactionConfirmationStatus::Finalized))) => {
+                        datapoint_info!(
+                            "ext-tx-latency-finalized",
+                            ("signature", signature.to_string(), String),
+                            ("latency", sent_at.elapsed().as_millis(), i64)
+                        );
+                        break;
+                    }
+                    (_, _) => {
+                        // do nothing here
                     }
                 }
-                HttpOrWs::Ws => {}
+                sleep(Duration::from_millis(latency_sleep_ms));
             }
-            println!("Got transaction signature: {}", signature);
         })
         .expect("measureTxLatency should have started successfully.")
 }
