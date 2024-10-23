c78e72d7d0390b61805203a18fd63fc42a5bdfcb
brooks <brooks@anza.xyz>
2024-10-23 18:41:45 -0400

    Revert "Advises kernel to use random access for disk bucket mmaps (#2140)"
    This reverts commit 72963e1469031fbc72d3d87a1f5e568e98f734ce.

--- bucket_map/src/bucket_storage.rs
+++ bucket_map/src/bucket_storage.rs
@@ -433,10 +433,6 @@ impl<O: BucketOccupied> BucketStorage<O> {
                 std::env::current_dir(),
             );
         });
-        // Access to the disk bucket files are random (excluding the linear search on collisions),
-        // so advise the kernel to treat the mmaps as such.
-        #[cfg(unix)]
-        mmap.advise(memmap2::Advice::Random).unwrap();
         measure_mmap.stop();
         stats
             .new_file_us
