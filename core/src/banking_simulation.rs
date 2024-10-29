703828ac2d35e20e8586fdc08187871fc6836e71
Ryo Onodera <ryoqun@gmail.com>
2024-10-29 14:27:06 +0900

    save

--- core/src/banking_simulation.rs
+++ core/src/banking_simulation.rs
@@ -935,7 +935,7 @@ impl BankingSimulator {
         blockstore: Arc<Blockstore>,
         block_production_method: BlockProductionMethod,
         unified_scheduler_pool: Option<Arc<DefaultSchedulerPool>>,
-        poh_recorder: Option<PohRecorder>,
+        new_poh_recorder: Option<NewPohRecorder>,
     ) -> Result<(), SimulateError> {
         let (sender_loop, simulator_loop, simulator_threads) = self.prepare_simulation(
             genesis_config,
@@ -943,7 +943,7 @@ impl BankingSimulator {
             blockstore,
             block_production_method,
             unified_scheduler_pool,
-            poh_recorder,
+            new_poh_recorder,
         );
 
         sender_loop.log_starting();
