88107aeacfae8bbc3d68d7f4b302a20839983fe1
Ryo Onodera <ryoqun@gmail.com>
2024-10-30 15:07:06 +0900

    save

--- core/src/validator.rs
+++ core/src/validator.rs
@@ -142,6 +142,7 @@ use {
     thiserror::Error,
     tokio::runtime::Runtime as TokioRuntime,
 };
+use solana_unified_scheduler_pool::SupportedSchedulingMode;
 
 const MAX_COMPLETED_DATA_SETS_IN_CHANNEL: usize = 100_000;
 const WAIT_FOR_SUPERMAJORITY_THRESHOLD_PERCENT: u64 = 80;
