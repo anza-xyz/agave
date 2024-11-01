39cd11e0ca7c2b67e0df99c60ba088466c6bd71b
Kirill Lykov <kirill.lykov@anza.xyz>
2024-11-01 15:30:54 +0100

    add spawn_tpu_client_send_txs

--- send-transaction-service/src/transaction_client.rs
+++ send-transaction-service/src/transaction_client.rs
@@ -1,14 +1,25 @@
 use {
     crate::{send_transaction_service_stats::SendTransactionServiceStats, tpu_info::TpuInfo},
+    async_trait::async_trait,
     log::warn,
-    solana_client::connection_cache::ConnectionCache,
+    solana_client::connection_cache::{ConnectionCache, Protocol},
     solana_connection_cache::client_connection::ClientConnection as TpuConnection,
     solana_measure::measure::Measure,
+    solana_tpu_client_next::{
+        connection_workers_scheduler::ConnectionWorkersSchedulerConfig,
+        leader_updater::LeaderUpdater, transaction_batch::TransactionBatch,
+        ConnectionWorkersScheduler,
+    },
     std::{
-        net::SocketAddr,
-        sync::{atomic::Ordering, Arc, Mutex},
+        net::{Ipv4Addr, SocketAddr},
+        sync::{atomic::Ordering, Arc, Mutex, RwLock},
         time::{Duration, Instant},
     },
+    tokio::{
+        runtime::Runtime,
+        sync::mpsc::{self, Sender},
+    },
+    tokio_util::sync::CancellationToken,
 };
 
 pub trait TransactionClient {
@@ -125,6 +136,101 @@ where
             self.send_transactions(address, wire_transactions.clone(), stats);
         }
     }
+}
+
+pub struct SendTransactionServiceLeaderUpdater<T: TpuInfo + std::marker::Send + 'static> {
+    leader_info_provider: CurrentLeaderInfo<T>,
+    my_tpu_address: SocketAddr,
+    tpu_peers: Option<Vec<SocketAddr>>,
+}
+
+//TODO(klykov): it is should not be async trait because we don't need here it to be stoppable
+#[async_trait]
+impl<T> LeaderUpdater for SendTransactionServiceLeaderUpdater<T>
+where
+    T: TpuInfo + std::marker::Send + 'static,
+{
+    //TODO(klykov): So each call will lead to shit tons of allocations, I think
+    //we should recalculate only once a slot or something?
+    fn next_leaders(&mut self, lookahead_slots: u64) -> Vec<SocketAddr> {
+        // it is &mut because it is not a service! so it needs to update the state
+        // so i had to change the interface
+        let discovered_peers = self
+            .leader_info_provider
+            .get_leader_info()
+            .map(|leader_info| leader_info.get_leader_tpus(lookahead_slots / 4, Protocol::QUIC))
+            .filter(|addresses| !addresses.is_empty())
+            .unwrap_or_else(|| vec![&self.my_tpu_address]);
+        let mut all_peers = self.tpu_peers.clone().unwrap_or_default();
+        all_peers.extend(discovered_peers.into_iter().cloned());
+        all_peers
+    }
+    async fn stop(&mut self) {}
+}
+
+struct TpuClientNextClient {
+    sender: Sender<TransactionBatch>,
+}
+
+impl TransactionClient for TpuClientNextClient {
+    fn send_transactions_in_batch(
+        &self,
+        wire_transactions: Vec<Vec<u8>>,
+        stats: &SendTransactionServiceStats,
+    ) {
+        let res = self
+            .sender
+            .try_send(TransactionBatch::new(wire_transactions));
+        match res {
+            Ok(_) => {
+                stats.send_attempt_count.fetch_add(1, Ordering::Relaxed);
+            }
+            Err(_) => {
+                warn!("Failed to send transaction transaction, transaction chanel is full.");
+                stats.send_failure_count.fetch_add(1, Ordering::Relaxed);
+            }
+        }
+    }
+}
+
+pub(crate) fn spawn_tpu_client_send_txs<T>(
+    runtime: Runtime,
+    my_tpu_address: SocketAddr,
+    tpu_peers: Option<Vec<SocketAddr>>,
+    leader_info: Option<T>,
+    leader_forward_count: u64,
+) -> Sender<TransactionBatch>
+where
+    T: TpuInfo + std::marker::Send + 'static,
+{
+    let leader_info_provider = CurrentLeaderInfo::new(leader_info);
+
+    let (transaction_sender, transaction_receiver) = mpsc::channel(16); // random number of now
+    let validator_identity = None;
+    runtime.spawn(async move {
+        let cancel = CancellationToken::new();
+        let leader_updater = SendTransactionServiceLeaderUpdater {
+            leader_info_provider,
+            my_tpu_address,
+            tpu_peers,
+        };
+        let config = ConnectionWorkersSchedulerConfig {
+            bind: SocketAddr::new(Ipv4Addr::new(0, 0, 0, 0).into(), 0),
+            stake_identity: validator_identity, //TODO In CC, do we send with identity?
+            num_connections: 1,
+            skip_check_transaction_age: true,
+            worker_channel_size: 2,
+            max_reconnect_attempts: 4,
+            lookahead_slots: leader_forward_count,
+        };
+        let _scheduler = tokio::spawn(ConnectionWorkersScheduler::run(
+            config,
+            Box::new(leader_updater),
+            transaction_receiver,
+            cancel.clone(),
+        ));
+    });
+    transaction_sender
 }
 
 /// The leader info refresh rate.
