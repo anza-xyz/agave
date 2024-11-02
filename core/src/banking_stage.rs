dfd27e98c55aca96fa3e41f4ac509cb635d734e0
Ryo Onodera <ryoqun@gmail.com>
2024-11-02 21:48:59 +0900

    save

--- core/src/banking_stage.rs
+++ core/src/banking_stage.rs
@@ -705,12 +705,12 @@ impl BankingStage {
                 self.next_task_id.fetch_add(count, Ordering::AcqRel)
             }
         }
-        info!("create_block_producing_scheduler: start!");
-        //let adapter = unified_scheduler_pool.banking_stage_adapter();
-        let decision_maker2 = DecisionMaker::new(cluster_info.id(), poh_recorder.clone());
+        let decision_maker = DecisionMaker::new(cluster_info.id(), poh_recorder.clone());
+
         unified_scheduler_pool.spawn_block_production_scheduler2(bank_forks.clone(), non_vote_receiver,
             Box::new(move |adapter: Arc<BankingStageAdapter>| {
-                let decision_maker = decision_maker2.clone();
+                info!("create_block_producing_scheduler: start!");
+                let decision_maker = decision_maker.clone();
                 let bank_forks = bank_forks.clone();
                 let mut id_generator = MonotonicIdGenerator::new();
 
@@ -785,9 +785,9 @@ impl BankingStage {
                     }
                     tasks
                 })
+                info!("create_block_producing_scheduler: end!");
             })
         );
-        info!("create_block_producing_scheduler: end!");
 
         Self { bank_thread_hdls: vec![] }
     }
