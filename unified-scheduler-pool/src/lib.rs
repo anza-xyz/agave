c1ed6088ead2521481c6fb9146a6c295a5a6ca20
Ryo Onodera <ryoqun@gmail.com>
2024-11-02 14:42:19 +0900

    save

--- unified-scheduler-pool/src/lib.rs
+++ unified-scheduler-pool/src/lib.rs
@@ -437,6 +437,23 @@ where
     where
         F: FnMut(BankingPacketBatch) -> Vec<Task> + Clone + Send + 'static
     {
+        info!("flash session: start!");
+        let banking_stage_context = Some((recv, on_banking_packet_receive));
+        let scheduler = {
+            let mut g = self.block_production_scheduler_inner.lock().expect("not poisoned");
+            let context = g.2.take().inspect(|context| {
+                assert_matches!(context.mode(), SchedulingMode::BlockProduction);
+            }).unwrap_or_else(|| {
+                SchedulingContext::new(SchedulingMode::BlockProduction, bank_forks.read().unwrap().root_bank())
+            });
+            let s = S::spawn(self.self_arc(), context, initialized_result_with_timings(), banking_stage_context);
+            assert!(g.0.replace(s.id()).is_none());
+            s
+        };
+        let id = scheduler.id();
+        self.return_scheduler(scheduler.into_inner().1, id, false);
+        self.block_production_scheduler_condvar.notify_all();
+        info!("flash session: end!");
     }
 
     pub fn spawn_block_production_scheduler(&self, bank_forks: &RwLock<BankForks>, recv: BankingPacketReceiver, on_banking_packet_receive: impl FnMut(BankingPacketBatch) -> Vec<Task> + Clone + Send + 'static) {
