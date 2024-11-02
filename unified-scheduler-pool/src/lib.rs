86219019da55fd857887ca74e132c09755cc3676
Ryo Onodera <ryoqun@gmail.com>
2024-11-02 14:42:49 +0900

    save

--- unified-scheduler-pool/src/lib.rs
+++ unified-scheduler-pool/src/lib.rs
@@ -438,6 +438,7 @@ where
         F: FnMut(BankingPacketBatch) -> Vec<Task> + Clone + Send + 'static
     {
         info!("flash session: start!");
+        let on_banking_packet_receive = on_on_banking_packet_receive(self.banking_stage_adapter());
         let banking_stage_context = Some((recv, on_banking_packet_receive));
         let scheduler = {
             let mut g = self.block_production_scheduler_inner.lock().expect("not poisoned");
