b415c19736c49543dee997aeff0ce72a9d90ff3e
Ryo Onodera <ryoqun@gmail.com>
2024-11-02 21:56:54 +0900

    save

--- unified-scheduler-pool/src/lib.rs
+++ unified-scheduler-pool/src/lib.rs
@@ -466,7 +466,7 @@ where
 
     pub fn spawn_block_production_scheduler2(&self, bank_forks: Arc<RwLock<BankForks>>, banking_packet_receiver: BankingPacketReceiver, mut on_spawn_block_production_scheduler: BBB) {
         info!("flash session: start!");
-        let bbbl = self.bbb.lock().unwrap();
+        let mut bbbl = self.bbb.lock().unwrap();
         *bbbl = Some(BlockProductionSchedulerRespawner {
             bank_forks,
             banking_packet_receiver,
