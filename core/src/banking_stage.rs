ad7fbb5b05e5f5b5b32d12103671ba819fa5e35f
Ryo Onodera <ryoqun@gmail.com>
2024-11-03 15:33:54 +0900

    save

--- core/src/banking_stage.rs
+++ core/src/banking_stage.rs
@@ -719,7 +719,7 @@ impl BankingStage {
         }
 
         impl BankingStageMonitor for S {
-            fn banking_stage_stutus(&self) -> BankingStageStatus {
+            fn banking_stage_status(&self) -> BankingStageStatus {
                 let r = if self.1.load(Ordering::Relaxed) {
                     BankingStageStatus::Exited
                 } else if matches!(self.0.make_consume_or_forward_decision(), BufferedPacketsDecision::Forward) {
