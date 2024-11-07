b7e7f8598590cfdcce4b06b927ca966d39c24f9e
Ryo Onodera <ryoqun@gmail.com>
2024-11-07 11:43:39 +0900

    save

--- unified-scheduler-pool/src/lib.rs
+++ unified-scheduler-pool/src/lib.rs
@@ -1726,6 +1726,11 @@ impl<S: SpawnableScheduler<TH>, TH: TaskHandler> ThreadManager<S, TH> {
                                 std::mem::forget(task);
                             }
                             log_scheduler!(info, "drained");
+                            session_started_at = Instant::now();
+                            cpu_session_started_at = cpu_time::ThreadTime::now();
+                            reported_task_total = 0;
+                            reported_executed_task_total = 0;
+                            error_count = 0;
                             session_resetting = false;
                         }
                         match new_task_receiver.recv().map(|a| a.into()) {
