b2659dbc56ca47c6e63fc77b5470b46d61d9b87b
ksolana <110843012+ksolana@users.noreply.github.com>
2024-11-07 07:11:18 -0800

    Adding stfu: a multi producer single consumer ring buffer
    Written by Andrew.
    Added a couple of tests as a first step to integrate with poh

---
+++ poh/stfu/README.md
@@ -0,0 +1,41 @@
+# stfu ![example workflow](https://github.com/apfitzge/stfu/actions/workflows/rust.yml/badge.svg)
+
+A dead simple MPSC fixed-size channel for Rust with the ability for the consumer to shut off producers.
+
+## Usage
+
+```rust
+// Create channel ends with a fixed capacity of 4.
+let (tx, rx) = stfu::with_capacity(4);
+
+let _tx2 = tx.clone(); // producer may be cloned
+
+// let rx2 = rx.clone(); // consumer may not be cloned
+
+// Pushing to the channel can fail for various reasons.
+assert!(tx.try_push(1).is_ok());
+assert!(tx.try_push(2).is_ok());
+assert!(tx.try_push(3).is_ok());
+assert!(tx.try_push(4).is_ok());
+// The channel is full. The push fails and the value is returned.
+assert_eq!(tx.try_push(5), Err(5));
+
+// The consumer can pop from the channel.
+assert_eq!(rx.pop(), Some(1));
+assert_eq!(rx.pop(), Some(2));
+
+// At this point the producer could push more items in.
+// But the consumer can prevent the producers from pushing
+// more items by shutting them off.
+rx.shut_off_producers(); // stfu!
+
+// The producer can no longer push to the channel.
+assert_eq!(tx.try_push(6), Err(6));
+
+// The consumer can still pop from the channel.
+assert_eq!(rx.pop(), Some(3));
+
+// And when producers are re-enabled, they can push again.
+rx.enable_producers();
+assert!(tx.try_push(6).is_ok());
+```
---
+++ poh/stfu/src/lib.rs
@@ -0,0 +1,55 @@
+mod consumer;
+mod producer;
+mod ring_buffer;
+
+pub use {consumer::Consumer, producer::Producer};
+
+pub fn with_capacity<T>(capacity: u32) -> (Producer<T>, Consumer<T>) {
+    let ring_buffer = std::sync::Arc::new(ring_buffer::RingBuffer::with_capacity(capacity));
+    let producer = Producer {
+        ring_buffer: ring_buffer.clone(),
+    };
+    let consumer = Consumer { ring_buffer };
+
+    (producer, consumer)
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn example() {
+        // Create channel ends with a fixed capacity of 4.
+        let (tx, rx) = crate::with_capacity(4);
+
+        let _tx2 = tx.clone(); // producer may be cloned
+
+        // let rx2 = rx.clone(); // consumer may not be cloned
+
+        // Pushing to the channel can fail for various reasons.
+        assert!(tx.try_push(1).is_ok());
+        assert!(tx.try_push(2).is_ok());
+        assert!(tx.try_push(3).is_ok());
+        assert!(tx.try_push(4).is_ok());
+        // The channel is full. The push fails and the value is returned.
+        assert_eq!(tx.try_push(5), Err(5));
+
+        // The consumer can pop from the channel.
+        assert_eq!(rx.pop(), Some(1));
+        assert_eq!(rx.pop(), Some(2));
+
+        // At this point the producer could push more items in.
+        // But the consumer can prevent the producers from pushing
+        // more items by shutting them off.
+        rx.shut_off_producers(); // stfu!
+
+        // The producer can no longer push to the channel.
+        assert_eq!(tx.try_push(6), Err(6));
+
+        // The consumer can still pop from the channel.
+        assert_eq!(rx.pop(), Some(3));
+
+        // And when producers are re-enabled, they can push again.
+        rx.enable_producers();
+        assert!(tx.try_push(6).is_ok());
+    }
+}
