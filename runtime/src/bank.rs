buffalojoec/solana

SVM: use lps from `FeeStructure` directly

runtime/src/bank.rs
@@ -3677,7 +3677,7 @@ impl Bank {
             epoch_total_stake: self.epoch_total_stake(self.epoch()),
             epoch_vote_accounts: self.epoch_vote_accounts(self.epoch()),
             feature_set: Arc::clone(&self.feature_set),
-            fee_structure: Some(&self.fee_structure),
+            fee_per_signature: self.fee_structure.lamports_per_signature,
             lamports_per_signature,
             rent_collector: Some(&rent_collector_with_metrics),
         };

svm/examples/json-rpc/server/src/rpc_process.rs
@@ -552,7 +552,7 @@ impl JsonRpcRequestProcessor {
             epoch_total_stake: self.epoch_total_stake(Epoch::default()),
             epoch_vote_accounts: self.epoch_vote_accounts(Epoch::default()),
             feature_set: Arc::clone(&bank.feature_set),
-            fee_structure: None,
+            fee_per_signature: lamports_per_signature,
             lamports_per_signature,
             rent_collector: None,
         };

svm/examples/paytube/src/lib.rs
@@ -151,7 +151,7 @@ impl PayTubeChannel {
             epoch_total_stake: None,
             epoch_vote_accounts: None,
             feature_set: Arc::new(feature_set),
-            fee_structure: Some(&fee_structure),
+            fee_per_signature: fee_structure.lamports_per_signature,
             lamports_per_signature,
             rent_collector: Some(&rent_collector),
         };

anza-xyz/agave

runtime/src/bank.rs
@@ -3716,7 +3716,7 @@ impl Bank {
             epoch_total_stake: Some(self.get_current_epoch_total_stake()),
             epoch_vote_accounts: Some(self.get_current_epoch_vote_accounts()),
             feature_set: Arc::clone(&self.feature_set),
-            fee_structure: Some(&self.fee_structure),
+            fee_lamports_per_signature: self.fee_structure.lamports_per_signature,
             lamports_per_signature,
             rent_collector: Some(&rent_collector_with_metrics),
         };

svm/examples/json-rpc/server/src/rpc_process.rs
@@ -549,7 +549,7 @@ impl JsonRpcRequestProcessor {
             epoch_total_stake: self.epoch_total_stake(Epoch::default()),
             epoch_vote_accounts: self.epoch_vote_accounts(Epoch::default()),
             feature_set: Arc::clone(&bank.feature_set),
-            fee_structure: None,
+            fee_lamports_per_signature: lamports_per_signature,
             lamports_per_signature,
             rent_collector: None,
         };

svm/examples/paytube/src/lib.rs
@@ -151,7 +151,7 @@ impl PayTubeChannel {
             epoch_total_stake: None,
             epoch_vote_accounts: None,
             feature_set: Arc::new(feature_set),
-            fee_structure: Some(&fee_structure),
+            fee_lamports_per_signature: fee_structure.lamports_per_signature,
             lamports_per_signature,
             rent_collector: Some(&rent_collector),
         };