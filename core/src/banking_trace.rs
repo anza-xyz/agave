b5bd70ca5d25c47c87c6f3ad057f6e524b4b4f81
Ryo Onodera <ryoqun@gmail.com>
2024-11-11 23:55:59 +0900

    save

--- core/src/banking_trace.rs
+++ core/src/banking_trace.rs
@@ -65,7 +65,7 @@ pub struct BankingTracer {
 #[cfg_attr(
     feature = "frozen-abi",
     derive(AbiExample),
-    frozen_abi(digest = "F5GH1poHbPqipU4DB3MczhSxHZw4o27f3C7QnMVirFci")
+    frozen_abi(digest = "BbAPnbnWp6FqLm5Nuyw8rYZcFCrg8DFd7kFBUxe3axcp")
 )]
 #[derive(Serialize, Deserialize, Debug)]
 pub struct TimedTracedEvent(pub std::time::SystemTime, pub TracedEvent);
