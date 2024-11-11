cbe03eb5733389d184bcc752e8dc8adf453257f4
Ryo Onodera <ryoqun@gmail.com>
2024-11-11 22:31:56 +0900

    save

--- ledger/src/blockstore_processor.rs
+++ ledger/src/blockstore_processor.rs
@@ -437,7 +437,7 @@ fn schedule_batches_for_execution(
     {
         let transaction_indexes2 = transaction_indexes
             .iter()
-            .map(|&i| i as solana_runtime::installed_scheduler_pool::Index)
+            .map(|&i| i as TaskKey)
             .collect::<Vec<_>>();
         bank.schedule_transaction_executions(
             batch
