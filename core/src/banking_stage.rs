7af5eb846997677a462a01771e421c865d5ec00d
Ryo Onodera <ryoqun@gmail.com>
2024-11-11 21:50:19 +0900

    save

--- core/src/banking_stage.rs
+++ core/src/banking_stage.rs
@@ -710,8 +710,13 @@ impl BankingStage {
         }
         let decision_maker = DecisionMaker::new(cluster_info.id(), poh_recorder.clone());
 
-        #[derive(Debug)]
         struct S(DecisionMaker, Arc<AtomicBool>);
+
+        impl std::fmt::Debug for S {
+            fn fmt(&self, _f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                todo!();
+            }
+        }
 
         impl BankingStageMonitor for S {
             fn banking_stage_status(&self) -> BankingStageStatus {
