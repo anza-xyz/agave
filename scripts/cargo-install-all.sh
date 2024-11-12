02e34af5f7a4cf7b0048d1f790b8d2e12a08066f
yihau <yihau.chen@icloud.com>
2024-11-12 12:58:24 +0800

    use anza-xyz/ntapi instead of the labs one

--- scripts/cargo-install-all.sh
+++ scripts/cargo-install-all.sh
@@ -204,7 +204,7 @@ check_dcou() {
     # the patch-related configs are needed for rust 1.69+ on Windows; see Cargo.toml
     # shellcheck disable=SC2086 # Don't want to double quote $rust_version
     "$cargo" $maybeRustVersion \
-      --config 'patch.crates-io.ntapi.git="https://github.com/solana-labs/ntapi"' \
+      --config 'patch.crates-io.ntapi.git="https://github.com/anza-xyz/ntapi"' \
       --config 'patch.crates-io.ntapi.rev="97ede981a1777883ff86d142b75024b023f04fad"' \
       install --locked spl-token-cli --root "$installDir" $maybeSplTokenCliVersionArg
   fi
