57e9ca4905042c5cb2ab3fdd5f4ff44f599f7520
Ryo Onodera <ryoqun@gmail.com>
2024-11-13 13:35:26 +0000

    save

--- unified-scheduler-pool/src/lib.rs
+++ unified-scheduler-pool/src/lib.rs
@@ -2339,7 +2339,7 @@ impl<TH: TaskHandler> SpawnableScheduler<TH> for PooledScheduler<TH> {
                 usage_queue_loader: UsageQueueLoader::default(),
             },
             SchedulingMode::BlockProduction => TaskCreator::BlockProduction {
-                banking_stage_adapter: banking_stage_adapter.unwrap(),
+                banking_stage_adapter: banking_stage_adapter.clone().unwrap(),
             },
         };
         let mut inner = Self::Inner {
