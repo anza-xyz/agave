b5b7424f3183e2ad77ce3dd1aa8916bd005451ee
brooks <brooks@anza.xyz>
2024-11-15 11:46:51 -0500

    fix backport merge conflict

--- ledger/src/bank_forks_utils.rs
+++ ledger/src/bank_forks_utils.rs
@@ -336,27 +336,6 @@ fn bank_forks_from_snapshot(
         bank
     };
 
-<<<<<<< HEAD
-=======
-    // We must inform accounts-db of the latest full snapshot slot, which is used by the background
-    // processes to handle zero lamport accounts.  Since we've now successfully loaded the bank
-    // from snapshots, this is a good time to do that update.
-    // Note, this must only be set if we should generate snapshots.
-    if snapshot_config.should_generate_snapshots() {
-        bank.rc
-            .accounts
-            .accounts_db
-            .set_latest_full_snapshot_slot(full_snapshot_archive_info.slot());
-    } else {
-        assert!(bank
-            .rc
-            .accounts
-            .accounts_db
-            .latest_full_snapshot_slot()
-            .is_none());
-    }
-
->>>>>>> 4ea10c224a (Startup only sets latest_full_snapshot_slot if generating snapshots (#3633))
     let full_snapshot_hash = FullSnapshotHash((
         full_snapshot_archive_info.slot(),
         *full_snapshot_archive_info.hash(),
