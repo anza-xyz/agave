f8f68b02c27d8f3c2a64e2899deb9100fe68a810
Ryo Onodera <ryoqun@gmail.com>
2024-11-22 13:51:50 +0900

    save

--- core/src/banking_stage.rs
+++ core/src/banking_stage.rs
@@ -743,9 +743,6 @@ impl BankingStage {
                         let indexes = PacketDeserializer::generate_packet_indexes(pp);
                         let ppp = PacketDeserializer::deserialize_packets2(pp, &indexes)
                             .filter_map(|(i, p)| {
-                                if p.original_packet().meta().is_tracer_packet() {
-                                    //warn!("pipeline_tracer: unified_scheduler submit receiver_len: {} {:?} {:?}", packet_deserializer.packet_batch_receiver.len(), std::thread::current(), std::backtrace::Backtrace::force_capture());
-                                }
                                 let (tx, _) = p.build_sanitized_transaction(
                                     bank.vote_only_bank(),
                                     &bank,
