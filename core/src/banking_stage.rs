6c4a96a4db4557b30c2144ddabe667674854f1e3
Ryo Onodera <ryoqun@gmail.com>
2024-11-22 22:28:46 +0900

    save

--- core/src/banking_stage.rs
+++ core/src/banking_stage.rs
@@ -715,7 +715,7 @@ impl BankingStage {
                 let decision_maker = decision_maker.clone();
                 let bank_forks = bank_forks.clone();
 
-                Box::new(move |batches: BankingPacketBatch| -> Box<dyn Iterator<Item = Task>> {
+                Box::new(|batches: BankingPacketBatch| -> Box<dyn Iterator<Item = Task>> {
                     let decision = decision_maker.make_consume_or_forward_decision();
                     let batches = if matches!(decision, BufferedPacketsDecision::Forward) {
                         &Vec::new()
