498c81dc9166e921c5f8c4130e1134e9554b9ed1
Joe Caulfield <joe.caulfield@anza.xyz>
2024-11-26 12:19:12 +0900

    bank: flatten core_bpf_migration module

--- runtime/src/bank/builtin_programs.rs
+++ runtime/src/bank/builtin_programs.rs
@@ -66,7 +66,7 @@ mod tests {
 mod tests_core_bpf_migration {
     use {
         crate::bank::{
-            builtins::core_bpf_migration::tests::TestContext,
+            core_bpf_migration::tests::TestContext,
             test_utils::goto_end_of_slot,
             tests::{create_genesis_config, new_bank_from_parent_with_bank_forks},
             Bank,
