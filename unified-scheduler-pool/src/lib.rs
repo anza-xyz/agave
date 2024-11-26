809f67fbc8ac593179432eec3ac178d8733f94b9
Ryo Onodera <ryoqun@gmail.com>
2024-11-26 15:19:57 +0900

    save

--- unified-scheduler-pool/src/lib.rs
+++ unified-scheduler-pool/src/lib.rs
@@ -2323,8 +2323,9 @@ impl BankingStageAdapter {
     fn recreate_task(&self, task: Task) -> Task {
         let transaction = task.transaction().clone();
         let old_index = task.index();
-        let new_index =
-            (old_index & ((u64::MAX as TaskKey) << 64)) | self.bulk_assign_task_ids(1) as TaskKey;
+        let original_shifted_priority = old_index & const { (u64::MAX as TaskKey) << TaskKey::BITS / 2 };
+        let new_task_id = self.bulk_assign_task_ids(1) as TaskKey
+        let new_index = original_shifted_priority | new_task_id;
 
         self.do_create_task(transaction, new_index)
     }
