b3224bb541d1e45ae35eddfdfe0c46228a0fcdb3
Ryo Onodera <ryoqun@gmail.com>
2024-11-26 22:36:20 +0900

    save

--- core/src/tpu.rs
+++ core/src/tpu.rs
@@ -157,6 +157,15 @@ impl Tpu {
             shared_staked_nodes_overrides,
         );
 
+        let Channels {
+            non_vote_sender,
+            non_vote_receiver,
+            tpu_vote_sender,
+            tpu_vote_receiver,
+            gossip_vote_sender,
+            gossip_vote_receiver,
+        } = banking_tracer.create_channels(unified_scheduler_pool.as_ref());
+
         let SpawnServerResult {
             endpoints: _,
             thread: tpu_quic_t,
@@ -200,15 +209,6 @@ impl Tpu {
             tpu_coalesce,
         )
         .unwrap();
-
-        let Channels {
-            non_vote_sender,
-            non_vote_receiver,
-            tpu_vote_sender,
-            tpu_vote_receiver,
-            gossip_vote_sender,
-            gossip_vote_receiver,
-        } = banking_tracer.create_channels(unified_scheduler_pool.as_ref());
 
         let sigverify_stage = {
             let verifier = TransactionSigVerifier::new(non_vote_sender);
