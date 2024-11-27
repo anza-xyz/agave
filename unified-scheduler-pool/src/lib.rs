70b021df6c9f7811e4cf5f2d07b85bb9f6bfab2c
Ryo Onodera <ryoqun@gmail.com>
2024-11-27 15:44:45 +0900

    save

--- unified-scheduler-pool/src/lib.rs
+++ unified-scheduler-pool/src/lib.rs
@@ -1914,7 +1914,8 @@ impl<S: SpawnableScheduler<TH>, TH: TaskHandler> ThreadManager<S, TH> {
         };
 
         let handler_main_loop = || {
-            let banking_packet_receiver = if let Some(b) = banking_stage_context.clone().as_ref() {
+            let banking_stage_context = banking_stage_context.clone();
+            let banking_packet_receiver = if let Some(b) = banking_stage_context.as_ref() {
                 b.banking_packet_receiver.clone()
             } else {
                 never()
