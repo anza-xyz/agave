f4c2f3cf951b597254f8816026b72119548026a5
Ryo Onodera <ryoqun@gmail.com>
2024-12-02 14:11:56 +0900

    save

--- ledger/src/blockstore_processor.rs
+++ ledger/src/blockstore_processor.rs
@@ -179,7 +179,10 @@ pub fn execute_batch(
         ExecutionRecordingConfig::new_single_setting(transaction_status_sender.is_some()),
         timings,
         log_messages_bytes_limit,
-        Some(|| true), //pre_commit_callback,
+        Some(|| {
+            pre_commit_callback();
+            true
+        })
     ) else {
         return Err(TransactionError::CommitFailed);
     };
