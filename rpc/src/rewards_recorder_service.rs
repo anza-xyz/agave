1bd69b2c1994e0d7996714692e886832d3c0a3a6
steviez <steven@anza.xyz>
2024-12-05 11:56:35 -0600

    Add start & stop logs

--- rpc/src/rewards_recorder_service.rs
+++ rpc/src/rewards_recorder_service.rs
@@ -32,15 +32,21 @@ impl RewardsRecorderService {
     ) -> Self {
         let thread_hdl = Builder::new()
             .name("solRewardsWritr".to_string())
-            .spawn(move || loop {
-                if exit.load(Ordering::Relaxed) {
-                    break;
+            .spawn(move || {
+                info!("RewardsRecorderService has started");
+                loop {
+                    if exit.load(Ordering::Relaxed) {
+                        break;
+                    }
+                    if let Err(RecvTimeoutError::Disconnected) = Self::write_rewards(
+                        &rewards_receiver,
+                        &max_complete_rewards_slot,
+                        &blockstore,
+                    ) {
+                        break;
+                    }
                 }
-                if let Err(RecvTimeoutError::Disconnected) =
-                    Self::write_rewards(&rewards_receiver, &max_complete_rewards_slot, &blockstore)
-                {
-                    break;
-                }
+                info!("RewardsRecorderService has stopped");
             })
             .unwrap();
         Self { thread_hdl }
