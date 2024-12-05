718e2613ef91b18d9083ae75f79bb49018894170
Joe Caulfield <joe.caulfield@anza.xyz>
2024-12-05 15:03:52 +0900

    add instr bindings

---
+++ svm-fuzz-harness/src/instr/mod.rs
@@ -0,0 +1,6 @@
+//! Instruction module.
+
+pub mod account;
+pub mod context;
+pub mod effects;
+pub mod features;
--- svm-fuzz-harness/src/lib.rs
+++ svm-fuzz-harness/src/lib.rs
@@ -1,6 +1,7 @@
 //! Solana SVM fuzzing harness.
 
 pub mod error;
+pub mod instr;
 pub mod proto {
     include!(concat!(env!("OUT_DIR"), "/org.solana.sealevel.v1.rs"));
 }
