fdd2808af2491802687ce17324cc4f6ce9c1269f
Steven Luscher <steven.luscher@anza.xyz>
2024-12-06 18:52:17 +0000

    A benchmark suite for the `getFirstAvailableBlock` RPC call

--- accounts-cluster-bench/src/main.rs
+++ accounts-cluster-bench/src/main.rs
@@ -256,6 +256,7 @@ pub enum RpcBench {
     TokenAccountsByOwner,
     Supply,
     TokenAccountsByDelegate,
+    FirstAvailableBlock,
 }
 
 #[derive(Debug)]
@@ -268,6 +269,7 @@ impl FromStr for RpcBench {
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s {
+            "first-available-block" => Ok(RpcBench::FirstAvailableBlock),
             "slot" => Ok(RpcBench::Slot),
             "supply" => Ok(RpcBench::Supply),
             "multiple-accounts" => Ok(RpcBench::MultipleAccounts),
@@ -390,6 +392,25 @@ fn run_rpc_bench_loop(
             break;
         }
         match rpc_bench {
+            RpcBench::FirstAvailableBlock => {
+                let mut rpc_time = Measure::start("rpc-get-first-available-block");
+                match client.get_first_available_block() {
+                    Ok(_slot) => {
+                        rpc_time.stop();
+                        stats.success += 1;
+                        stats.total_success_time_us += rpc_time.as_us();
+                    }
+                    Err(e) => {
+                        rpc_time.stop();
+                        stats.total_errors_time_us += rpc_time.as_us();
+                        stats.errors += 1;
+                        if last_error.elapsed().as_secs() > 2 {
+                            info!("get_first_available_block error: {:?}", e);
+                            last_error = Instant::now();
+                        }
+                    }
+                }
+            }
             RpcBench::Slot => {
                 let mut rpc_time = Measure::start("rpc-get-slot");
                 match client.get_slot() {
