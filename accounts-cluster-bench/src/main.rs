42e6f23e5ff97e7ccb1f006ae45d4057ec9e3b83
Steven Luscher <steven.luscher@anza.xyz>
2024-12-06 20:20:39 +0000

    Track the signatures of transactions created by the bench runner

--- accounts-cluster-bench/src/main.rs
+++ accounts-cluster-bench/src/main.rs
@@ -8,7 +8,9 @@ use {
         hidden_unless_forced, input_parsers::pubkey_of, input_validators::is_url_or_moniker,
     },
     solana_cli_config::{ConfigInput, CONFIG_FILE},
-    solana_client::transaction_executor::TransactionExecutor,
+    solana_client::{
+        rpc_client::SerializableTransaction, transaction_executor::TransactionExecutor,
+    },
     solana_gossip::gossip_service::discover,
     solana_inline_spl::token,
     solana_measure::measure::Measure,
@@ -21,7 +23,7 @@ use {
         message::Message,
         program_pack::Pack,
         pubkey::Pubkey,
-        signature::{read_keypair_file, Keypair, Signer},
+        signature::{read_keypair_file, Keypair, Signature, Signer},
         system_instruction, system_program,
         transaction::Transaction,
     },
@@ -29,11 +31,12 @@ use {
     spl_token::state::Account,
     std::{
         cmp::min,
+        collections::VecDeque,
         process::exit,
         str::FromStr,
         sync::{
             atomic::{AtomicBool, AtomicU64, Ordering},
-            Arc,
+            Arc, RwLock,
         },
         thread::{sleep, Builder, JoinHandle},
         time::{Duration, Instant},
@@ -130,6 +133,30 @@ fn airdrop_lamports(client: &RpcClient, id: &Keypair, desired_balance: u64) -> b
 struct SeedTracker {
     max_created: Arc<AtomicU64>,
     max_closed: Arc<AtomicU64>,
+}
+
+type TransactionSignatureTracker = Arc<RwLock<VecDeque<Signature>>>;
+trait TransactionSignatureTrackerImpl<T> {
+    fn get_random(&self) -> Option<T>;
+    fn track_transactions(&self, transactions: &[Transaction]);
+}
+impl TransactionSignatureTrackerImpl<Signature> for TransactionSignatureTracker {
+    fn get_random(&self) -> Option<Signature> {
+        let signatures = self.read().unwrap();
+        if signatures.is_empty() {
+            None
+        } else {
+            let random_index = thread_rng().gen_range(0..signatures.len());
+            let random_signature = signatures.get(random_index);
+            random_signature.cloned()
+        }
+    }
+    fn track_transactions(&self, transactions: &[Transaction]) {
+        let mut lock = self.write().unwrap();
+        for signature in transactions.iter().map(Transaction::get_signature) {
+            lock.push_back(*signature);
+        }
+    }
 }
 
 fn make_create_message(
@@ -339,6 +366,7 @@ struct RpcBenchStats {
     total_success_time_us: u64,
 }
 
+#[allow(clippy::too_many_arguments)]
 fn run_rpc_bench_loop(
     rpc_bench: RpcBench,
     thread: usize,
@@ -349,6 +377,7 @@ fn run_rpc_bench_loop(
     max_closed: &AtomicU64,
     max_created: &AtomicU64,
     mint: &Option<Pubkey>,
+    transaction_signature_tracker: &TransactionSignatureTracker,
 ) {
     let mut stats = RpcBenchStats::default();
     let mut iters = 0;
@@ -513,6 +542,7 @@ fn make_rpc_bench_threads(
     seed_tracker: &SeedTracker,
     base_keypair_pubkey: Pubkey,
     num_rpc_bench_threads: usize,
+    transaction_signature_tracker: &TransactionSignatureTracker,
 ) -> Vec<JoinHandle<()>> {
     let program_id = if mint.is_some() {
         token::id()
@@ -527,6 +557,7 @@ fn make_rpc_bench_threads(
                 let exit = exit.clone();
                 let max_closed = seed_tracker.max_closed.clone();
                 let max_created = seed_tracker.max_created.clone();
+                let signature_tracker = transaction_signature_tracker.clone();
                 let mint = *mint;
                 Builder::new()
                     .name(format!("rpc-bench-{}", thread))
@@ -541,6 +572,7 @@ fn make_rpc_bench_threads(
                             &max_closed,
                             &max_created,
                             &mint,
+                            &signature_tracker,
                         )
                     })
                     .unwrap()
@@ -594,6 +626,8 @@ fn run_accounts_bench(
         max_created: Arc::new(AtomicU64::default()),
         max_closed: Arc::new(AtomicU64::default()),
     };
+    let transaction_signature_tracker =
+        TransactionSignatureTracker::new(RwLock::new(VecDeque::with_capacity(5000)));
 
     info!("Starting balance(s): {:?}", balances);
 
@@ -627,6 +661,7 @@ fn run_accounts_bench(
             &seed_tracker,
             base_keypair_pubkey,
             num_rpc_bench_threads,
+            &transaction_signature_tracker,
         )
     } else {
         Vec::new()
@@ -689,6 +724,7 @@ fn run_accounts_bench(
                             .collect();
                         balances[i] = balances[i].saturating_sub(lamports * txs.len() as u64);
                         info!("txs: {}", txs.len());
+                        transaction_signature_tracker.track_transactions(&txs);
                         let new_ids = executor.push_transactions(txs);
                         info!("ids: {}", new_ids.len());
                         tx_sent_count += new_ids.len();
@@ -722,6 +758,7 @@ fn run_accounts_bench(
                         .collect();
                     balances[0] = balances[0].saturating_sub(fee * txs.len() as u64);
                     info!("close txs: {}", txs.len());
+                    transaction_signature_tracker.track_transactions(&txs);
                     let new_ids = executor.push_transactions(txs);
                     info!("close ids: {}", new_ids.len());
                     tx_sent_count += new_ids.len();
@@ -812,6 +849,7 @@ fn run_accounts_bench(
                                 .collect();
                             balances[i] = balances[i].saturating_sub(fee * txs.len() as u64);
                             info!("close txs: {}", txs.len());
+                            transaction_signature_tracker.track_transactions(&txs);
                             let new_ids = executor.push_transactions(txs);
                             info!("close ids: {}", new_ids.len());
                             tx_sent_count += new_ids.len();
