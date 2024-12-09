1580abcf34cd2608b3da857a1d60e5161bd6c27b
Pankaj Garg <pankaj@anza.xyz>
2024-12-09 07:40:49 -0800

    move clone to dev-context-only-utils

--- compute-budget-instruction/src/compute_budget_instruction_details.rs
+++ compute-budget-instruction/src/compute_budget_instruction_details.rs
@@ -18,7 +18,8 @@ use {
 };
 
 #[cfg_attr(test, derive(Eq, PartialEq))]
-#[derive(Clone, Default, Debug)]
+#[cfg_attr(feature = "dev-context-only-utils", derive(Clone))]
+#[derive(Default, Debug)]
 pub struct ComputeBudgetInstructionDetails {
     // compute-budget instruction details:
     // the first field in tuple is instruction index, second field is the unsanitized value set by user
