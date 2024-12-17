9839ab82a91df4276f6ba0293b0c00b608fb770e
Ryo Onodera <ryoqun@gmail.com>
2024-12-17 23:31:30 +0900

    Revert "Create RootBankCache earlier for unified scheduler"
    This reverts commit 736b2d052398244a75bf9baed14ca0dbc32f8222.

--- core/src/tpu.rs
+++ core/src/tpu.rs
@@ -33,7 +33,6 @@ use {
     solana_runtime::{
         bank_forks::BankForks,
         prioritization_fee_cache::PrioritizationFeeCache,
-        root_bank_cache::RootBankCache,
         vote_sender_types::{ReplayVoteReceiver, ReplayVoteSender},
     },
     solana_sdk::{clock::Slot, pubkey::Pubkey, quic::NotifyKeyUpdate, signature::Keypair},
@@ -101,7 +100,6 @@ impl Tpu {
         shred_version: u16,
         vote_tracker: Arc<VoteTracker>,
         bank_forks: Arc<RwLock<BankForks>>,
-        root_bank_cache: RootBankCache,
         verified_vote_sender: VerifiedVoteSender,
         gossip_verified_vote_hash_sender: GossipVerifiedVoteHashSender,
         replay_vote_receiver: ReplayVoteReceiver,
@@ -262,7 +260,6 @@ impl Tpu {
             gossip_vote_sender,
             vote_tracker,
             bank_forks.clone(),
-            root_bank_cache,
             subscriptions.clone(),
             verified_vote_sender,
             gossip_verified_vote_hash_sender,
