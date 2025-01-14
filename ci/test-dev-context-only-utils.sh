e9a01e396398bfee3718e7c9bd8a5d857b4e73f9
Ryo Onodera <ryoqun@gmail.com>
2025-01-14 15:07:47 +0900

    wip: ci: Distribute dcou builds more evenly

--- ci/test-dev-context-only-utils.sh
+++ ci/test-dev-context-only-utils.sh
@@ -2,5 +2,7 @@
 
 set -eo pipefail
 
+(unset RUSTC_WRAPPER; cargo install --force --git https://github.com/ryoqun/cargo-hack.git --branch interleaved-partition cargo-hack)
+
 scripts/check-dev-context-only-utils.sh check-all-targets "$@"
 scripts/check-dev-context-only-utils.sh check-bins-and-lib "$@"
