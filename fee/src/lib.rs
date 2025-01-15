52cfd859b178c40b23c2b3d4595959ea032e6334
Andrew Fitzgerald <apfitzge@gmail.com>
2025-01-15 08:34:00 -0600

    tests

--- fee/src/lib.rs
+++ fee/src/lib.rs
@@ -75,3 +75,93 @@ impl<Tx: SVMMessage> From<&Tx> for SignatureCounts {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_calculate_fee_details_internal() {
+        const LAMPORTS_PER_SIGNATURE: u64 = 5_000;
+
+        // Impossible case - 0 signatures.
+        {
+            let signature_counts = SignatureCounts {
+                num_transaction_signatures: 0,
+                num_ed25519_signatures: 0,
+                num_secp256k1_signatures: 0,
+            };
+            let prioritization_fee = 0;
+
+            assert_eq!(
+                calculate_fee_details_internal(
+                    signature_counts,
+                    LAMPORTS_PER_SIGNATURE,
+                    prioritization_fee,
+                    false
+                ),
+                FeeDetails::new(0, 0, false)
+            );
+        }
+
+        // Simple signature
+        {
+            let signature_counts = SignatureCounts {
+                num_transaction_signatures: 1,
+                num_ed25519_signatures: 0,
+                num_secp256k1_signatures: 0,
+            };
+            let prioritization_fee = 0;
+
+            assert_eq!(
+                calculate_fee_details_internal(
+                    signature_counts,
+                    LAMPORTS_PER_SIGNATURE,
+                    prioritization_fee,
+                    false
+                ),
+                FeeDetails::new(LAMPORTS_PER_SIGNATURE, 0, false)
+            );
+        }
+
+        // Simple signature and prioritization.
+        {
+            let signature_counts = SignatureCounts {
+                num_transaction_signatures: 1,
+                num_ed25519_signatures: 0,
+                num_secp256k1_signatures: 0,
+            };
+            let prioritization_fee = 1_000;
+
+            assert_eq!(
+                calculate_fee_details_internal(
+                    signature_counts,
+                    LAMPORTS_PER_SIGNATURE,
+                    prioritization_fee,
+                    false
+                ),
+                FeeDetails::new(LAMPORTS_PER_SIGNATURE, 1_000, false)
+            );
+        }
+
+        // Pre-compile signatures.
+        {
+            let signature_counts = SignatureCounts {
+                num_transaction_signatures: 1,
+                num_ed25519_signatures: 2,
+                num_secp256k1_signatures: 3,
+            };
+            let prioritization_fee = 4_000;
+
+            assert_eq!(
+                calculate_fee_details_internal(
+                    signature_counts,
+                    LAMPORTS_PER_SIGNATURE,
+                    prioritization_fee,
+                    false
+                ),
+                FeeDetails::new(6 * LAMPORTS_PER_SIGNATURE, 4_000, false)
+            );
+        }
+    }
+}
