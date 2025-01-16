04ad98f8c86d613326e6b02408535ea76874fa08
Ryo Onodera <ryoqun@gmail.com>
2025-01-16 15:06:39 +0900

    Disable sccache altogether

--- ci/test-dev-context-only-utils.sh
+++ ci/test-dev-context-only-utils.sh
@@ -3,6 +3,7 @@
 set -eo pipefail
 
 (unset RUSTC_WRAPPER; cargo install --force --git https://github.com/ryoqun/cargo-hack.git --branch interleaved-partition cargo-hack)
+unset RUSTC_WRAPPER
 
 scripts/check-dev-context-only-utils.sh check-all-targets "$@"
 scripts/check-dev-context-only-utils.sh check-bins-and-lib "$@"
