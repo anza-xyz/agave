f3e96b7244196fde163ccbc86491d471818f205b
Andrew Fitzgerald <apfitzge@gmail.com>
2025-01-19 21:51:28 -0600

    Change prio_graph_scheduler configurations for 1k maxs, 256 look ahead

--- core/src/banking_stage/transaction_scheduler/prio_graph_scheduler.rs
+++ core/src/banking_stage/transaction_scheduler/prio_graph_scheduler.rs
@@ -59,7 +59,7 @@ impl PrioGraphScheduler {
             account_locks: ThreadAwareAccountLocks::new(num_threads),
             consume_work_senders,
             finished_consume_work_receiver,
-            look_ahead_window_size: 2048,
+            look_ahead_window_size: 256,
             prio_graph: PrioGraph::new(passthrough_priority),
         }
     }
@@ -170,7 +170,7 @@ impl PrioGraphScheduler {
 
         let mut unblock_this_batch =
             Vec::with_capacity(self.consume_work_senders.len() * TARGET_NUM_TRANSACTIONS_PER_BATCH);
-        const MAX_TRANSACTIONS_PER_SCHEDULING_PASS: usize = 100_000;
+        const MAX_TRANSACTIONS_PER_SCHEDULING_PASS: usize = 1_000;
         let mut num_scheduled: usize = 0;
         let mut num_sent: usize = 0;
         let mut num_unschedulable: usize = 0;
