da031c75b5f7df06e80d67a10f18efd77a854f53
Ryo Onodera <ryoqun@gmail.com>
2025-01-23 23:16:28 +0900

    Make sccache use local disk storage for dcou

--- ci/test-dev-context-only-utils.sh
+++ ci/test-dev-context-only-utils.sh
@@ -3,7 +3,12 @@
 set -eo pipefail
 
 (unset RUSTC_WRAPPER; cargo install --force --git https://github.com/ryoqun/cargo-hack.git --branch interleaved-partition cargo-hack)
-unset RUSTC_WRAPPER
+
+unset SCCACHE_GCS_KEY_PATH SCCACHE_GCS_BUCKET SCCACHE_GCS_RW_MODE SCCACHE_GCS_KEY_PREFIX
+export SCCACHE_CACHE_SIZE="200G"
+sccache --show-stats
 
 scripts/check-dev-context-only-utils.sh check-all-targets "$@"
 scripts/check-dev-context-only-utils.sh check-bins-and-lib "$@"
+
+sccache --stop-server
