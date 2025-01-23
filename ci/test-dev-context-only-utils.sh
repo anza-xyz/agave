de1ac388beee4d85b57e8f0ad9dbb4b94e29adce
Ryo Onodera <ryoqun@gmail.com>
2025-01-23 23:23:11 +0900

    Use _

--- ci/test-dev-context-only-utils.sh
+++ ci/test-dev-context-only-utils.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
 set -eo pipefail
+source ./ci/_
 
 (unset RUSTC_WRAPPER; cargo install --force --git https://github.com/ryoqun/cargo-hack.git --branch interleaved-partition cargo-hack)
 
 unset SCCACHE_GCS_KEY_PATH SCCACHE_GCS_BUCKET SCCACHE_GCS_RW_MODE SCCACHE_GCS_KEY_PREFIX
 export SCCACHE_CACHE_SIZE="200G"
-sccache --show-stats
+_ sccache --show-stats
 
 scripts/check-dev-context-only-utils.sh check-all-targets "$@"
 scripts/check-dev-context-only-utils.sh check-bins-and-lib "$@"
 
-sccache --stop-server
+_ sccache --stop-server
