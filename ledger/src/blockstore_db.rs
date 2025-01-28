91d02bba6299f77359fa8c8001109162a825afed
steviez <steven@anza.xyz>
2025-01-28 14:43:50 -0600

    root

--- ledger/src/blockstore_db.rs
+++ ledger/src/blockstore_db.rs
@@ -56,8 +56,6 @@ const MAX_WRITE_BUFFER_SIZE: u64 = 256 * 1024 * 1024; // 256MB
 //   include/rocksdb/advanced_options.h#L908C30-L908C30
 const PERIODIC_COMPACTION_SECONDS: u64 = 60 * 60 * 24;
 
-// Column family for root data
-const ROOT_CF: &str = "root";
 /// Column family for indexes
 const INDEX_CF: &str = "index";
 /// Column family for Data Shreds
@@ -1295,7 +1293,7 @@ impl TypedColumn for columns::BankHash {
 
 impl SlotColumn for columns::Root {}
 impl ColumnName for columns::Root {
-    const NAME: &'static str = ROOT_CF;
+    const NAME: &'static str = "root";
 }
 impl TypedColumn for columns::Root {
     type Type = bool;
