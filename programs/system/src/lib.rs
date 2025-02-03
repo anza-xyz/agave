08c38731138b97ee6f4edac9d3f5cdbc5144471e
Pankaj Garg <pankaj@anza.xyz>
2025-02-03 12:05:32 -0800

    re-export from solana-system-program

--- programs/system/src/lib.rs
+++ programs/system/src/lib.rs
@@ -3,4 +3,7 @@ pub mod system_instruction;
 pub mod system_processor;
 
 use solana_sdk_ids::system_program;
-pub use system_program::id;
+pub use {
+    solana_nonce_account::{get_system_account_kind, SystemAccountKind},
+    system_program::id,
+};
