a2a35807fe314cf53738879675811ffc00e193c2
Ryo Onodera <ryoqun@gmail.com>
2025-02-07 22:16:48 +0900

    save

--- unified-scheduler-pool/src/lib.rs
+++ unified-scheduler-pool/src/lib.rs
@@ -267,7 +267,7 @@ impl BankingStageHelper {
     pub fn send_new_task(&self, task: Task) -> ScheduleResult {
         let sender = self.new_task_sender.upgrade() else {
             return Err(SchedulerAborted);
-        }
+        };
         sender.send(NewTaskPayload::Payload(task)).unwrap();
         Ok(())
     }
