yihau/solana

agave-validator: add args tests for set-public-address

validator/src/commands/set_public_address/mod.rs
@@ -1,11 +1,39 @@
 use {
-    crate::{admin_rpc_service, cli::DefaultArgs},
+    crate::{admin_rpc_service, cli::DefaultArgs, commands::FromClapArgMatches},
     clap::{App, Arg, ArgGroup, ArgMatches, SubCommand},
     std::{net::SocketAddr, path::Path, process::exit},
 };
 
+const COMMAND: &str = "set-public-address";
+
+#[derive(Debug, PartialEq)]
+pub struct SetPublicAddressArg {
+    pub tpu_addr: Option<SocketAddr>,
+    pub tpu_forwards_addr: Option<SocketAddr>,
+}
+
+impl FromClapArgMatches for SetPublicAddressArg {
+    fn from_clap_arg_match(matches: &ArgMatches) -> Self {
+        let parse_arg_addr = |arg_name: &str, arg_long: &str| -> Option<SocketAddr> {
+            matches.value_of(arg_name).map(|host_port| {
+                solana_net_utils::parse_host_port(host_port).unwrap_or_else(|err| {
+                    eprintln!(
+                        "Failed to parse --{arg_long} address. It must be in the HOST:PORT \
+                     format. {err}"
+                    );
+                    exit(1);
+                })
+            })
+        };
+        SetPublicAddressArg {
+            tpu_addr: parse_arg_addr("tpu_addr", "tpu"),
+            tpu_forwards_addr: parse_arg_addr("tpu_forwards_addr", "tpu-forwards"),
+        }
+    }
+}
+
 pub fn command(_default_args: &DefaultArgs) -> App<'_, '_> {
-    SubCommand::with_name("set-public-address")
+    SubCommand::with_name(COMMAND)
         .about("Specify addresses to advertise in gossip")
         .arg(
             Arg::with_name("tpu_addr")
@@ -33,18 +61,7 @@ pub fn command(_default_args: &DefaultArgs) -> App<'_, '_> {
 }
 
 pub fn execute(matches: &ArgMatches, ledger_path: &Path) {
-    let parse_arg_addr = |arg_name: &str, arg_long: &str| -> Option<SocketAddr> {
-        matches.value_of(arg_name).map(|host_port| {
-            solana_net_utils::parse_host_port(host_port).unwrap_or_else(|err| {
-                eprintln!(
-                    "Failed to parse --{arg_long} address. It must be in the HOST:PORT format. {err}"
-                );
-                exit(1);
-            })
-        })
-    };
-    let tpu_addr = parse_arg_addr("tpu_addr", "tpu");
-    let tpu_forwards_addr = parse_arg_addr("tpu_forwards_addr", "tpu-forwards");
+    let set_public_address_arg = SetPublicAddressArg::from_clap_arg_match(matches);
 
     macro_rules! set_public_address {
         ($public_addr:expr, $set_public_address:ident, $request:literal) => {
@@ -61,10 +78,74 @@ pub fn execute(matches: &ArgMatches, ledger_path: &Path) {
             }
         };
     }
-    set_public_address!(tpu_addr, set_public_tpu_address, "setPublicTpuAddress");
     set_public_address!(
-        tpu_forwards_addr,
+        set_public_address_arg.tpu_addr,
+        set_public_tpu_address,
+        "setPublicTpuAddress"
+    );
+    set_public_address!(
+        set_public_address_arg.tpu_forwards_addr,
         set_public_tpu_forwards_address,
         "setPublicTpuForwardsAddress"
     );
+}
+
+#[cfg(test)]
+mod tests {
+    use {
+        super::*,
+        crate::commands::tests::{
+            verify_args_struct_by_command, verify_args_struct_by_command_is_error,
+        },
+    };
+
+    #[test]
+    fn verify_args_struct_by_command_set_public_default() {
+        verify_args_struct_by_command_is_error::<SetPublicAddressArg>(
+            command(&DefaultArgs::default()),
+            vec![COMMAND],
+        );
+    }
+
+    #[test]
+    fn verify_args_struct_by_command_set_public_address_tpu() {
+        verify_args_struct_by_command(
+            command(&DefaultArgs::default()),
+            vec![COMMAND, "--tpu", "127.0.0.1:8080"],
+            SetPublicAddressArg {
+                tpu_addr: Some(SocketAddr::from(([127, 0, 0, 1], 8080))),
+                tpu_forwards_addr: None,
+            },
+        );
+    }
+
+    #[test]
+    fn verify_args_struct_by_command_set_public_address_tpu_forwards() {
+        verify_args_struct_by_command(
+            command(&DefaultArgs::default()),
+            vec![COMMAND, "--tpu-forwards", "127.0.0.1:8081"],
+            SetPublicAddressArg {
+                tpu_addr: None,
+                tpu_forwards_addr: Some(SocketAddr::from(([127, 0, 0, 1], 8081))),
+            },
+        );
+    }
+
+    #[test]
+    fn verify_args_struct_by_command_set_public_address_tpu_and_tpu_forwards() {
+        verify_args_struct_by_command(
+            command(&DefaultArgs::default()),
+            vec![
+                COMMAND,
+                "--tpu",
+                "127.0.0.1:8080",
+                "--tpu-forwards",
+                "127.0.0.1:8081",
+            ],
+            SetPublicAddressArg {
+                tpu_addr: Some(SocketAddr::from(([127, 0, 0, 1], 8080))),
+                tpu_forwards_addr: Some(SocketAddr::from(([127, 0, 0, 1], 8081))),
+            },
+        );
+    }
 }

anza-xyz/agave

validator/src/commands/set_public_address/mod.rs
@@ -1,11 +1,40 @@
 use {
-    crate::{admin_rpc_service, cli::DefaultArgs},
+    crate::{admin_rpc_service, cli::DefaultArgs, commands::FromClapArgMatches},
     clap::{App, Arg, ArgGroup, ArgMatches, SubCommand},
     std::{net::SocketAddr, path::Path},
 };
 
+const COMMAND: &str = "set-public-address";
+
+#[derive(Debug, PartialEq)]
+pub struct SetPublicAddressArg {
+    pub tpu_addr: Option<SocketAddr>,
+    pub tpu_forwards_addr: Option<SocketAddr>,
+}
+
+impl FromClapArgMatches for SetPublicAddressArg {
+    fn from_clap_arg_match(matches: &ArgMatches) -> Result<Self, String> {
+        let parse_arg_addr = |arg_name: &str,
+                              arg_long: &str|
+         -> Result<Option<SocketAddr>, String> {
+            matches.value_of(arg_name).map(|host_port| {
+                solana_net_utils::parse_host_port(host_port).map_err(|err| {
+                    format!(
+                        "failed to parse --{arg_long} address. It must be in the HOST:PORT format. {err}"
+                    )
+                })
+            })
+            .transpose()
+        };
+        Ok(SetPublicAddressArg {
+            tpu_addr: parse_arg_addr("tpu_addr", "tpu")?,
+            tpu_forwards_addr: parse_arg_addr("tpu_forwards_addr", "tpu-forwards")?,
+        })
+    }
+}
+
 pub fn command(_default_args: &DefaultArgs) -> App<'_, '_> {
-    SubCommand::with_name("set-public-address")
+    SubCommand::with_name(COMMAND)
         .about("Specify addresses to advertise in gossip")
         .arg(
             Arg::with_name("tpu_addr")
@@ -33,18 +62,7 @@ pub fn command(_default_args: &DefaultArgs) -> App<'_, '_> {
 }
 
 pub fn execute(matches: &ArgMatches, ledger_path: &Path) -> Result<(), String> {
-    let parse_arg_addr = |arg_name: &str, arg_long: &str| -> Result<Option<SocketAddr>, String> {
-        matches.value_of(arg_name).map(|host_port| {
-            solana_net_utils::parse_host_port(host_port).map_err(|err| {
-                format!(
-                    "failed to parse --{arg_long} address. It must be in the HOST:PORT format. {err}"
-                )
-            })
-        })
-        .transpose()
-    };
-    let tpu_addr = parse_arg_addr("tpu_addr", "tpu")?;
-    let tpu_forwards_addr = parse_arg_addr("tpu_forwards_addr", "tpu-forwards")?;
+    let set_public_address_arg = SetPublicAddressArg::from_clap_arg_match(matches)?;
 
     macro_rules! set_public_address {
         ($public_addr:expr, $set_public_address:ident, $request:literal) => {
@@ -60,10 +78,75 @@ pub fn execute(matches: &ArgMatches, ledger_path: &Path) -> Result<(), String> {
             }
         };
     }
-    set_public_address!(tpu_addr, set_public_tpu_address, "set public tpu address")?;
     set_public_address!(
-        tpu_forwards_addr,
+        set_public_address_arg.tpu_addr,
+        set_public_tpu_address,
+        "setPublicTpuAddress"
+    )?;
+    set_public_address!(
+        set_public_address_arg.tpu_forwards_addr,
         set_public_tpu_forwards_address,
         "set public tpu forwards address"
-    )
+    )?;
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use {
+        super::*,
+        crate::commands::tests::{
+            verify_args_struct_by_command, verify_args_struct_by_command_is_error,
+        },
+    };
+
+    #[test]
+    fn verify_args_struct_by_command_set_public_default() {
+        verify_args_struct_by_command_is_error::<SetPublicAddressArg>(
+            command(&DefaultArgs::default()),
+            vec![COMMAND],
+        );
+    }
+
+    #[test]
+    fn verify_args_struct_by_command_set_public_address_tpu() {
+        verify_args_struct_by_command(
+            command(&DefaultArgs::default()),
+            vec![COMMAND, "--tpu", "127.0.0.1:8080"],
+            SetPublicAddressArg {
+                tpu_addr: Some(SocketAddr::from(([127, 0, 0, 1], 8080))),
+                tpu_forwards_addr: None,
+            },
+        );
+    }
+
+    #[test]
+    fn verify_args_struct_by_command_set_public_address_tpu_forwards() {
+        verify_args_struct_by_command(
+            command(&DefaultArgs::default()),
+            vec![COMMAND, "--tpu-forwards", "127.0.0.1:8081"],
+            SetPublicAddressArg {
+                tpu_addr: None,
+                tpu_forwards_addr: Some(SocketAddr::from(([127, 0, 0, 1], 8081))),
+            },
+        );
+    }
+
+    #[test]
+    fn verify_args_struct_by_command_set_public_address_tpu_and_tpu_forwards() {
+        verify_args_struct_by_command(
+            command(&DefaultArgs::default()),
+            vec![
+                COMMAND,
+                "--tpu",
+                "127.0.0.1:8080",
+                "--tpu-forwards",
+                "127.0.0.1:8081",
+            ],
+            SetPublicAddressArg {
+                tpu_addr: Some(SocketAddr::from(([127, 0, 0, 1], 8080))),
+                tpu_forwards_addr: Some(SocketAddr::from(([127, 0, 0, 1], 8081))),
+            },
+        );
+    }
 }