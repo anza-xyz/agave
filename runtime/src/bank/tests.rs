00ac83b1060e2324fb00fd1aa6868c54b29a95da
4r33x <vonauwers@gmail.com>
2025-03-02 22:29:31 +0300

    Add post accounts state data to geyser transactions

--- runtime/src/bank/tests.rs
+++ runtime/src/bank/tests.rs
@@ -3272,6 +3272,7 @@ fn test_load_and_execute_commit_transactions_fees_only(enable_fees_only_txs: boo
                     loaded_accounts_count: 2,
                     loaded_accounts_data_size: nonce_size as u32,
                 },
+                post_accounts_states: vec![]
             })]
         );
     } else {
