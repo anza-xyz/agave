a2899e160ab8981fb6d4f684c64a15f47f7ed131
Rory Harris <rharri4@gmail.com>
2025-03-07 10:18:21 -0800

    Update blockstore_db.rs
    Removing trailing whitespace

--- ledger/src/blockstore_db.rs
+++ ledger/src/blockstore_db.rs
@@ -1180,9 +1180,9 @@ fn get_db_options(blockstore_options: &BlockstoreOptions) -> Options {
         options.set_disable_auto_compactions(true);
     }
 
-    // Limit to (10) 50 MB log files (500 MB total) 
-    // Logs grow at < 5 MB / hour, so this provides several days of logs 
-    options.set_max_log_file_size(50 * 1024 * 1024); 
+    // Limit to (10) 50 MB log files (500 MB total)
+    // Logs grow at < 5 MB / hour, so this provides several days of logs
+    options.set_max_log_file_size(50 * 1024 * 1024);
     options.set_keep_log_file_num(10);
 
     // Allow Rocks to open/keep open as many files as it needs for performance;
