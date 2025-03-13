a580607f0d14aafc8ab3c1d61ef8a6e44b47716c
Kevin Heavey <heaveykevin2@gmail.com>
2025-03-13 19:01:57 +0400

    define is_nonverlapping function in solana-bpf-loader-program (#4888)
    * move is_nonverlapping function to solana-bpf-loader-program
    
    * copy over test_is_nonoverlapping
    
    (cherry picked from commit ccbf3f25f332cf4bfa4f1a9bf27db8d3333b3064)
    
    # Conflicts:
    #	programs/bpf_loader/src/syscalls/mod.rs

--- programs/bpf_loader/src/syscalls/mod.rs
+++ programs/bpf_loader/src/syscalls/mod.rs
@@ -12,6 +12,7 @@ pub use self::{
 };
 #[allow(deprecated)]
 use {
+<<<<<<< HEAD
     agave_feature_set::{
         self as feature_set, abort_on_invalid_curve, blake3_syscall_enabled,
         bpf_account_data_direct_mapping, curve25519_syscall_enabled,
@@ -24,6 +25,9 @@ use {
         remaining_compute_units_syscall_enabled, FeatureSet,
     },
     agave_precompiles::is_precompile,
+=======
+    crate::syscalls::mem_ops::is_nonoverlapping,
+>>>>>>> ccbf3f25f (define is_nonverlapping function in solana-bpf-loader-program (#4888))
     solana_account_info::AccountInfo,
     solana_big_mod_exp::{big_mod_exp, BigModExpParams},
     solana_blake3_hasher as blake3,
@@ -41,7 +45,6 @@ use {
     solana_log_collector::{ic_logger_msg, ic_msg},
     solana_poseidon as poseidon,
     solana_program_entrypoint::{BPF_ALIGN_OF_U128, MAX_PERMITTED_DATA_INCREASE, SUCCESS},
-    solana_program_memory::is_nonoverlapping,
     solana_program_runtime::{invoke_context::InvokeContext, stable_log},
     solana_pubkey::{Pubkey, PubkeyError, MAX_SEEDS, MAX_SEED_LEN, PUBKEY_BYTES},
     solana_sbpf::{
