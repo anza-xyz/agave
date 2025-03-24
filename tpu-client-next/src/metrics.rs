0ab2e7ed85570e672569f5d86d2ae2198beb0968
Alex Pyattaev <alex.pyattaev@gmail.com>
2025-03-24 13:31:49 +0200

    Apply suggestions from code review by Kirill
    Co-authored-by: kirill lykov <lykov.kirill@gmail.com>

--- tpu-client-next/src/metrics.rs
+++ tpu-client-next/src/metrics.rs
@@ -1,3 +1,6 @@
+//! If `metrics` feature is activated, this module provides `report_to_influxdb`
+//! method for [`SendTransactionStats`] which periodically reports transaction
+//! sending statistics to InfluxDB.
 use {
     crate::SendTransactionStats,
     solana_metrics::datapoint_info,
@@ -7,7 +10,7 @@ use {
 };
 
 impl SendTransactionStats {
-    ///Report the statistics to influxdb in a compact form
+    /// Report the statistics to influxdb in a compact form.
     #[allow(clippy::arithmetic_side_effects)]
     pub async fn report_to_influxdb(
         self: Arc<Self>,
@@ -17,31 +20,34 @@ impl SendTransactionStats {
     ) {
         let mut interval = interval(reporting_interval);
         let stats = self.clone();
-        while !cancel.is_cancelled() {
-            interval.tick().await;
-            let view = stats.read_and_reset();
-            let connect_error = view.connect_error_cids_exhausted
-                + view.connect_error_other
-                + view.connect_error_invalid_remote_address;
-            let connection_error = view.connection_error_reset
-                + view.connection_error_cids_exhausted
-                + view.connection_error_timed_out
-                + view.connection_error_application_closed
-                + view.connection_error_transport_error
-                + view.connection_error_version_mismatch
-                + view.connection_error_locally_closed;
-            let write_error = view.write_error_stopped
-                + view.write_error_closed_stream
-                + view.write_error_connection_lost
-                + view.write_error_zero_rtt_rejected;
+        loop {
+            select! {
+                    _ = interval.tick() => {
+                    let view = stats.read_and_reset();
+                    let connect_error = view.connect_error_cids_exhausted
+                        + view.connect_error_other
+                        + view.connect_error_invalid_remote_address;
+                    let connection_error = view.connection_error_reset
+                        + view.connection_error_cids_exhausted
+                        + view.connection_error_timed_out
+                        + view.connection_error_application_closed
+                        + view.connection_error_transport_error
+                        + view.connection_error_version_mismatch
+                        + view.connection_error_locally_closed;
+                    let write_error = view.write_error_stopped
+                        + view.write_error_closed_stream
+                        + view.write_error_connection_lost
+                        + view.write_error_zero_rtt_rejected;
 
-            datapoint_info!(
-                name,
-                ("connect_error", connect_error, i64),
-                ("connection_error", connection_error, i64),
-                ("successfully_sent", view.successfully_sent, i64),
-                ("write_error", write_error, i64),
-            );
-        }
+                    datapoint_info!(
+                        name,
+                        ("connect_error", connect_error, i64),
+                        ("connection_error", connection_error, i64),
+                        ("successfully_sent", view.successfully_sent, i64),
+                        ("write_error", write_error, i64),
+                    );
+                }
+                _ = cancel.cancelled() => break,
+            }
     }
 }
