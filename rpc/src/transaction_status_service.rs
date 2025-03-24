8ed4f8d031a1323e4d2230d6095f11ec16b4ed3c
4r33x <vonauwers@gmail.com>
2025-03-24 17:47:45 +0300

    Change Vec<T> to &[T]

--- rpc/src/transaction_status_service.rs
+++ rpc/src/transaction_status_service.rs
@@ -212,7 +212,7 @@ impl TransactionStatusService {
                             transaction.signature(),
                             &transaction_status_meta,
                             &transaction,
-                            post_accounts_states,
+                            &post_accounts_states,
                         );
                     }
 
@@ -393,7 +393,7 @@ pub(crate) mod tests {
             signature: &Signature,
             transaction_status_meta: &TransactionStatusMeta,
             transaction: &SanitizedTransaction,
-            _post_accounts_states: Vec<(Pubkey, AccountSharedData)>,
+            _post_accounts_states: &[(Pubkey, AccountSharedData)],
         ) {
             self.notifications.insert(
                 TestNotifierKey {
