d57515376f6307d7278b72a8bb8f19b44d8f9115
Ryo Onodera <ryoqun@gmail.com>
2025-04-13 22:27:46 +0900

    save

--- banking-bench/src/main.rs
+++ banking-bench/src/main.rs
@@ -573,6 +573,7 @@ fn main() {
         // processed, with `FALSE` indicate there is still bank. or returns TRUE indicate a
         // bank has expired before receiving all txs.
         if check_txs(
+            block_production_method,
             &signal_receiver,
             packets_for_this_iteration.transactions.len(),
             &poh_recorder,
