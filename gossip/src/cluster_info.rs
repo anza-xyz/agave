steviez/solana

Reduce num default tvu threads from 8 to 1
We currently create 8 threads that solely try to pull packets out from
sockets associated with the turbine port. Multiple threads were added to
mitigate buffer receive errors. With improvements in the software
including the use of recvmmsg, using 8 threads is overkill and we can
read from this port plenty fast with a single thread

gossip/src/cluster_info.rs
@@ -141,7 +141,7 @@ const MIN_NUM_STAKED_NODES: usize = 500;
 // Must have at least one socket to monitor the TVU port
 // The unsafes are safe because we're using fixed, known non-zero values
 pub const MINIMUM_NUM_TVU_SOCKETS: NonZeroUsize = unsafe { NonZeroUsize::new_unchecked(1) };
-pub const DEFAULT_NUM_TVU_SOCKETS: NonZeroUsize = unsafe { NonZeroUsize::new_unchecked(8) };
+pub const DEFAULT_NUM_TVU_SOCKETS: NonZeroUsize = MINIMUM_NUM_TVU_SOCKETS;
 
 #[derive(Debug, PartialEq, Eq, Error)]
 pub enum ClusterInfoError {

anza-xyz/agave

gossip/src/cluster_info.rs
@@ -136,7 +136,7 @@ const MIN_NUM_STAKED_NODES: usize = 500;
 
 // Must have at least one socket to monitor the TVU port
 pub const MINIMUM_NUM_TVU_RECEIVE_SOCKETS: NonZeroUsize = NonZeroUsize::new(1).unwrap();
-pub const DEFAULT_NUM_TVU_RECEIVE_SOCKETS: NonZeroUsize = NonZeroUsize::new(8).unwrap();
+pub const DEFAULT_NUM_TVU_RECEIVE_SOCKETS: NonZeroUsize = MINIMUM_NUM_TVU_RECEIVE_SOCKETS;
 pub const MINIMUM_NUM_TVU_RETRANSMIT_SOCKETS: NonZeroUsize = NonZeroUsize::new(1).unwrap();
 pub const DEFAULT_NUM_TVU_RETRANSMIT_SOCKETS: NonZeroUsize = NonZeroUsize::new(12).unwrap();
 