f81064d15f8c7572a8a3309d7f8482574a07bd13
Alex Pyattaev <alex.pyattaev@gmail.com>
2025-04-17 08:44:04 +0000

    prevent accidental bind with SO_REUSEPORT using our bind_in_range functions

--- gossip/src/cluster_info.rs
+++ gossip/src/cluster_info.rs
@@ -2775,7 +2775,7 @@ impl Node {
                 .expect("tpu_vote multi_bind");
 
         let (tpu_vote_quic_port, tpu_vote_quic) =
-            Self::bind_with_config(bind_ip_addr, port_range, socket_config_reuseport);
+            Self::bind_with_config(bind_ip_addr, port_range, socket_config);
         let tpu_vote_quic = bind_more_with_config(
             tpu_vote_quic,
             num_quic_endpoints.get(),
