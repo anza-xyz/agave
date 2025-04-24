c204702dd02e61a3c57e08112b8f6ca262b1490c
Andrew Fitzgerald <apfitzge@gmail.com>
2025-04-24 15:34:00 -0500

    explicit paths in linux-only blocks

--- streamer/src/recvmmsg.rs
+++ streamer/src/recvmmsg.rs
@@ -55,7 +55,7 @@ fn cast_socket_addr(addr: &sockaddr_storage, hdr: &mmsghdr) -> Option<SocketAddr
         // ref: https://github.com/rust-lang/socket2/blob/65085d9dff270e588c0fbdd7217ec0b392b05ef2/src/sockaddr.rs#L167-L172
         let addr = unsafe { &*(addr as *const _ as *const sockaddr_in) };
         return Some(SocketAddr::V4(SocketAddrV4::new(
-            Ipv4Addr::from(addr.sin_addr.s_addr.to_ne_bytes()),
+            std::net::Ipv4Addr::from(addr.sin_addr.s_addr.to_ne_bytes()),
             u16::from_be(addr.sin_port),
         )));
     }
@@ -65,7 +65,7 @@ fn cast_socket_addr(addr: &sockaddr_storage, hdr: &mmsghdr) -> Option<SocketAddr
         // ref: https://github.com/rust-lang/socket2/blob/65085d9dff270e588c0fbdd7217ec0b392b05ef2/src/sockaddr.rs#L174-L189
         let addr = unsafe { &*(addr as *const _ as *const sockaddr_in6) };
         return Some(SocketAddr::V6(SocketAddrV6::new(
-            Ipv6Addr::from(addr.sin6_addr.s6_addr),
+            std::net::Ipv6Addr::from(addr.sin6_addr.s6_addr),
             u16::from_be(addr.sin6_port),
             addr.sin6_flowinfo,
             addr.sin6_scope_id,
