b485ea541179ddc9e70ce61c0370a9b22aaaa5b8
brooks <brooks@anza.xyz>
2025-04-28 13:53:07 -0400

    move backpressure loop (and setting exit) out of thread

--- validator/src/admin_rpc_service.rs
+++ validator/src/admin_rpc_service.rs
@@ -260,43 +260,45 @@ impl AdminRpc for AdminRpcImpl {
     fn exit(&self, meta: Self::Metadata) -> Result<()> {
         debug!("exit admin rpc request received");
 
+        // Delay exit signal until this RPC request completes, otherwise the caller of `exit` might
+        // receive a confusing error as the validator shuts down before a response is sent back.
+        thread::sleep(Duration::from_millis(100));
+
+        warn!("validator exit requested");
+        meta.validator_exit.write().unwrap().exit();
+
+        if !meta.validator_exit_backpressure.is_empty() {
+            let service_names = meta.validator_exit_backpressure.keys();
+            info!("Wait for these services to complete: {service_names:?}");
+            error!(
+                "brooks DEBUG: backpressure flags: {:?}",
+                meta.validator_exit_backpressure
+            );
+            loop {
+                // brooks NOTE: initial sleep is a grace period to allow anyone to raise their backpressure flags
+                // subsequent sleeps are to throttle how often we check and log
+                thread::sleep(Duration::from_secs(1));
+
+                let mut any_flags_raised = false;
+                for (name, flag) in meta.validator_exit_backpressure.iter() {
+                    let is_flag_raised = flag.load(Ordering::Relaxed);
+                    if is_flag_raised {
+                        info!("{name}'s exit backpressure flag is raised");
+                        any_flags_raised = true;
+                    }
+                }
+                if !any_flags_raised {
+                    error!("brooks DEBUG: no flags raised; break!");
+                    break;
+                }
+            }
+            info!("All services have completed.");
+        }
+
+        // brooks TODO: try making it not a thread
         let thread_handle = thread::Builder::new()
             .name("solProcessExit".into())
             .spawn(move || {
-                // Delay exit signal until this RPC request completes, otherwise the caller of `exit` might
-                // receive a confusing error as the validator shuts down before a response is sent back.
-                thread::sleep(Duration::from_millis(100));
-
-                warn!("validator exit requested");
-                meta.validator_exit.write().unwrap().exit();
-
-                if !meta.validator_exit_backpressure.is_empty() {
-                    let service_names = meta.validator_exit_backpressure.keys();
-                    info!("Wait for these services to complete: {service_names:?}");
-                    error!(
-                        "brooks DEBUG: backpressure flags: {:?}",
-                        meta.validator_exit_backpressure
-                    );
-                    loop {
-                        // brooks NOTE: initial sleep is a grace period to allow anyone to raise their backpressure flags
-                        // subsequent sleeps are to throttle how often we check and log
-                        thread::sleep(Duration::from_secs(1));
-
-                        let mut any_flags_raised = false;
-                        for (name, flag) in meta.validator_exit_backpressure.iter() {
-                            let is_flag_raised = flag.load(Ordering::Relaxed);
-                            if is_flag_raised {
-                                info!("{name}'s exit backpressure flag is raised");
-                                any_flags_raised = true;
-                            }
-                        }
-                        if !any_flags_raised {
-                            error!("brooks DEBUG: no flags raised; break!");
-                            break;
-                        }
-                    }
-                    info!("All services have completed.");
-                }
 
                 // TODO: Debug why Exit doesn't always cause the validator to fully exit
                 // (rocksdb background processing or some other stuck thread perhaps?).
@@ -320,8 +322,6 @@ impl AdminRpc for AdminRpcImpl {
         error!("brooks DEBUG: Joining exit thread...");
         let result = thread_handle.join();
         error!("brooks DEBUG: Joining exit thread... DONE, result: {result:?}");
-        // brooks XXX: extra sleep to see the log above
-        thread::sleep(Duration::from_millis(100));
 
         Ok(())
     }
