fe4f9b9b196679d91033ca06da6b2844e0f61426
marc <marcinbieszke@gmail.com>
2025-05-04 11:30:49 +0200

    convert benchmarks to criterion

--- transaction-status/benches/extract_memos.rs
+++ transaction-status/benches/extract_memos.rs
@@ -1,16 +1,11 @@
-#![feature(test)]
-
-extern crate test;
-
 use {
+    criterion::{criterion_group, criterion_main, Criterion},
     solana_message::{compiled_instruction::CompiledInstruction, Message},
     solana_pubkey::Pubkey,
     solana_transaction_status::extract_memos::{spl_memo_id_v1, spl_memo_id_v3, ExtractMemos},
-    test::Bencher,
 };
 
-#[bench]
-fn bench_extract_memos(b: &mut Bencher) {
+fn bench_extract_memos(c: &mut Criterion) {
     let mut account_keys: Vec<Pubkey> = (0..64).map(|_| Pubkey::new_unique()).collect();
     account_keys[62] = spl_memo_id_v1();
     account_keys[63] = spl_memo_id_v3();
@@ -30,5 +25,10 @@ fn bench_extract_memos(b: &mut Bencher) {
         ..Message::default()
     };
 
-    b.iter(|| message.extract_memos());
+    c.bench_function("extract_memos", |b| {
+        b.iter(|| message.extract_memos());
+    });
 }
+
+criterion_group!(benches, bench_extract_memos);
+criterion_main!(benches);
