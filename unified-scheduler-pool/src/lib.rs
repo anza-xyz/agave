82f90f347f8709239f039d37aa0f809cd25f8c25
Ryo Onodera <ryoqun@gmail.com>
2025-05-14 22:47:17 +0900

    save

--- unified-scheduler-pool/src/lib.rs
+++ unified-scheduler-pool/src/lib.rs
@@ -2029,7 +2029,7 @@ impl<S: SpawnableScheduler<TH>, TH: TaskHandler> ThreadManager<S, TH> {
                                 count += 1;
                             }
                             state_machine.reinitialize();
-                            sleepless_testing::at(CheckPoint::Discarded(count.into()));
+                            sleepless_testing::at(CheckPoint::Discarded(count.0));
                         }
                         // Prepare for the new session.
                         match new_task_receiver.recv() {
