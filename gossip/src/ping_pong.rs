09d0ed18e5096d6c6d4599cabd2537d0cb63f3fb
greg <greg@anza.xyz>
2025-06-02 22:53:42 +0000

    reduce ping pong check metric report by 2^5 or 32

--- gossip/src/ping_pong.rs
+++ gossip/src/ping_pong.rs
@@ -1,4 +1,5 @@
 use {
+    crate::cluster_info_metrics::should_report_message_signature,
     lru::LruCache,
     rand::{CryptoRng, Rng},
     serde_big_array::BigArray,
@@ -19,6 +20,7 @@ use {
 
 const KEY_REFRESH_CADENCE: Duration = Duration::from_secs(60);
 const PING_PONG_HASH_PREFIX: &[u8] = "SOLANA_PING_PONG".as_bytes();
+const PONG_SIGNATURE_SAMPLE_LEADING_ZEROS: u32 = 5;
 
 // For backward compatibility we are using a const generic parameter here.
 // N should always be >= 8 and only the first 8 bytes are used. So the new code
@@ -118,6 +120,10 @@ impl Pong {
     pub fn from(&self) -> &Pubkey {
         &self.from
     }
+
+    pub(crate) fn signature(&self) -> &Signature {
+        &self.signature
+    }
 }
 
 impl Sanitize for Pong {
@@ -181,12 +187,17 @@ impl<const N: usize> PingCache<N> {
         };
         self.pongs.put(remote_node, now);
         if let Some(sent_time) = self.ping_times.pop(&socket.ip()) {
-            let rtt = now.saturating_duration_since(sent_time);
-            datapoint_info!(
-                "ping_rtt",
-                ("peer_ip", socket.ip().to_string(), String),
-                ("rtt_us", rtt.as_micros() as i64, i64),
-            );
+            if should_report_message_signature(
+                pong.signature(),
+                PONG_SIGNATURE_SAMPLE_LEADING_ZEROS,
+            ) {
+                let rtt = now.saturating_duration_since(sent_time);
+                datapoint_info!(
+                    "ping_rtt",
+                    ("peer_ip", socket.ip().to_string(), String),
+                    ("rtt_us", rtt.as_micros() as i64, i64),
+                );
+            }
         }
         true
     }
