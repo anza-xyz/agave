ed3f2736f4281542c6fac714c30fba13f8dee3fd
yihau <yihau.chen@icloud.com>
2025-06-20 19:02:41 +0800

    --no-snapshot-fetch

--- validator/src/commands/run/args/rpc_bootstrap_config.rs
+++ validator/src/commands/run/args/rpc_bootstrap_config.rs
@@ -6,12 +6,18 @@ use {
 #[derive(Debug, PartialEq, Clone, Default)]
 pub struct RpcBootstrapConfig {
     pub no_genesis_fetch: bool,
+    pub no_snapshot_fetch: bool,
 }
 
 impl FromClapArgMatches for RpcBootstrapConfig {
     fn from_clap_arg_match(matches: &ArgMatches) -> Result<Self> {
         let no_genesis_fetch = matches.is_present("no_genesis_fetch");
 
-        Ok(Self { no_genesis_fetch })
+        let no_snapshot_fetch = matches.is_present("no_snapshot_fetch");
+
+        Ok(Self {
+            no_genesis_fetch,
+            no_snapshot_fetch,
+        })
     }
 }
