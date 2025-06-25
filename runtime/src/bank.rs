0c23cd545c7303438d464f187d04d83ff9848690
Ashwin Sekar <ashwin@anza.xyz>
2025-06-25 10:42:18 -0400

    replay: remove reentrant locking in prune program cache

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -1621,7 +1621,12 @@ impl Bank {
         }
     }
 
-    pub fn prune_program_cache(&self, new_root_slot: Slot, new_root_epoch: Epoch) {
+    pub fn prune_program_cache(
+        &self,
+        new_root_slot: Slot,
+        new_root_epoch: Epoch,
+        bank_forks: &BankForks,
+    ) {
         let upcoming_environment = self
             .transaction_processor
             .epoch_boundary_preparation
@@ -1632,7 +1637,7 @@ impl Bank {
             .global_program_cache
             .write()
             .unwrap()
-            .prune(new_root_slot, upcoming_environment);
+            .prune(new_root_slot, upcoming_environment, bank_forks);
     }
 
     pub fn prune_program_cache_by_deployment_slot(&self, deployment_slot: Slot) {
