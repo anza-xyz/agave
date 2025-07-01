e87e50f3f9933ea9d437578e2b67cf9c821020e9
marc <marcinbieszke@gmail.com>
2025-07-01 15:12:53 +0200

    fix: arithmetic operation that can potentially result in unexpected side-effects

--- net-utils/src/sockets.rs
+++ net-utils/src/sockets.rs
@@ -296,7 +296,7 @@ pub fn bind_two_in_range_with_offset_and_config(
     for port in range.0..=max_start_port {
         let first_bind_result = bind_to_with_config(ip_addr, port, sock1_config);
         if let Ok(first_bind) = first_bind_result {
-            let second_port = port + offset;
+            let second_port = port.saturating_add(offset);
             let second_bind_result = bind_to_with_config(ip_addr, second_port, sock2_config);
             if let Ok(second_bind) = second_bind_result {
                 return Ok((
