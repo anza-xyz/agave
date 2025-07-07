0a7745c230bfba8a349a2681203d0a35e4740577
Kamil Skalski <kamil.skalski@gmail.com>
2025-07-07 15:57:44 +0200

    Use eprintln.

--- accounts-db/store-tool/src/main.rs
+++ accounts-db/store-tool/src/main.rs
@@ -234,7 +234,7 @@ fn do_search(
                     );
                 }
             }
-        }).unwrap_or_else(|err| println!("failed to scan accounts in file '{}': {err}",
+        }).unwrap_or_else(|err| eprintln!("failed to scan accounts in file '{}': {err}",
                          file.display()));
     });
 
