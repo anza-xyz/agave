8e6620e3539b92353f772fd878de8ca21f7778c4
Alex Pyattaev <alex.pyattaev@anza.xyz>
2025-07-09 12:00:53 +0000

    new better bucketed rate limiter

--- streamer/src/nonblocking/connection_rate_limiter.rs
+++ streamer/src/nonblocking/connection_rate_limiter.rs
@@ -1,6 +1,7 @@
 use {
     governor::{DefaultDirectRateLimiter, DefaultKeyedRateLimiter, Quota, RateLimiter},
-    std::{net::IpAddr, num::NonZeroU32, sync::Mutex, time::Instant},
+    lazy_lru::LruCache,
+    std::{borrow::Borrow, net::IpAddr, num::NonZeroU32, sync::Mutex, time::Instant},
 };
 
 pub struct ConnectionRateLimiter {
@@ -131,8 +132,64 @@ pub mod test {
         std::thread::sleep(Duration::from_millis(120));
         assert_eq!(tb.current_tokens(), 100, "Bucket should not overfill");
     }
+    #[test]
+    fn test_token_buckets() {
+        let prototype_bucket = TokenBucket::new(100, 100, 1000.0);
+        let mut rl = KeyedRateLimiter::new(8, prototype_bucket);
+        let ip1 = IpAddr::V4(Ipv4Addr::from_bits(1234));
+        let ip2 = IpAddr::V4(Ipv4Addr::from_bits(4321));
+        assert_eq!(rl.current_tokens(ip1), None, "Initially no buckets exist");
+        rl.consume_tokens(ip1, 50)
+            .expect("Bucket is initially full");
+        rl.consume_tokens(ip1, 50)
+            .expect("We should still have >50 tokens left");
+        rl.consume_tokens(ip1, 50)
+            .expect_err("There should not be enough tokens now");
+        rl.consume_tokens(ip2, 50)
+            .expect("Bucket is initially full");
+        rl.consume_tokens(ip2, 50)
+            .expect("We should still have >50 tokens left");
+        rl.consume_tokens(ip2, 50)
+            .expect_err("There should not be enough tokens now");
+        std::thread::sleep(Duration::from_millis(50));
+        assert!(
+            rl.current_tokens(ip1).unwrap() > 40,
+            "We should be refilling at ~1 token per millisecond"
+        );
+        assert!(
+            rl.current_tokens(ip1).unwrap() < 70,
+            "We should be refilling at ~1 token per millisecond"
+        );
+        rl.consume_tokens(ip1, 40)
+            .expect("Bucket should have enough for another request now");
+        std::thread::sleep(Duration::from_millis(120));
+        assert_eq!(
+            rl.current_tokens(ip1),
+            Some(100),
+            "Bucket should not overfill"
+        );
+        assert_eq!(
+            rl.current_tokens(ip2),
+            Some(100),
+            "Bucket should not overfill"
+        );
+
+        rl.consume_tokens(ip2, 100).expect("Bucket should be full");
+        for ip in 0..16 {
+            let ip = IpAddr::V4(Ipv4Addr::from_bits(ip));
+            rl.consume_tokens(ip, 50).unwrap();
+        }
+        assert_eq!(
+            rl.current_tokens(ip1),
+            None,
+            "Record should have been erased"
+        );
+        rl.consume_tokens(ip2, 100)
+            .expect("New bucket should have been made for ip2");
+    }
 }
 
+#[derive(Clone)]
 struct TokenBucketState {
     tokens: u64,
     last_access: u64,
@@ -145,6 +202,17 @@ pub struct TokenBucket {
     max_tokens: u64,
     base_time: Instant,
     state: Mutex<TokenBucketState>,
+}
+
+impl Clone for TokenBucket {
+    fn clone(&self) -> Self {
+        Self {
+            tokens_per_second: self.tokens_per_second,
+            max_tokens: self.max_tokens,
+            base_time: self.base_time,
+            state: Mutex::new(self.state.lock().unwrap().clone()),
+        }
+    }
 }
 
 impl TokenBucket {
@@ -180,7 +248,6 @@ impl TokenBucket {
         let now = self.time_us();
         let mut state = self.state.lock().unwrap();
         self.update_state(now, &mut state);
-        dbg!(state.tokens);
         if state.tokens >= request_size {
             state.tokens -= request_size;
             Ok(state.tokens)
@@ -201,7 +268,6 @@ impl TokenBucket {
         let new_tokens = elapsed * self.tokens_per_second / 1e6;
         // check if we can mint at least 1 new token
         if new_tokens > 1.0 {
-            dbg!(new_tokens as u64);
             // update time of last mint
             state.last_access = now;
             // fill the bucket
@@ -212,6 +278,37 @@ impl TokenBucket {
     }
 }
 
-struct KeyedRateLimiter {
-    data: Laz,
+pub struct KeyedRateLimiter {
+    data: LruCache<IpAddr, TokenBucket>,
+    prototype_bucket: TokenBucket,
+}
+
+impl KeyedRateLimiter {
+    pub fn new(capacity: usize, prototype_bucket: TokenBucket) -> Self {
+        Self {
+            data: LruCache::new(capacity),
+            prototype_bucket,
+        }
+    }
+    pub fn current_tokens(&self, key: impl Borrow<IpAddr>) -> Option<u64> {
+        let bucket = self.data.get(key.borrow())?;
+        Some(bucket.current_tokens())
+    }
+
+    pub fn consume_tokens(
+        &mut self,
+        key: impl Borrow<IpAddr>,
+        request_size: u64,
+    ) -> Result<u64, u64> {
+        let bucket = self.data.get(key.borrow());
+        match bucket {
+            Some(bucket) => bucket.consume_tokens(request_size),
+            None => {
+                let bucket = self.prototype_bucket.clone();
+                let res = bucket.consume_tokens(request_size);
+                self.data.put(*key.borrow(), bucket);
+                return res;
+            }
+        }
+    }
 }
