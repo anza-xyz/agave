a424f3271b5d0b7489d4f5d4a4c3b82f315ab801
jeff washington <jeff.washington@solana.com>
2025-07-11 17:34:15 -0500

    remove cli args

--- validator/src/commands/run/args.rs
+++ validator/src/commands/run/args.rs
@@ -1524,13 +1524,6 @@ pub fn add_args<'a>(app: App<'a, 'a>, default_args: &'a DefaultArgs) -> App<'a, 
             ),
     )
     .arg(
-        Arg::with_name("accounts_db_test_hash_calculation")
-            .long("accounts-db-test-hash-calculation")
-            .help(
-                "DEPRECATED",
-            ),
-    )
-    .arg(
         Arg::with_name("accounts_shrink_optimize_total_space")
             .long("accounts-shrink-optimize-total-space")
             .takes_value(true)
