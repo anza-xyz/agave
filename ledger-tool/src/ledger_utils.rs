18765930da2dc414adfc09b72dfce4b6550d5307
jeff washington <jeff.washington@solana.com>
2025-07-11 15:52:13 -0500

    format

--- ledger-tool/src/ledger_utils.rs
+++ ledger-tool/src/ledger_utils.rs
@@ -404,11 +404,8 @@ pub fn load_and_process_ledger(
         snapshot_request_handler,
         pruned_banks_request_handler,
     };
-    let accounts_background_service = AccountsBackgroundService::new(
-        bank_forks.clone(),
-        exit.clone(),
-        abs_request_handler,
-    );
+    let accounts_background_service =
+        AccountsBackgroundService::new(bank_forks.clone(), exit.clone(), abs_request_handler);
 
     let result = blockstore_processor::process_blockstore_from_root(
         blockstore.as_ref(),
