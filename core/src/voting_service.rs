55e3b46ad02e48e1cd3bedd4638424c06ecac872
Alex Pyattaev <alex.pyattaev@anza.xyz>
2025-07-17 07:22:14 +0000

    explicitly release bank_forks lock

--- core/src/voting_service.rs
+++ core/src/voting_service.rs
@@ -118,7 +118,8 @@ impl VotingService {
                         // trigger mock alpenglow vote if we have just cast an actual vote
                         if let Some(slot) = vote_slot {
                             if let Some(ag) = mock_alpenglow.as_mut() {
-                                ag.signal_new_slot(slot, &bank_forks.read().unwrap().root_bank());
+                                let root_bank = { bank_forks.read().unwrap().root_bank() };
+                                ag.signal_new_slot(slot, &root_bank);
                             }
                         }
                     }
