63dc14cb1a908ebd25105361163222dce49d91b3
Ryo Onodera <ryoqun@gmail.com>
2025-07-22 18:24:57 +0900

    Improve ci stability with faster joining

--- unified-scheduler-pool/src/lib.rs
+++ unified-scheduler-pool/src/lib.rs
@@ -17,7 +17,9 @@ use qualifier_attr::qualifiers;
 use {
     agave_banking_stage_ingress_types::{BankingPacketBatch, BankingPacketReceiver},
     assert_matches::assert_matches,
-    crossbeam_channel::{self, never, select_biased, Receiver, RecvError, SendError, Sender},
+    crossbeam_channel::{
+        self, never, select_biased, Receiver, RecvError, RecvTimeoutError, SendError, Sender,
+    },
     dashmap::DashMap,
     derive_where::derive_where,
     dyn_clone::{clone_trait_object, DynClone},
@@ -130,6 +132,7 @@ pub struct SchedulerPool<S: SpawnableScheduler<TH>, TH: TaskHandler> {
     weak_self: Weak<Self>,
     next_scheduler_id: AtomicSchedulerId,
     max_usage_queue_count: usize,
+    scheduler_pool_sender: Sender<Weak<Self>>,
     cleaner_thread: JoinHandle<()>,
     _phantom: PhantomData<TH>,
 }
@@ -454,10 +457,12 @@ where
         let cleaner_main_loop = move || {
             info!("cleaner_main_loop: started...");
 
-            let weak_scheduler_pool: Weak<Self> =
-                scheduler_pool_receiver.into_iter().next().unwrap();
+            let weak_scheduler_pool: Weak<Self> = scheduler_pool_receiver.recv().unwrap();
             loop {
-                sleep(pool_cleaner_interval);
+                match scheduler_pool_receiver.recv_timeout(pool_cleaner_interval) {
+                    Ok(_) => unreachable!(),
+                    Err(RecvTimeoutError::Disconnected | RecvTimeoutError::Timeout) => (),
+                }
 
                 let Some(scheduler_pool) = weak_scheduler_pool.upgrade() else {
                     // this is the only safe termination point of cleaner_main_loop while all other
@@ -626,6 +631,7 @@ where
             weak_self: weak_self.clone(),
             next_scheduler_id: AtomicSchedulerId::default(),
             max_usage_queue_count,
+            scheduler_pool_sender: scheduler_pool_sender.clone(),
             cleaner_thread,
             _phantom: PhantomData,
         });
@@ -951,7 +957,7 @@ where
         // Drop impl, because of the need to take the ownership of the join handle of the cleaner
         // thread...
         let mut this = self;
-        let this: Self = loop {
+        let mut this: Self = loop {
             match Arc::try_unwrap(this) {
                 Ok(pool) => {
                     break pool;
@@ -966,6 +972,8 @@ where
                 }
             }
         };
+        // Accelerate cleaner thread joining by disconnection
+        this.scheduler_pool_sender = crossbeam_channel::bounded(1).0;
         this.cleaner_thread.join().unwrap();
 
         info!("SchedulerPool::uninstalled_from_bank_forks(): ...finished");
