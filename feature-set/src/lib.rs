d4453f32f731d16543c6515c1e8a60f9ef7928a0
Dean 利迪恩 <dean@bitping.com>
2025-08-07 22:51:38 +0800

    SIMD-1094: Deprecate rent exemption threshold

--- feature-set/src/lib.rs
+++ feature-set/src/lib.rs
@@ -159,6 +159,7 @@ impl FeatureSet {
             reenable_zk_elgamal_proof_program: self
                 .is_active(&reenable_zk_elgamal_proof_program::id()),
             raise_cpi_nesting_limit_to_8: self.is_active(&raise_cpi_nesting_limit_to_8::id()),
+            deprecate_rent_exemption_threshold: self.is_active(&deprecate_rent_exemption_threshold::id()),
         }
     }
 }
@@ -1125,6 +1126,10 @@ pub mod raise_cpi_nesting_limit_to_8 {
     solana_pubkey::declare_id!("6TkHkRmP7JZy1fdM6fg5uXn76wChQBWGokHBJzrLB3mj");
 }
 
+pub mod deprecate_rent_exemption_threshold {
+    solana_pubkey::declare_id!("rent6iVy6PDoViPBeJ6k5EJQrkj62h7DPyLbWGHwjrC");
+}
+
 pub static FEATURE_NAMES: LazyLock<AHashMap<Pubkey, &'static str>> = LazyLock::new(|| {
     [
         (secp256k1_program_enabled::id(), "secp256k1 program"),
@@ -1366,6 +1371,7 @@ pub static FEATURE_NAMES: LazyLock<AHashMap<Pubkey, &'static str>> = LazyLock::n
         (raise_block_limits_to_100m::id(), "SIMD-0286: Raise block limit to 100M"),
         (raise_account_cu_limit::id(), "SIMD-0306: Raise account CU limit to 40% max"),
         (raise_cpi_nesting_limit_to_8::id(), "SIMD-0296: Raise CPI nesting limit from 4 to 8"),
+        (deprecate_rent_exemption_threshold::id(), "SIMD-0194: Deprecate rent exemption threshold")
         /*************** ADD NEW FEATURES HERE ***************/
     ]
     .iter()
