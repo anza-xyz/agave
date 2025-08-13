ab17efa298347947cd30c742bc6ac74eb8ca41d9
hana <81144685+2501babe@users.noreply.github.com>
2025-08-13 09:37:22 -0700

    compute-budget: test all migrating builtins

--- builtins-default-costs/src/lib.rs
+++ builtins-default-costs/src/lib.rs
@@ -1,5 +1,8 @@
 #![cfg_attr(feature = "frozen-abi", feature(min_specialization))]
 #![allow(clippy::arithmetic_side_effects)]
+
+#[cfg(feature = "dev-context-only-utils")]
+use qualifier_attr::field_qualifiers;
 use {
     agave_feature_set::{self as feature_set},
     ahash::AHashMap,
@@ -11,6 +14,10 @@ use {
 };
 
 #[derive(Clone)]
+#[cfg_attr(
+    feature = "dev-context-only-utils",
+    field_qualifiers(core_bpf_migration_feature(pub), position(pub))
+)]
 pub struct MigratingBuiltinCost {
     core_bpf_migration_feature: Pubkey,
     // encoding positional information explicitly for migration feature item,
