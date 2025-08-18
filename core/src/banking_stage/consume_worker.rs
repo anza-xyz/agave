31024f2de85574782b3c1139140fa748667bca8a
Andrew Fitzgerald <apfitzge@gmail.com>
2025-08-18 14:16:38 -0500

    get back to work

--- core/src/banking_stage/consume_worker.rs
+++ core/src/banking_stage/consume_worker.rs
@@ -6,6 +6,7 @@ use {
     },
     crate::banking_stage::consumer::RetryableIndex,
     crossbeam_channel::{Receiver, SendError, Sender, TryRecvError},
+    solana_clock::DEFAULT_MS_PER_SLOT,
     solana_poh::poh_recorder::SharedWorkingBank,
     solana_runtime::bank::Bank,
     solana_runtime_transaction::transaction_with_meta::TransactionWithMeta,
@@ -69,17 +70,28 @@ impl<Tx: TransactionWithMeta> ConsumeWorker<Tx> {
     }
 
     pub fn run(self) -> Result<(), ConsumeWorkerError<Tx>> {
+        let mut last_recv = Instant::now();
         while !self.exit.load(Ordering::Relaxed) {
+            let now = Instant::now();
             match self.consume_receiver.try_recv() {
-                Ok(work) => match self.consume(work)? {
-                    ProcessingStatus::Processed => {}
-                    ProcessingStatus::CouldNotProcess(work) => {
-                        self.retry_drain(work)?;
+                Ok(work) => {
+                    last_recv = now;
+                    match self.consume(work)? {
+                        ProcessingStatus::Processed => {}
+                        ProcessingStatus::CouldNotProcess(work) => {
+                            self.retry_drain(work)?;
+                        }
                     }
-                },
+                }
                 Err(TryRecvError::Empty) => {
+                    // Don't sleep on empty immediately, only if we have not received
+                    // work for a while (1 slot).
+                    const SLEEP_THRESHOLD: Duration = Duration::from_millis(DEFAULT_MS_PER_SLOT);
+                    // Sleep a short duration between checks to avoid busy waiting.
                     const SLEEP_DURATION: Duration = Duration::from_millis(1);
-                    std::thread::sleep(SLEEP_DURATION);
+                    if now.duration_since(last_recv) > SLEEP_THRESHOLD {
+                        std::thread::sleep(SLEEP_DURATION);
+                    }
                 }
                 Err(err) => {
                     return Err(ConsumeWorkerError::from(err));
