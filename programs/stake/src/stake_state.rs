27bb0ad0e8963c33936e79477df482dc81c4047d
hana <81144685+2501babe@users.noreply.github.com>
2025-08-21 01:57:11 -0700

    second pass, i think everything else is required

--- programs/stake/src/stake_state.rs
+++ programs/stake/src/stake_state.rs
@@ -1,7 +1,4 @@
-//! Stake state
-//! * delegate stakes to vote accounts
-//! * keep track of rewards
-//! * own mining pools
+//! Stake state: helper functions for creating and parsing account data
 
 #[deprecated(
     since = "1.8.0",
@@ -10,23 +7,14 @@
 pub use solana_stake_interface::state::*;
 use {
     solana_account::{state_traits::StateMut, AccountSharedData, ReadableAccount},
-    solana_clock::{Clock, Epoch},
-    solana_instruction::error::InstructionError,
-    solana_program_runtime::invoke_context::InvokeContext,
+    solana_clock::Epoch,
     solana_pubkey::Pubkey,
     solana_rent::Rent,
     solana_sdk_ids::stake::id,
     solana_stake_interface::{
-        error::StakeError,
-        instruction::LockupArgs,
         stake_flags::StakeFlags,
-        stake_history::{StakeHistory, StakeHistoryEntry},
-        tools::{acceptable_reference_epoch_credits, eligible_for_deactivate_delinquent},
     },
-    solana_svm_log_collector::ic_msg,
-    solana_transaction_context::{BorrowedAccount, IndexOfAccount, InstructionContext},
-    solana_vote_interface::state::{VoteStateV3, VoteStateVersions},
-    std::{collections::HashSet, convert::TryFrom},
+    solana_vote_interface::state::VoteStateV3,
 };
 
 // utility function, used by Stakes, tests
@@ -54,7 +42,7 @@ pub fn meta_from(account: &AccountSharedData) -> Option<Meta> {
     from(account).and_then(|state: StakeStateV2| state.meta())
 }
 
-pub(crate) fn new_stake(
+fn new_stake(
     stake: u64,
     voter_pubkey: &Pubkey,
     vote_state: &VoteStateV3,
@@ -64,53 +52,6 @@ pub(crate) fn new_stake(
         delegation: Delegation::new(voter_pubkey, stake, activation_epoch),
         credits_observed: vote_state.credits(),
     }
-}
-
-// utility function, used by runtime::Stakes, tests
-pub fn new_stake_history_entry<'a, I>(
-    epoch: Epoch,
-    stakes: I,
-    history: &StakeHistory,
-    new_rate_activation_epoch: Option<Epoch>,
-) -> StakeHistoryEntry
-where
-    I: Iterator<Item = &'a Delegation>,
-{
-    stakes.fold(StakeHistoryEntry::default(), |sum, stake| {
-        sum + stake.stake_activating_and_deactivating(epoch, history, new_rate_activation_epoch)
-    })
-}
-
-// utility function, used by tests
-pub fn create_stake_history_from_delegations(
-    bootstrap: Option<u64>,
-    epochs: std::ops::Range<Epoch>,
-    delegations: &[Delegation],
-    new_rate_activation_epoch: Option<Epoch>,
-) -> StakeHistory {
-    let mut stake_history = StakeHistory::default();
-
-    let bootstrap_delegation = if let Some(bootstrap) = bootstrap {
-        vec![Delegation {
-            activation_epoch: u64::MAX,
-            stake: bootstrap,
-            ..Delegation::default()
-        }]
-    } else {
-        vec![]
-    };
-
-    for epoch in epochs {
-        let entry = new_stake_history_entry(
-            epoch,
-            delegations.iter().chain(bootstrap_delegation.iter()),
-            &stake_history,
-            new_rate_activation_epoch,
-        );
-        stake_history.add(epoch, entry);
-    }
-
-    stake_history
 }
 
 // genesis investor accounts
@@ -154,25 +95,6 @@ pub fn create_account(
         rent,
         lamports,
         Epoch::MAX,
-    )
-}
-
-// utility function, used by tests
-pub fn create_account_with_activation_epoch(
-    authorized: &Pubkey,
-    voter_pubkey: &Pubkey,
-    vote_account: &AccountSharedData,
-    rent: &Rent,
-    lamports: u64,
-    activation_epoch: Epoch,
-) -> AccountSharedData {
-    do_create_account(
-        authorized,
-        voter_pubkey,
-        vote_account,
-        rent,
-        lamports,
-        activation_epoch,
     )
 }
 
