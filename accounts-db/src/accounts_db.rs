fb5a745ed8e4c160683626f592896e12cbe0e4b5
HaoranYi <219428+HaoranYi@users.noreply.github.com>
2025-09-02 15:34:02 +0000

    pr: insert_with

--- accounts-db/src/accounts_db.rs
+++ accounts-db/src/accounts_db.rs
@@ -7175,8 +7175,7 @@ impl AccountsDb {
                     if account_info.is_zero_lamport() {
                         slot_offsets
                             .entry(*slot_alive)
-                            .or_default()
-                            .push(account_info.offset());
+                            .or_insert_with(|| vec![account_info.offset()]);
                     }
                 }
                 AccountsIndexScanResult::OnlyKeepInMemoryIfDirty
