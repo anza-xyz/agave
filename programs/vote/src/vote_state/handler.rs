153ea959b23178822f11cdf1ef279fa5e6825b26
Joe Caulfield <jcaulfield135@gmail.com>
2025-09-03 11:52:49 +0000

    review feedback: `TargetVoteState` enum

--- programs/vote/src/vote_state/handler.rs
+++ programs/vote/src/vote_state/handler.rs
@@ -29,14 +29,18 @@ pub enum VoteStateTargetVersion {
     V4,
 }
 
+enum TargetVoteState {
+    V3(VoteStateV3),
+    V4(VoteStateV4),
+}
+
 /// Vote state handler for
 /// * Deserializing vote state
 /// * Converting vote state in-memory to target version
 /// * Operating on the vote state data agnostically
 /// * Serializing the resulting state to the vote account
 pub struct VoteStateHandler {
-    state: VoteStateVersions,
-    target_version: VoteStateTargetVersion,
+    target_state: TargetVoteState,
 }
 
 impl VoteStateHandler {
@@ -46,41 +50,36 @@ impl VoteStateHandler {
         vote_account: &BorrowedInstructionAccount,
         target_version: VoteStateTargetVersion,
     ) -> Result<Self, InstructionError> {
-        let state = vote_account.get_state::<VoteStateVersions>().map(|state| {
-            match target_version {
-                VoteStateTargetVersion::V3 => {
-                    VoteStateVersions::V3(Box::new(state.convert_to_v3()))
-                }
-                VoteStateTargetVersion::V4 => {
-                    // Requires vote-interface release or local reimplementation.
-                    unimplemented!()
-                }
+        let state = vote_account.get_state::<VoteStateVersions>()?;
+        let target_state = match target_version {
+            VoteStateTargetVersion::V3 => TargetVoteState::V3(state.convert_to_v3()),
+            VoteStateTargetVersion::V4 => {
+                // Requires vote-interface release or local reimplementation.
+                unimplemented!()
             }
-        })?;
-        Ok(Self {
-            state,
-            target_version,
-        })
+        };
+        Ok(Self { target_state })
     }
 
     /// Check if the vote state is uninitialized.
     pub fn is_uninitialized(&self) -> bool {
-        self.state.is_uninitialized()
+        match &self.target_state {
+            TargetVoteState::V3(v3) => v3.node_pubkey == Pubkey::default(),
+            TargetVoteState::V4(v4) => v4.node_pubkey == Pubkey::default(),
+        }
     }
 
     pub fn authorized_withdrawer(&self) -> &Pubkey {
-        match self.state {
-            VoteStateVersions::V3(ref v3) => &v3.authorized_withdrawer,
-            // VoteStateVersions::V4(ref v4) => &v4.authorized_withdrawer,
-            _ => panic!("Version not supported for update"),
+        match &self.target_state {
+            TargetVoteState::V3(v3) => &v3.authorized_withdrawer,
+            TargetVoteState::V4(v4) => &v4.authorized_withdrawer,
         }
     }
 
     pub fn set_authorized_withdrawer(&mut self, authorized_withdrawer: Pubkey) {
-        match self.state {
-            VoteStateVersions::V3(ref mut v3) => v3.authorized_withdrawer = authorized_withdrawer,
-            // VoteStateVersions::V4(ref mut v4) => v4.authorized_withdrawer = authorized_withdrawer,
-            _ => panic!("Version not supported for update"),
+        match &mut self.target_state {
+            TargetVoteState::V3(v3) => v3.authorized_withdrawer = authorized_withdrawer,
+            TargetVoteState::V4(v4) => v4.authorized_withdrawer = authorized_withdrawer,
         }
     }
 
@@ -94,60 +93,64 @@ impl VoteStateHandler {
     where
         F: Fn(Pubkey) -> Result<(), InstructionError>,
     {
-        match self.state {
-            VoteStateVersions::V3(ref mut v3) => {
+        match &mut self.target_state {
+            TargetVoteState::V3(v3) => {
                 v3.set_new_authorized_voter(authorized_pubkey, current_epoch, target_epoch, verify)
             }
-            // VoteStateVersions::V4(ref mut v4) => {
-            //     v4.set_new_authorized_voter(authorized_pubkey, current_epoch, target_epoch, verify)
-            // }
-            _ => panic!("Version not supported for update"),
+            TargetVoteState::V4(_v4) => {
+                // V4 implementation not yet available
+                unimplemented!("V4 vote state not yet implemented")
+            }
         }
     }
 
     pub fn commission(&self) -> u8 {
-        match self.state {
-            VoteStateVersions::V3(ref v3) => v3.commission,
-            // VoteStateVersions::V4(ref v4) => {
-            //     // For v4, return the inflation rewards commission converted back to u8
-            //     (v4.inflation_rewards_commission_bps / 100) as u8
-            // }
-            _ => panic!("Version not supported for update"),
+        match &self.target_state {
+            TargetVoteState::V3(v3) => v3.commission,
+            TargetVoteState::V4(_v4) => {
+                // V4 implementation not yet available
+                unimplemented!("V4 vote state not yet implemented")
+            }
         }
     }
 
     pub fn set_commission(&mut self, commission: u8) {
-        match self.state {
-            VoteStateVersions::V3(ref mut v3) => v3.commission = commission,
-            // VoteStateVersions::V4(ref mut v4) => {
-            //     // For v4, set the inflation rewards commission in basis points
-            //     v4.inflation_rewards_commission_bps = u16::from(commission).saturating_mul(100);
-            // }
-            _ => panic!("Version not supported for update"),
+        match &mut self.target_state {
+            TargetVoteState::V3(v3) => v3.commission = commission,
+            TargetVoteState::V4(_v4) => {
+                // V4 implementation not yet available
+                unimplemented!("V4 vote state not yet implemented")
+            }
         }
     }
 
     pub fn node_pubkey(&self) -> &Pubkey {
-        match self.state {
-            VoteStateVersions::V3(ref v3) => &v3.node_pubkey,
-            // VoteStateVersions::V4(ref v4) => &v4.node_pubkey,
-            _ => panic!("Version not supported for update"),
+        match &self.target_state {
+            TargetVoteState::V3(v3) => &v3.node_pubkey,
+            TargetVoteState::V4(_v4) => {
+                // V4 implementation not yet available
+                unimplemented!("V4 vote state not yet implemented")
+            }
         }
     }
 
     pub fn set_node_pubkey(&mut self, node_pubkey: Pubkey) {
-        match self.state {
-            VoteStateVersions::V3(ref mut v3) => v3.node_pubkey = node_pubkey,
-            // VoteStateVersions::V4(ref mut v4) => v4.node_pubkey = node_pubkey,
-            _ => panic!("Version not supported for update"),
+        match &mut self.target_state {
+            TargetVoteState::V3(v3) => v3.node_pubkey = node_pubkey,
+            TargetVoteState::V4(_v4) => {
+                // V4 implementation not yet available
+                unimplemented!("V4 vote state not yet implemented")
+            }
         }
     }
 
     pub fn epoch_credits_last(&self) -> Option<&(Epoch, u64, u64)> {
-        match self.state {
-            VoteStateVersions::V3(ref v3) => v3.epoch_credits.last(),
-            // VoteStateVersions::V4(ref v4) => v4.epoch_credits.last(),
-            _ => panic!("Version not supported for update"),
+        match &self.target_state {
+            TargetVoteState::V3(v3) => v3.epoch_credits.last(),
+            TargetVoteState::V4(_v4) => {
+                // V4 implementation not yet available
+                unimplemented!("V4 vote state not yet implemented")
+            }
         }
     }
 
@@ -156,28 +159,31 @@ impl VoteStateHandler {
         &self,
         vote_account: &mut BorrowedInstructionAccount,
     ) -> Result<(), InstructionError> {
-        // If the account is not large enough to store the vote state, then attempt a realloc to make it large enough.
-        // The realloc can only proceed if the vote account has balance sufficient for rent exemption at the new size.
-        if (vote_account.get_data().len() < VoteStateVersions::vote_state_size_of(true))
-            && (!vote_account
-                .is_rent_exempt_at_data_length(VoteStateVersions::vote_state_size_of(true))
-                || vote_account
-                    .set_data_length(VoteStateVersions::vote_state_size_of(true))
-                    .is_err())
-        {
-            match &self.state {
-                VoteStateVersions::V3(v3) => {
+        match &self.target_state {
+            TargetVoteState::V3(v3) => {
+                // If the account is not large enough to store the vote state, then attempt a realloc to make it large enough.
+                // The realloc can only proceed if the vote account has balance sufficient for rent exemption at the new size.
+                if (vote_account.get_data().len() < VoteStateVersions::vote_state_size_of(true))
+                    && (!vote_account
+                        .is_rent_exempt_at_data_length(VoteStateVersions::vote_state_size_of(true))
+                        || vote_account
+                            .set_data_length(VoteStateVersions::vote_state_size_of(true))
+                            .is_err())
+                {
                     // Account cannot be resized to the size of a vote state as it will not be rent exempt, or failed to be
                     // resized for other reasons.  So store the V1_14_11 version.
                     return vote_account.set_state(&VoteStateVersions::V1_14_11(Box::new(
-                        VoteState1_14_11::from(v3.deref().to_owned()),
+                        VoteState1_14_11::from(v3.clone()),
                     )));
                 }
-                _ => panic!("Version not supported for V1_14_11 conversion"),
-            };
+                // Vote account is large enough to store the newest version of vote state
+                vote_account.set_state(&VoteStateVersions::V3(Box::new(v3.clone())))
+            }
+            TargetVoteState::V4(_v4) => {
+                // V4 implementation not yet available
+                unimplemented!("V4 vote state not yet implemented")
+            }
         }
-        // Vote account is large enough to store the newest version of vote state
-        vote_account.set_state(&self.state)
     }
 
     pub fn init_vote_account_state(
