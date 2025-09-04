720b11fae90ffd0b901b26a7f37ace37b1f0977f
Brennan <brennan.watt@anza.xyz>
2025-09-04 20:42:21 +0000

    add votor-messages

---
+++ votor-messages/src/lib.rs
@@ -0,0 +1,12 @@
+//! Alpenglow Vote program
+#![cfg_attr(feature = "frozen-abi", feature(min_specialization))]
+#![deny(missing_docs)]
+
+pub mod consensus_message;
+pub mod vote;
+
+#[cfg_attr(feature = "frozen-abi", macro_use)]
+#[cfg(feature = "frozen-abi")]
+extern crate solana_frozen_abi_macro;
+
+solana_program::declare_id!("Vote222222222222222222222222222222222222222");
