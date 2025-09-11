5e809055e6b1fd5ea8e851924fe7f68c6d6a4f65
Kirill Lykov <lykov.kirill@gmail.com>
2025-09-11 21:51:13 +0200

    introduce cancel to Streamer

--- core/src/validator.rs
+++ core/src/validator.rs
@@ -736,8 +736,8 @@ impl Validator {
         timer.stop();
         info!("Cleaning orphaned account snapshot directories done. {timer}");
 
-        // token used to cancel tpu-client-next.
-        let cancel_tpu_client_next = CancellationToken::new();
+        // token used to cancel tpu-client-next and streamer.
+        let cancel = CancellationToken::new();
         {
             let exit = exit.clone();
             config
@@ -745,12 +745,12 @@ impl Validator {
                 .write()
                 .unwrap()
                 .register_exit(Box::new(move || exit.store(true, Ordering::Relaxed)));
-            let cancel_tpu_client_next = cancel_tpu_client_next.clone();
+            let cancel = cancel.clone();
             config
                 .validator_exit
                 .write()
                 .unwrap()
-                .register_exit(Box::new(move || cancel_tpu_client_next.cancel()));
+                .register_exit(Box::new(move || cancel.cancel()));
         }
 
         let (
@@ -1175,7 +1175,7 @@ impl Validator {
                     Arc::as_ref(&identity_keypair),
                     node.sockets.rpc_sts_client,
                     runtime_handle.clone(),
-                    cancel_tpu_client_next.clone(),
+                    cancel.clone(),
                 )
             } else {
                 let Some(connection_cache) = &connection_cache else {
@@ -1630,7 +1630,7 @@ impl Validator {
                 Arc::as_ref(&identity_keypair),
                 tpu_transactions_forwards_client_sockets.take().unwrap(),
                 runtime_handle.clone(),
-                cancel_tpu_client_next,
+                cancel.clone(),
                 node_multihoming.clone(),
             ))
         };
@@ -1687,6 +1687,7 @@ impl Validator {
             config.enable_block_production_forwarding,
             config.generator_config.clone(),
             key_notifiers.clone(),
+            cancel,
         );
 
         datapoint_info!(
