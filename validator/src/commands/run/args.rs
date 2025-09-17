b7dc774d5900ba2d1ee8b0f3b4b11a3bc19d1dbe
yihau <yihau.chen@icloud.com>
2025-09-17 09:43:57 +0800

    --accounts-db-clean-threads and --accounts-db-background-threads

--- validator/src/commands/run/args.rs
+++ validator/src/commands/run/args.rs
@@ -1700,6 +1700,7 @@ mod tests {
         std::{
             fs,
             net::{IpAddr, Ipv4Addr},
+            num::NonZeroUsize,
             path::{absolute, PathBuf},
         },
     };
@@ -1746,6 +1747,10 @@ mod tests {
                         ..AccountsIndexConfig::default()
                     }),
                     account_indexes: Some(AccountSecondaryIndexes::default()),
+                    num_background_threads: Some(
+                        NonZeroUsize::new(solana_accounts_db::accounts_db::quarter_thread_count())
+                            .unwrap(),
+                    ),
                     ..AccountsDbConfig::default()
                 },
             }
