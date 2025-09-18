373cf63b3028adad8addf59d198805462d826eb2
marc <marcinbieszke@gmail.com>
2025-09-18 08:30:30 +0200

    remove option from chained_merkle_root param in setup_erasure_shreds_with_index_and_chained_merkle_and_last_in_slot

--- ledger/src/blockstore.rs
+++ ledger/src/blockstore.rs
@@ -10145,7 +10145,7 @@ pub mod tests {
             parent_slot,
             num_entries,
             fec_set_index,
-            Some(chained_merkle_root),
+            chained_merkle_root,
             true,
         )
     }
@@ -10165,7 +10165,7 @@ pub mod tests {
             &leader_keypair,
             &entries,
             is_last_in_slot,
-            chained_merkle_root,
+            Some(chained_merkle_root),
             fec_set_index, // next_shred_index
             fec_set_index, // next_code_index
             &ReedSolomonCache::default(),
