72615864564f27a2cb86d4a3be9e14d24bbc4b59
Wen <113942165+wen-coding@users.noreply.github.com>
2025-09-29 12:46:54 -0700

    (Alpenglow) Upstream rest of votor crate.

--- votor/src/lib.rs
+++ votor/src/lib.rs
@@ -1,34 +1,28 @@
 #![cfg_attr(feature = "frozen-abi", feature(min_specialization))]
 
+#[macro_use]
+extern crate log;
+extern crate serde_derive;
+
 #[cfg(feature = "agave-unstable-api")]
 pub mod commitment;
 
 #[cfg(feature = "agave-unstable-api")]
 pub mod common;
 
-#[cfg(feature = "agave-unstable-api")]
 mod consensus_metrics;
-
-#[cfg(feature = "agave-unstable-api")]
-pub mod consensus_pool;
+mod consensus_pool;
+mod consensus_pool_service;
 
 #[cfg(feature = "agave-unstable-api")]
 pub mod event;
 
+mod event_handler;
+
 #[cfg(feature = "agave-unstable-api")]
 pub mod root_utils;
 
-#[cfg(feature = "agave-unstable-api")]
-#[macro_use]
-extern crate log;
-
-#[cfg(feature = "agave-unstable-api")]
-extern crate serde_derive;
-
-#[cfg(feature = "agave-unstable-api")]
 mod staked_validators_cache;
-
-#[cfg(feature = "agave-unstable-api")]
 mod timer_manager;
 
 #[cfg(feature = "agave-unstable-api")]
@@ -36,11 +30,11 @@ pub mod vote_history;
 #[cfg(feature = "agave-unstable-api")]
 pub mod vote_history_storage;
 
-#[cfg(feature = "agave-unstable-api")]
 mod voting_service;
+mod voting_utils;
 
 #[cfg(feature = "agave-unstable-api")]
-mod voting_utils;
+pub mod votor;
 
 #[cfg_attr(feature = "frozen-abi", macro_use)]
 #[cfg(feature = "frozen-abi")]
