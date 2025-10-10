124a41ec037169e55e8363d5634fa55e1a63b10a
Charles Moyes <cmoyes@jumptrading.com>
2025-10-10 15:13:22 +0000

    Use Cargo links

--- svm-fuzz-harness/build.rs
+++ svm-fuzz-harness/build.rs
@@ -1,18 +1,33 @@
-use std::io::Result;
+use std::{env, fs, path::PathBuf};
 
-fn main() -> Result<()> {
-    let proto_base_path = std::path::PathBuf::from("protosol/proto");
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Get absolute proto dir from producer
+    let proto_dir = PathBuf::from(
+        env::var("DEP_PROTOSOL_PROTO_DIR")
+            .expect("protosol did not expose PROTO_DIR"),
+    );
 
-    let protos = &[
-        proto_base_path.join("context.proto"),
-        proto_base_path.join("invoke.proto"),
-    ];
+    println!("cargo:rerun-if-env-changed=DEP_PROTOSOL_PROTO_DIR");
 
-    protos
-        .iter()
-        .for_each(|proto| println!("cargo:rerun-if-changed={}", proto.display()));
+    // Collect absolute .proto paths
+    let mut proto_files = vec![];
+    for entry in fs::read_dir(&proto_dir)? {
+        let path = entry?.path();
+        if path.extension().and_then(|e| e.to_str()) == Some("proto") {
+            println!("cargo:rerun-if-changed={}", path.display());
+            proto_files.push(path);
+        }
+    }
 
-    prost_build::Config::new().compile_protos(protos, &[proto_base_path])?;
+    // Compile protos into Rust
+    let out_dir = PathBuf::from(env::var("OUT_DIR")?);
+    let mut config = prost_build::Config::new();
+    config.out_dir(&out_dir);
+
+    config.compile_protos(
+        &proto_files.iter().map(|p| p.display().to_string()).collect::<Vec<_>>(),
+        &[proto_dir.to_str().unwrap()],
+    )?;
 
     Ok(())
 }
