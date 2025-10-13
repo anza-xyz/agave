5295d5527a34c6b7fe85edf9cb97a3668b248f12
Charles Moyes <cmoyes@jumptrading.com>
2025-10-13 15:42:26 +0000

    Move path normalization back up to producer

--- svm-fuzz-harness/build.rs
+++ svm-fuzz-harness/build.rs
@@ -1,33 +1,11 @@
-use std::{
-    env, fs,
-    path::{Path, PathBuf},
-};
-
-fn maybe_normalize_windows_path(path: &Path) -> PathBuf {
-    #[cfg(windows)]
-    {
-        // UNC paths are not supported by prost_build, so we need to re-normalize them.
-        const EXTENDED_PREFIX: &str = r"\\?\";
-        let s = path.display().to_string();
-        if s.starts_with(EXTENDED_PREFIX) {
-            PathBuf::from(&s[EXTENDED_PREFIX.len()..])
-        } else {
-            path.to_path_buf()
-        }
-    }
-
-    #[cfg(not(windows))]
-    {
-        path.to_path_buf()
-    }
-}
+use std::{env, fs, path::PathBuf};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Get absolute proto dir from producer
-    let proto_dir = maybe_normalize_windows_path(&PathBuf::from(
+    let proto_dir = PathBuf::from(
         env::var("DEP_PROTOSOL_PROTO_DIR")
             .expect("protosol did not expose PROTO_DIR, did protosol build.rs run first?"),
-    ));
+    );
 
     println!("cargo:rerun-if-env-changed=DEP_PROTOSOL_PROTO_DIR");
     println!("cargo:rerun-if-changed={}", proto_dir.display());
