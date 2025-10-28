da392c1286cd246eab8853e5627b34b40b7a541c
ra <40503841+RadiantAeon@users.noreply.github.com>
2025-10-28 21:08:28 +0000

    revert to loading keypair from cluster info when calling push_vote

--- gossip/src/cluster_info.rs
+++ gossip/src/cluster_info.rs
@@ -861,7 +861,8 @@ impl ClusterInfo {
         }
     }
 
-    pub fn push_vote(&self, tower: &[Slot], vote: Transaction, keypair: &Keypair) {
+    pub fn push_vote(&self, tower: &[Slot], vote: Transaction) {
+        let keypair = self.keypair();
         debug_assert!(tower.iter().tuple_windows().all(|(a, b)| a < b));
         // Find the oldest crds vote by wallclock that has a lower slot than `tower`
         // and recycle its vote-index. If the crds buffer is not full we instead add a new vote-index.
@@ -879,7 +880,7 @@ impl ClusterInfo {
             );
         };
         debug_assert!(vote_index < MAX_VOTES);
-        self.push_vote_at_index(vote, vote_index, keypair);
+        self.push_vote_at_index(vote, vote_index, &keypair);
     }
 
     pub fn refresh_vote(&self, refresh_vote: Transaction, refresh_vote_slot: Slot) {
@@ -3031,7 +3032,7 @@ mod tests {
             if first_vote.is_none() {
                 first_vote = Some(vote_tx.clone());
             }
-            cluster_info.push_vote(&prev_votes, vote_tx, &keypair);
+            cluster_info.push_vote(&prev_votes, vote_tx);
         }
 
         let initial_votes = cluster_info.get_votes(&mut Cursor::default());
@@ -3095,7 +3096,7 @@ mod tests {
             &[unrefresh_ix], // instructions
             None,            // payer
         );
-        cluster_info.push_vote(&unrefresh_tower, unrefresh_tx.clone(), &keypair);
+        cluster_info.push_vote(&unrefresh_tower, unrefresh_tx.clone());
         let mut cursor = Cursor::default();
         let votes = cluster_info.get_votes(&mut cursor);
         assert_eq!(votes, vec![unrefresh_tx.clone()]);
@@ -3184,7 +3185,7 @@ mod tests {
             None,  // payer
         );
         let tower = vec![7]; // Last slot in the vote.
-        cluster_info.push_vote(&tower, tx.clone(), &keypair);
+        cluster_info.push_vote(&tower, tx.clone());
 
         let (labels, votes) = cluster_info.get_votes_with_labels(&mut cursor);
         assert_eq!(votes, vec![tx]);
@@ -3239,7 +3240,7 @@ mod tests {
             let slot = k as Slot;
             tower.push(slot);
             let vote = new_vote_transaction(vec![slot]);
-            cluster_info.push_vote(&tower, vote, &keypair);
+            cluster_info.push_vote(&tower, vote);
 
             let vote_slots = get_vote_slots(&cluster_info);
             let min_vote = k.saturating_sub(MAX_VOTES as usize - 1) as u64;
