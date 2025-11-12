90a942f83da696520d527d1da5a3cb258cfc6563
ANISH-SR <rawatanish08@gmail.com>
2025-11-12 18:54:32 +0530

    refactor(streamer): remove socket.rs and cleanup

--- streamer/src/socket.rs
+++
@@ -1,3 +0,0 @@
-// Re-export SocketAddrSpace from net-utils for backward compatibility.
-// This allows existing code using `solana_streamer::socket::SocketAddrSpace` to continue working.
-pub use solana_net_utils::SocketAddrSpace;
