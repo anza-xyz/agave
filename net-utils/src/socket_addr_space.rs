22c8885e3d110000ab257a476730049e560b8e03
ANISH-SR <rawatanish08@gmail.com>
2025-11-14 23:18:14 +0530

    refactor: migrate SocketAddrSpace and drop obsolete streamer usage

--- net-utils/src/socket_addr_space.rs
+++ net-utils/src/socket_addr_space.rs
@@ -15,61 +15,27 @@ impl SocketAddrSpace {
         }
     }
 
+    /// Returns true if the IP address is valid.
     #[inline]
     #[must_use]
     pub fn check(&self, addr: &SocketAddr) -> bool {
         if matches!(self, SocketAddrSpace::Unspecified) {
             return true;
         }
+
+        // TODO: remove these once IpAddr::is_global is stable.
         match addr.ip() {
             IpAddr::V4(addr) => {
-                !(addr.is_private()
-                    || addr.is_loopback()
-                    || addr.is_link_local()
-                    || addr.is_broadcast()
-                    || addr.is_documentation()
-                    || addr.is_unspecified()
-                    || addr.is_multicast())
+                // TODO: Consider excluding:
+                // addr.is_link_local() || addr.is_broadcast()
+                // || addr.is_documentation() || addr.is_unspecified()
+                !(addr.is_private() || addr.is_loopback())
             }
             IpAddr::V6(addr) => {
-                !(addr.is_loopback()
-                    || addr.is_unspecified()
-                    || addr.is_multicast()
-                    || addr.is_unicast_link_local())
+                // TODO: Consider excluding:
+                // addr.is_unspecified(),
+                !addr.is_loopback()
             }
-        }
-    }
-
-    #[inline]
-    #[must_use]
-    pub fn is_multicast(&self, addr: &SocketAddr) -> bool {
-        match addr.ip() {
-            IpAddr::V4(addr) => addr.is_multicast(),
-            IpAddr::V6(addr) => addr.is_multicast(),
-        }
-    }
-
-    #[inline]
-    #[must_use]
-    pub fn is_private(&self, addr: &SocketAddr) -> bool {
-        match addr.ip() {
-            IpAddr::V4(addr) => addr.is_private(),
-            IpAddr::V6(addr) => addr.is_unicast_link_local() || addr.is_unique_local(),
-        }
-    }
-
-    #[inline]
-    #[must_use]
-    pub fn is_loopback(&self, addr: &SocketAddr) -> bool {
-        addr.ip().is_loopback()
-    }
-
-    #[inline]
-    #[must_use]
-    pub fn is_link_local(&self, addr: &SocketAddr) -> bool {
-        match addr.ip() {
-            IpAddr::V4(addr) => addr.is_link_local(),
-            IpAddr::V6(addr) => addr.is_unicast_link_local(),
         }
     }
 }
@@ -107,42 +73,16 @@ mod tests {
     }
 
     #[test]
-    fn test_is_multicast() {
-        let space = SocketAddrSpace::Global;
-        assert!(space.is_multicast(&SocketAddr::from(([224, 0, 0, 1], 8080))));
-        assert!(!space.is_multicast(&SocketAddr::from(([8, 8, 8, 8], 53))));
-    }
-
-    #[test]
-    fn test_is_private() {
-        let space = SocketAddrSpace::Global;
-        assert!(space.is_private(&SocketAddr::from(([10, 0, 0, 1], 8080))));
-        assert!(space.is_private(&SocketAddr::from(([192, 168, 1, 1], 8080))));
-        assert!(!space.is_private(&SocketAddr::from(([8, 8, 8, 8], 53))));
-    }
-
-    #[test]
-    fn test_is_loopback() {
-        let space = SocketAddrSpace::Global;
-        assert!(space.is_loopback(&SocketAddr::from(([127, 0, 0, 1], 8080))));
-        assert!(!space.is_loopback(&SocketAddr::from(([8, 8, 8, 8], 53))));
-    }
-
-    #[test]
-    fn test_is_link_local() {
-        let space = SocketAddrSpace::Global;
-        assert!(space.is_link_local(&SocketAddr::from(([169, 254, 0, 1], 8080))));
-        assert!(!space.is_link_local(&SocketAddr::from(([8, 8, 8, 8], 53))));
-    }
-
-    #[test]
     fn test_ipv6_addresses() {
         let space = SocketAddrSpace::Global;
+
         assert!(!space.check(&SocketAddr::new(IpAddr::V6(Ipv6Addr::LOCALHOST), 8080)));
+
         assert!(!space.check(&SocketAddr::new(
             IpAddr::V6(Ipv6Addr::new(0xff00, 0, 0, 0, 0, 0, 0, 1)),
             8080
         )));
+
         assert!(!space.check(&SocketAddr::new(
             IpAddr::V6(Ipv6Addr::new(0xfe80, 0, 0, 0, 0, 0, 0, 1)),
             8080
--- streamer/src/socket.rs
+++
@@ -1,3 +0,0 @@
-// Intentionally empty.
-// The `socket` module previously hosted `SocketAddrSpace`.
-// That type has moved to `solana_net_utils`.
