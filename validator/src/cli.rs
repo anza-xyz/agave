ec2758b4da7b554c1831dd2ef6f2027522e15389
Alex Pyattaev <alex.pyattaev@anza.xyz>
2025-11-14 18:13:41 +0000

    fix order of args

--- validator/src/cli.rs
+++ validator/src/cli.rs
@@ -129,14 +129,6 @@ fn deprecated_arguments() -> Vec<DeprecatedArg> {
     }
 
     add_arg!(
-        // deprecated in v3.1.1
-        Arg::with_name("cuda")
-            .long("cuda")
-            .takes_value(false)
-            .help("Use CUDA"),
-        usage_warning: "CUDA support will be dropped"
-    );
-    add_arg!(
         // deprecated in v3.0.0
         Arg::with_name("accounts_db_clean_threads")
             .long("accounts-db-clean-threads")
@@ -186,6 +178,14 @@ fn deprecated_arguments() -> Vec<DeprecatedArg> {
                  [default: <LEDGER>/accounts_hash_cache]",
             ),
             usage_warning: "The accounts hash cache is obsolete",
+    );
+    add_arg!(
+        // deprecated in v3.1.1
+        Arg::with_name("cuda")
+            .long("cuda")
+            .takes_value(false)
+            .help("Use CUDA"),
+        usage_warning: "CUDA support will be dropped"
     );
     add_arg!(Arg::with_name("disable_accounts_disk_index")
         // (actually) deprecated in v3.1.0
