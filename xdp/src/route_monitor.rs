e7ad3bd724a154e3b0114c294087b2c72115f153
greg <greg@anza.xyz>
2025-11-21 18:22:25 +0000

    remove all route neighbor filtering

--- xdp/src/route_monitor.rs
+++ xdp/src/route_monitor.rs
@@ -1,18 +1,12 @@
 use {
     crate::{
-        netlink::{
-            parse_rtm_newneigh, parse_rtm_newroute, NeighborEntry, NetlinkMessage, NetlinkSocket,
-            RouteEntry,
-        },
+        netlink::{parse_rtm_newneigh, parse_rtm_newroute, NetlinkMessage, NetlinkSocket},
         route::Router,
     },
     arc_swap::ArcSwap,
     libc::{
-        self, pollfd, AF_INET, NUD_PERMANENT, NUD_REACHABLE, NUD_STALE, POLLERR, POLLHUP, POLLIN,
-        POLLNVAL, RTMGRP_IPV4_ROUTE, RTMGRP_NEIGH, RTM_DELNEIGH, RTM_DELROUTE, RTM_F_CLONED,
-        RTM_NEWNEIGH, RTM_NEWROUTE, RTN_ANYCAST, RTN_BLACKHOLE, RTN_BROADCAST, RTN_LOCAL,
-        RTN_MULTICAST, RTN_NAT, RTN_PROHIBIT, RTN_THROW, RTN_UNICAST, RTN_UNREACHABLE, RTN_UNSPEC,
-        RTN_XRESOLVE, RT_TABLE_DEFAULT, RT_TABLE_LOCAL, RT_TABLE_MAIN, RT_TABLE_UNSPEC,
+        self, pollfd, POLLERR, POLLHUP, POLLIN, POLLNVAL, RTMGRP_IPV4_ROUTE, RTMGRP_NEIGH,
+        RTM_DELNEIGH, RTM_DELROUTE, RTM_F_CLONED, RTM_NEWNEIGH, RTM_NEWROUTE,
     },
     log::*,
     std::{
@@ -81,10 +75,7 @@ impl RouteMonitor {
                     // Drain channel
                     match state.sock.recv() {
                         Ok(msgs) => {
-                            if !msgs.is_empty() {
-                                state.dirty |=
-                                    Self::process_netlink_updates(&mut state.router, &msgs);
-                            }
+                            state.dirty |= Self::process_netlink_updates(&mut state.router, &msgs);
                         }
                         Err(e) => {
                             error!("netlink recv error: {e}");
@@ -104,14 +95,14 @@ impl RouteMonitor {
             match m.header.nlmsg_type {
                 RTM_NEWROUTE => {
                     if let Some(r) = parse_rtm_newroute(m) {
-                        if r.family as i32 == AF_INET && is_valid_route_entry(&r) {
+                        if r.flags & RTM_F_CLONED == 0 {
                             dirty |= router.upsert_route(r);
                         }
                     }
                 }
                 RTM_DELROUTE => {
                     if let Some(r) = parse_rtm_newroute(m) {
-                        if r.family as i32 == AF_INET && is_valid_route_entry(&r) {
+                        if r.flags & RTM_F_CLONED == 0 {
                             dirty |= router.remove_route(r);
                         }
                     }
@@ -119,18 +110,14 @@ impl RouteMonitor {
                 RTM_NEWNEIGH => {
                     if let Some(n) = parse_rtm_newneigh(m, None) {
                         if let Some(IpAddr::V4(_)) = n.destination {
-                            if is_valid_neighbor_entry(&n) {
-                                dirty |= router.upsert_neighbor(n);
-                            }
+                            dirty |= router.upsert_neighbor(n);
                         }
                     }
                 }
                 RTM_DELNEIGH => {
                     if let Some(n) = parse_rtm_newneigh(m, None) {
                         if let Some(IpAddr::V4(ip)) = n.destination {
-                            if is_valid_neighbor_entry(&n) {
-                                dirty |= router.remove_neighbor(ip, n.ifindex as u32);
-                            }
+                            dirty |= router.remove_neighbor(ip, n.ifindex as u32);
                         }
                     }
                 }
@@ -190,53 +177,4 @@ fn poll(pfd: &mut pollfd, timeout: Duration) -> Result<i32, Error> {
         return Err(Error::last_os_error());
     }
     Ok(rc)
-}
-
-#[inline]
-fn is_valid_route_entry(r: &RouteEntry) -> bool {
-    if r.flags & RTM_F_CLONED != 0 {
-        return false;
-    }
-    if !match r.table {
-        None => true,
-        Some(t) => is_supported_route_table_id(t),
-    } {
-        return false;
-    }
-    is_supported_route_type(r.type_)
-}
-
-/// Returns true if this neighbor entry is valid and usable
-#[inline]
-fn is_valid_neighbor_entry(n: &NeighborEntry) -> bool {
-    n.lladdr.is_some() && (n.state & (NUD_REACHABLE | NUD_PERMANENT | NUD_STALE)) != 0
-}
-
-// Removes cloned routes, non-main/local table routes, and invalid route types
-// Many invisible routes may be inserted, we need to remove them.
-#[inline]
-fn is_supported_route_type(ty: u8) -> bool {
-    matches!(
-        ty,
-        RTN_UNICAST
-            | RTN_LOCAL
-            | RTN_ANYCAST
-            | RTN_BROADCAST
-            | RTN_MULTICAST
-            | RTN_BLACKHOLE
-            | RTN_THROW
-            | RTN_UNREACHABLE
-            | RTN_PROHIBIT
-            | RTN_NAT
-            | RTN_XRESOLVE
-            | RTN_UNSPEC
-    )
-}
-
-#[inline]
-fn is_supported_route_table_id(table: u32) -> bool {
-    table == RT_TABLE_MAIN as u32
-        || table == RT_TABLE_LOCAL as u32
-        || table == RT_TABLE_DEFAULT as u32
-        || table == RT_TABLE_UNSPEC as u32
 }
