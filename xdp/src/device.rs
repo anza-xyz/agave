411bc1ea033abb766986fb6d065019a21bddeb52
nuel77 <emmanuel.thomas237@gmail.com>
2025-11-28 14:39:24 +0000

    add size getters

--- xdp/src/device.rs
+++ xdp/src/device.rs
@@ -101,8 +101,8 @@ impl NetworkDevice {
             unsafe {
                 slice::from_raw_parts(req.ifr_ifru.ifru_hwaddr.sa_data.as_ptr() as *const u8, 6)
             }
-                .try_into()
-                .unwrap(),
+            .try_into()
+            .unwrap(),
         ))
     }
 
@@ -271,7 +271,7 @@ impl RingConsumer {
             consumer,
             cached_consumer: unsafe { (*consumer).load(Ordering::Relaxed) },
         }
-    }x
+    }
 
     pub fn available(&self) -> u32 {
         self.cached_producer.wrapping_sub(self.cached_consumer)
