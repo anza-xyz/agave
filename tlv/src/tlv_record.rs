d6593568081a69126dbd0e181d524b3beec9db6f
Alex Pyattaev <alex.pyattaev@anza.xyz>
2025-12-01 22:13:21 +0000

    zach

--- tlv/src/tlv_record.rs
+++ tlv/src/tlv_record.rs
@@ -1,8 +1,7 @@
 use {
-    crate::{TlvSerialize, MAX_VALUE_LENGTH},
+    crate::{MAX_VALUE_LENGTH, TlvSerialize},
     bytes::BytesMut,
-    std::mem::MaybeUninit,
-    wincode::{containers, io::Reader, len::ShortU16Len, SchemaRead, SchemaWrite},
+    wincode::{SchemaRead, SchemaWrite, containers, io::Reader, len::ShortU16Len},
 };
 
 /// Type-Length-Value encoded entry.
@@ -44,7 +43,7 @@ impl TlvRecord {
     /// If this returns None, the record was not properly formatted,
     /// and one must assume the rest of the buffer is not readable.
     pub fn deserialize<'a>(buffer: &mut impl Reader<'a>) -> Option<Self> {
-        Some(Self::get(buffer).ok()?);
+        Some(Self::get(buffer).ok()?)
     }
 }
 
@@ -94,6 +93,13 @@ mod tests {
     use {
         crate::{TlvEncodeError, TlvRecord, TlvSerialize},
         bytes::BytesMut,
+        solana_short_vec::decode_shortu16_len,
+        std::mem::{MaybeUninit, ptr, transmute},
+        wincode::{
+            ReadResult, SchemaRead, SchemaWrite, WriteError, WriteResult,
+            error::{read_length_encoding_overflow, write_length_encoding_overflow},
+            io::Reader,
+        },
     };
 
     #[test]
@@ -146,5 +152,204 @@ mod tests {
             assert_eq!(cursor.position(), rec.serialized_size());
             assert_eq!(received.value(), rec.value());
         }
+    }
+
+    #[derive(Debug)]
+    #[repr(transparent)]
+    pub struct ShortU16(u16);
+
+    impl From<ShortU16> for usize {
+        fn from(value: ShortU16) -> Self {
+            value.0 as usize
+        }
+    }
+
+    /// Branchless computation of the number of bytes needed to encode a short u16.
+    ///
+    /// See [`solana_short_vec::ShortU16`] for more details.
+    #[inline(always)]
+    #[allow(clippy::arithmetic_side_effects)]
+    fn short_u16_bytes_needed(len: u16) -> usize {
+        1 + (len >= 0x80) as usize + (len >= 0x4000) as usize
+    }
+
+    #[inline(always)]
+    fn try_short_u16_bytes_needed<T: TryInto<u16>>(len: T) -> WriteResult<usize> {
+        match len.try_into() {
+            Ok(len) => Ok(short_u16_bytes_needed(len)),
+            Err(_) => Err(write_length_encoding_overflow("u16::MAX")),
+        }
+    }
+
+    /// Encode a short u16 into the given buffer.
+    ///
+    /// See [`solana_short_vec::ShortU16`] for more details.
+    ///
+    /// # Safety
+    ///
+    /// - `dst` must be a valid for writes.
+    /// - `dst` must be valid for `needed` bytes.
+    #[inline(always)]
+    unsafe fn encode_short_u16(dst: *mut u8, needed: usize, len: u16) {
+        // From `solana_short_vec`:
+        //
+        // u16 serialized with 1 to 3 bytes. If the value is above
+        // 0x7f, the top bit is set and the remaining value is stored in the next
+        // bytes. Each byte follows the same pattern until the 3rd byte. The 3rd
+        // byte may only have the 2 least-significant bits set, otherwise the encoded
+        // value will overflow the u16.
+        match needed {
+            1 => ptr::write(dst, len as u8),
+            2 => {
+                ptr::write(dst, ((len & 0x7f) as u8) | 0x80);
+                ptr::write(dst.add(1), (len >> 7) as u8);
+            }
+            3 => {
+                ptr::write(dst, ((len & 0x7f) as u8) | 0x80);
+                ptr::write(dst.add(1), (((len >> 7) & 0x7f) as u8) | 0x80);
+                ptr::write(dst.add(2), (len >> 14) as u8);
+            }
+            _ => unreachable!(),
+        }
+    }
+
+    impl<'de> SchemaRead<'de> for ShortU16 {
+        type Dst = ShortU16;
+
+        fn read(
+            reader: &mut impl Reader<'de>,
+            dst: &mut std::mem::MaybeUninit<Self::Dst>,
+        ) -> ReadResult<()> {
+            let Ok((len, read)) = decode_shortu16_len(reader.fill_buf(3)?) else {
+                return Err(read_length_encoding_overflow("u16::MAX"));
+            };
+            unsafe { reader.consume_unchecked(read) };
+            Ok(len)
+        }
+    }
+
+    impl SchemaWrite for ShortU16 {
+        type Src = ShortU16;
+
+        fn size_of(src: &Self::Src) -> wincode::WriteResult<usize> {
+            try_short_u16_bytes_needed(len)
+        }
+
+        fn write(
+            writer: &mut impl wincode::io::Writer,
+            src: &Self::Src,
+        ) -> wincode::WriteResult<()> {
+            let src = src.0;
+            let needed = short_u16_bytes_needed(src);
+            let mut buf = [MaybeUninit::<u8>::uninit(); 3];
+            // SAFETY: short_u16 uses a maximum of 3 bytes, so the buffer is always large enough.
+            unsafe { encode_short_u16(buf.as_mut_ptr().cast(), needed, src) };
+            // SAFETY: encode_short_u16 writes exactly `needed` bytes.
+            let buf =
+                unsafe { transmute::<&[MaybeUninit<u8>], &[u8]>(buf.get_unchecked(..needed)) };
+            writer.write(buf)?;
+            Ok(())
+        }
+    }
+
+    #[derive(Debug, SchemaRead, SchemaWrite)]
+    struct Skippable<T: SchemaWrite<Src = T> + for<'a> SchemaRead<'a, Dst = T>> {
+        payload_len: ShortU16,
+        payload: T,
+    }
+
+    impl Skippable<T: SchemaWrite<Src = T> + for<'a> SchemaRead<'a, Dst = T>> {
+        fn try_new(value: T) -> WriteResult<Self> {
+            Ok(Self {
+                payload_len: ShortU16(
+                    wincode::serialized_size(&value)?
+                        .try_into()
+                        .map_err(|e| WriteError::LengthEncodingOverflow("Value too long"))?,
+                ),
+                payload: value,
+            })
+        }
+    }
+
+    #[derive(Debug, wincode::SchemaRead, wincode::SchemaWrite)]
+    enum MyEnum {
+        #[wincode(tag = 1)]
+        A { data: Skippable<u64> },
+        #[wincode(tag = 2)]
+        B { data: Skippable<u8> },
+    }
+
+    #[derive(Debug, wincode::SchemaRead, wincode::SchemaWrite)]
+    enum MyEnum2 {
+        #[wincode(tag = 3)]
+        C { data: Skippable<u8> },
+    }
+    #[derive(Debug)]
+    struct MyEnumFlexible;
+
+    impl<'de> SchemaRead<'de> for MyEnumFlexible {
+        type Dst = Option<MyEnum>;
+
+        fn read(
+            reader: &mut impl Reader<'de>,
+            dst: &mut std::mem::MaybeUninit<Self::Dst>,
+        ) -> wincode::ReadResult<()> {
+            match MyEnum::get(reader) {
+                Ok(x) => {
+                    dst.write(Some(x));
+                    Ok(())
+                }
+                Err(wincode::ReadError::InvalidTagEncoding(_)) => {
+                    let length = ShortU16::get(reader)?;
+                    reader.consume(length.into())?;
+                    dst.write(None);
+                    Ok(())
+                }
+                Err(e) => Err(e),
+            }
+        }
+    }
+    #[derive(Debug)]
+    struct MyEnumFlexible2;
+
+    impl<'de> wincode::SchemaRead<'de> for MyEnumFlexible2 {
+        type Dst = Option<MyEnum2>;
+
+        fn read(
+            reader: &mut impl Reader<'de>,
+            dst: &mut std::mem::MaybeUninit<Self::Dst>,
+        ) -> wincode::ReadResult<()> {
+            match MyEnum2::get(reader) {
+                Ok(x) => {
+                    dst.write(Some(x));
+                    Ok(())
+                }
+                Err(wincode::ReadError::InvalidTagEncoding(_)) => {
+                    let length = ShortU16::get(reader)?;
+                    reader.consume(length.into())?;
+                    dst.write(None);
+                    Ok(())
+                }
+                Err(e) => Err(e),
+            }
+        }
+    }
+    #[test]
+    fn test_zach() {
+        let message1 = MyEnum::A {
+            data: Skippable::try_new(777).unwrap(),
+        };
+        let message2 = MyEnum2::C {
+            data: Skippable::try_new(42).unwrap(),
+        };
+
+        let buf1 = wincode::serialize(&message1).unwrap();
+        let buf2 = wincode::serialize(&message2).unwrap();
+        dbg!(&buf1, &buf2);
+        let res = MyEnumFlexible::get(&mut buf1.as_slice());
+
+        dbg!(res);
+        let res = MyEnumFlexible2::get(&mut buf1.as_slice());
+        dbg!(res);
     }
 }
