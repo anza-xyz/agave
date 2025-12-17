052738b02d2a2f8956e4825039553ca6d2782a60
puhtaytow <18026645+puhtaytow@users.noreply.github.com>
2025-12-17 11:50:52 +0100

    simplify the script
    Co-authored-by: Jon C <me@jonc.dev>

--- ci/test-frozen-abi.sh
+++ ci/test-frozen-abi.sh
@@ -11,9 +11,7 @@ source "$here/rust-version.sh" nightly
 
 packages=$(cargo +"$rust_nightly" metadata --no-deps --format-version=1 | jq -r '.packages[] | select(.features | has("frozen-abi")) | .name')
 for package in $packages; do
-  for test_name in test_api_digest test_abi_digest; do
-    cmd="cargo +$rust_nightly test -p $package --features frozen-abi --lib -- $test_name --nocapture"
-    echo "--- $cmd"
-    $cmd
-  done
+  cmd="cargo +$rust_nightly test -p $package --features frozen-abi --lib -- test_abi_digest test_api_digest --nocapture"
+  echo "--- $cmd"
+  $cmd
 done
