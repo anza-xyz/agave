f49cbbea8a1cf0463bd002b6a64430e3730ab5c8
Carl Lin <carl@solana.com>
2025-12-19 15:17:37 -0500

    Re-enable unstable apis

--- votor/src/lib.rs
+++ votor/src/lib.rs
@@ -1,4 +1,23 @@
+#![cfg_attr(
+    not(feature = "agave-unstable-api"),
+    deprecated(
+        since = "3.1.0",
+        note = "This crate has been marked for formal inclusion in the Agave Unstable API. From \
+                v4.0.0 onward, the `agave-unstable-api` crate feature must be specified to \
+                acknowledge use of an interface that may break without warning."
+    )
+)]
 #![cfg_attr(feature = "frozen-abi", feature(min_specialization))]
+// Activate some of the Rust 2024 lints to make the future migration easier.
+#![warn(if_let_rescope)]
+#![warn(keyword_idents_2024)]
+#![warn(rust_2024_incompatible_pat)]
+#![warn(tail_expr_drop_order)]
+#![warn(unsafe_attr_outside_unsafe)]
+#![warn(unsafe_op_in_unsafe_fn)]
+
+#[macro_use]
+extern crate log;
 
 pub mod commitment;
 pub mod common;
@@ -14,10 +33,8 @@ pub mod vote_history;
 pub mod vote_history_storage;
 pub mod voting_service;
 pub mod voting_utils;
+#[allow(dead_code)]
 pub mod votor;
-
-#[macro_use]
-extern crate log;
 
 #[cfg_attr(feature = "frozen-abi", macro_use)]
 #[cfg(feature = "frozen-abi")]
