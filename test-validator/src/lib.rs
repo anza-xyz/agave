4822297878d160bb5aa7803b4292c12677a34cd2
Wen <113942165+wen-coding@users.noreply.github.com>
2025-12-18 23:38:19 -0800

    Fix local-net and deactivate_features test.

--- test-validator/src/lib.rs
+++ test-validator/src/lib.rs
@@ -1561,6 +1561,7 @@ mod test {
             agave_feature_set::deprecate_rewards_sysvar::id(),
             agave_feature_set::disable_fees_sysvar::id(),
             alpenglow::id(),
+            agave_feature_set::bls_pubkey_management_in_vote_account::id(),
         ]
         .into_iter()
         .for_each(|feature| {
