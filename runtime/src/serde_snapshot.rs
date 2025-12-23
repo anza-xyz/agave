6a26a379ed50c1d659a5965c7a67421e86995524
Kamil Skalski <kamil.skalski@gmail.com>
2025-12-23 06:55:01 +0100

    perf(serde_snapshot): use Vec for serialized slot to storages mapping

--- runtime/src/serde_snapshot.rs
+++ runtime/src/serde_snapshot.rs
@@ -78,7 +78,7 @@ const MAX_STREAM_SIZE: u64 = 32 * 1024 * 1024 * 1024;
 #[cfg_attr(feature = "frozen-abi", derive(AbiExample))]
 #[derive(Clone, Debug, Deserialize, Serialize, PartialEq, Eq)]
 pub(crate) struct AccountsDbFields<T>(
-    HashMap<Slot, SmallVec<[T; 1]>>,
+    Vec<(Slot, SmallVec<[T; 1]>)>,
     u64, // obsolete, formerly write_version
     Slot,
     BankHashInfo,
