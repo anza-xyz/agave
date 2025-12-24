77e7297ab8576e66d1c02915619954c5ba7ecedb
yihau <yihau.chen@icloud.com>
2025-12-24 09:18:18 +0800

    remove useless tests

--- tpu-client-next/src/logging.rs
+++ tpu-client-next/src/logging.rs
@@ -2,18 +2,3 @@
 pub use log::{debug, error, info, trace, warn};
 #[cfg(feature = "tracing")]
 pub use tracing::{debug, error, info, trace, warn};
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_logging_macros_available() {
-        // This test verifies that the logging macros are available
-        // and can be called without errors
-        debug!("Test debug message");
-        error!("Test error message");
-        info!("Test info message");
-        warn!("Test warn message");
-    }
-}
