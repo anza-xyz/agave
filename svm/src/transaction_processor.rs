64e7884533fbd591bba94a02b4e6db0d66180d9e
igor56D <igor.durovic@anza.xyz>
2026-01-22 12:27:18 -0600

    refactor post-exec balance checks to only include necessary state info

--- svm/src/transaction_processor.rs
+++ svm/src/transaction_processor.rs
@@ -10,7 +10,9 @@ use {
         nonce_info::NonceInfo,
         program_loader::{get_program_deployment_slot, load_program_with_pubkey},
         rollback_accounts::RollbackAccounts,
-        transaction_account_state_info::{new_legacy, new_post_exec, new_pre_exec, verify_changes},
+        transaction_account_state_info::{
+            new_post_exec, new_post_exec_legacy, new_pre_exec, new_pre_exec_legacy, verify_changes,
+        },
         transaction_balances::{BalanceCollectionRoutines, BalanceCollector},
         transaction_error_metrics::TransactionErrorMetrics,
         transaction_execution_result::{ExecutedTransaction, TransactionExecutionDetails},
@@ -958,7 +960,7 @@ impl<FG: ForkGraph> TransactionBatchProcessor<FG> {
         let pre_account_state_info = if environment.feature_set.relax_post_exec_min_balance_check {
             new_pre_exec(&transaction_context, tx)
         } else {
-            new_legacy(&transaction_context, tx, &environment.rent)
+            new_pre_exec_legacy(&transaction_context, tx, &environment.rent)
         };
 
         let log_collector = if config.recording_config.enable_log_recording {
@@ -1017,7 +1019,7 @@ impl<FG: ForkGraph> TransactionBatchProcessor<FG> {
                             &environment.rent,
                         )
                     } else {
-                        new_legacy(&transaction_context, tx, &environment.rent)
+                        new_post_exec_legacy(&transaction_context, tx, &environment.rent)
                     };
                 verify_changes(
                     &pre_account_state_info,
