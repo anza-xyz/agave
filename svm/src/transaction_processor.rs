c1695a8ad6b83ed857540cef40cbc744fcca4880
igor56D <igor.durovic@anza.xyz>
2026-01-22 11:29:26 -0600

    feature gate pre-exec balance checks

--- svm/src/transaction_processor.rs
+++ svm/src/transaction_processor.rs
@@ -10,9 +10,7 @@ use {
         nonce_info::NonceInfo,
         program_loader::{get_program_deployment_slot, load_program_with_pubkey},
         rollback_accounts::RollbackAccounts,
-        transaction_account_state_info::{
-            new_post_exec, new_post_exec_legacy, new_pre_exec, verify_changes,
-        },
+        transaction_account_state_info::{new_legacy, new_post_exec, new_pre_exec, verify_changes},
         transaction_balances::{BalanceCollectionRoutines, BalanceCollector},
         transaction_error_metrics::TransactionErrorMetrics,
         transaction_execution_result::{ExecutedTransaction, TransactionExecutionDetails},
@@ -957,7 +955,11 @@ impl<FG: ForkGraph> TransactionBatchProcessor<FG> {
             tx.num_instructions(),
         );
 
-        let pre_account_state_info = new_pre_exec(&transaction_context, tx);
+        let pre_account_state_info = if environment.feature_set.relax_post_exec_min_balance_check {
+            new_pre_exec(&transaction_context, tx)
+        } else {
+            new_legacy(&transaction_context, tx, &environment.rent)
+        };
 
         let log_collector = if config.recording_config.enable_log_recording {
             match config.log_messages_bytes_limit {
@@ -1015,7 +1017,7 @@ impl<FG: ForkGraph> TransactionBatchProcessor<FG> {
                             &environment.rent,
                         )
                     } else {
-                        new_post_exec_legacy(&transaction_context, tx, &environment.rent)
+                        new_legacy(&transaction_context, tx, &environment.rent)
                     };
                 verify_changes(
                     &pre_account_state_info,
