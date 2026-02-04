fcf70ab2b25ffa2e932e8132aa077f7b7ad187fa
Yihau Chen <yihau.chen@icloud.com>
2026-02-04 22:07:25 +0800

    ci: move all agents to use the default queue (#10330)
    (cherry picked from commit b226b00794c73d14c24d0e5e5620fe1d7c6e288c)
    
    # Conflicts:
    #	ci/buildkite-pipeline.sh

--- ci/buildkite-pipeline.sh
+++ ci/buildkite-pipeline.sh
@@ -154,7 +154,7 @@ command_step() {
     timeout_in_minutes: $3
     artifact_paths: "log-*.txt"
     agents:
-      queue: "${4:-solana}"
+      queue: "${4:-default}"
 EOF
 }
 
@@ -179,6 +179,7 @@ wait_step() {
   echo "  - wait" >> "$output_file"
 }
 
+<<<<<<< HEAD
 all_test_steps() {
   command_step checks1 "ci/docker-run-default-image.sh ci/test-checks.sh" 20 check
   command_step dcou-1-of-3 "ci/docker-run-default-image.sh ci/test-dev-context-only-utils.sh --partition 1/3" 20 check
@@ -186,6 +187,37 @@ all_test_steps() {
   command_step dcou-3-of-3 "ci/docker-run-default-image.sh ci/test-dev-context-only-utils.sh --partition 3/3" 20 check
   command_step miri "ci/docker-run-default-image.sh ci/test-miri.sh" 5 check
   command_step frozen-abi "ci/docker-run-default-image.sh ci/test-abi.sh" 15 check
+=======
+generate_feature_steps() {
+	cat >> "$output_file" <<EOF
+  - group: "feature-checks"
+    steps:
+EOF
+  total_feature_checks=5
+  for i in $(seq 1 $total_feature_checks); do
+    cat >> "$output_file" <<EOF
+      - name: "feature-check-part-$i"
+        command: "ci/docker-run-default-image.sh ci/feature-check/test-feature.sh $i/$total_feature_checks"
+        timeout_in_minutes: 20
+        agents:
+          queue: "default"
+EOF
+	done
+	cat >> "$output_file" <<EOF
+      - name: "feature-check-dev-bins"
+        command: "ci/docker-run-default-image.sh ci/feature-check/test-feature-dev-bins.sh"
+        timeout_in_minutes: 20
+        agents:
+          queue: "default"
+EOF
+}
+
+all_test_steps() {
+  command_step checks1 "ci/docker-run-default-image.sh ci/test-checks.sh" 20 default
+  generate_feature_steps
+  command_step miri "ci/docker-run-default-image.sh ci/test-miri.sh" 5 default
+  command_step frozen-abi "ci/docker-run-default-image.sh ci/test-frozen-abi.sh" 30 default
+>>>>>>> b226b0079 (ci: move all agents to use the default queue (#10330))
   wait_step
 
   # Full test suite
@@ -227,7 +259,7 @@ all_test_steps() {
     name: "stable-sbf"
     timeout_in_minutes: 35
     agents:
-      queue: "solana"
+      queue: "default"
 EOF
   else
     annotate --style info \
@@ -256,7 +288,29 @@ EOF
              ^ci/test-coverage.sh \
              ^scripts/coverage.sh \
       ; then
+<<<<<<< HEAD
     command_step coverage "ci/docker-run-default-image.sh ci/test-coverage.sh" 80
+=======
+    cat >> "$output_file" <<"EOF"
+  - group: "coverage"
+    steps:
+      - command: "ci/docker-run-default-image.sh ci/coverage/part-1.sh"
+        name: "coverage-1"
+        timeout_in_minutes: 60
+        agents:
+          queue: "default"
+      - command: "ci/docker-run-default-image.sh ci/coverage/part-2.sh"
+        name: "coverage-2"
+        timeout_in_minutes: 60
+        agents:
+          queue: "default"
+      - command: "ci/docker-run-default-image.sh ci/coverage/part-3.sh"
+        name: "coverage-3"
+        timeout_in_minutes: 60
+        agents:
+          queue: "default"
+EOF
+>>>>>>> b226b0079 (ci: move all agents to use the default queue (#10330))
   else
     annotate --style info --context test-coverage \
       "Coverage skipped as no .rs files were modified"
@@ -264,7 +318,7 @@ EOF
 }
 
 pull_or_push_steps() {
-  command_step sanity "ci/test-sanity.sh" 5 check
+  command_step sanity "ci/test-sanity.sh" 5 default
   wait_step
 
   # Check for any .sh file changes
@@ -272,7 +326,7 @@ pull_or_push_steps() {
               .sh$ \
               ^.buildkite/hooks \
       ; then
-    command_step shellcheck "ci/shellcheck.sh" 5 check
+    command_step shellcheck "ci/shellcheck.sh" 5 default
     wait_step
   fi
 
