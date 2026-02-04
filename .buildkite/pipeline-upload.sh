12f500e8b31a8dbfeb0b4b90ff93a6d62c6ae741
Yihau Chen <yihau.chen@icloud.com>
2026-02-04 22:07:25 +0800

    ci: move all agents to use the default queue (#10330)
    (cherry picked from commit b226b00794c73d14c24d0e5e5620fe1d7c6e288c)
    
    # Conflicts:
    #	.buildkite/pipeline-upload.sh
    #	ci/buildkite-pipeline.sh

--- .buildkite/pipeline-upload.sh
+++ .buildkite/pipeline-upload.sh
@@ -11,8 +11,27 @@ set -e
 cd "$(dirname "$0")"/..
 source ci/_
 
+<<<<<<< HEAD
 _ ci/buildkite-pipeline.sh pipeline.yml
 echo +++ pipeline
 cat pipeline.yml
+=======
+if [[ $BUILDKITE_BRANCH == gh-readonly-queue* ]]; then
+  # github merge queue
+  cat <<EOF | tee /dev/tty | buildkite-agent pipeline upload
+priority: 10
+steps:
+  - name: "sanity"
+    command: "ci/docker-run-default-image.sh ci/test-sanity.sh"
+    timeout_in_minutes: 5
+    agents:
+      queue: "default"
+  - name: "checks"
+    command: "ci/docker-run-default-image.sh ci/test-checks.sh"
+    timeout_in_minutes: 30
+    agents:
+      queue: "default"
+EOF
+>>>>>>> b226b0079 (ci: move all agents to use the default queue (#10330))
 
 _ buildkite-agent pipeline upload pipeline.yml
