5dee851795516e0f7486bce7add10e9333de291d
Justin Starry <justin@anza.xyz>
2026-02-04 17:10:06 +0000

    Revert "Compute snapshot slot leader from epoch stakes (#10266)"
    This reverts commit c990cfff67564591042d2f06c6c0540918ced1b3.

--- runtime/src/bank/builtins/core_bpf_migration/mod.rs
+++ runtime/src/bank/builtins/core_bpf_migration/mod.rs
@@ -504,7 +504,6 @@ pub(crate) mod tests {
                 tests::{create_genesis_config, create_simple_test_bank},
                 Bank,
             },
-            genesis_utils::{create_genesis_config_with_leader, GenesisConfigInfo},
             runtime_config::RuntimeConfig,
             snapshot_bank_utils::{bank_from_snapshot_archives, bank_to_full_snapshot_archive},
             snapshot_utils::create_tmp_accounts_dir_for_tests,
@@ -2060,9 +2059,7 @@ pub(crate) mod tests {
     // activated and the migration was successful.
     #[test]
     fn test_startup_from_snapshot_after_replace_spl_token_with_p_token() {
-        let leader_id = Pubkey::new_unique();
-        let GenesisConfigInfo { genesis_config, .. } =
-            create_genesis_config_with_leader(0, &leader_id, LAMPORTS_PER_SOL);
+        let (genesis_config, _mint_keypair) = create_genesis_config(0);
         let mut bank = Bank::new_for_tests(&genesis_config);
 
         let bpf_loader_v2_program_address = Pubkey::new_unique();
@@ -2145,7 +2142,6 @@ pub(crate) mod tests {
             &genesis_config,
             &RuntimeConfig::default(),
             None,
-            None, // leader_for_tests
             None,
             false,
             false,
