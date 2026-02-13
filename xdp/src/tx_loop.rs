c4fbf85d26923fd5140a7f432093c6a07c742c69
greg <greg@anza.xyz>
2026-02-13 10:16:54 +0000

    log errors, return err as before

--- xdp/src/tx_loop.rs
+++ xdp/src/tx_loop.rs
@@ -181,10 +181,9 @@ impl TxLoopBuilder<OwnedUmem<PageAlignedMemory>> {
             umem,
         } = self;
 
-        let (socket, tx) =
-            Socket::tx(queue, umem, zero_copy, tx_size * 2, tx_size).unwrap_or_else(|err| {
-                panic!("failed to create AF_XDP socket on queue {queue_id:?}: {err}")
-            });
+        let Ok((socket, tx)) = Socket::tx(queue, umem, zero_copy, tx_size * 2, tx_size) else {
+            panic!("failed to create AF_XDP socket on queue {queue_id:?}");
+        };
 
         let Tx {
             // this is where we'll queue frames
