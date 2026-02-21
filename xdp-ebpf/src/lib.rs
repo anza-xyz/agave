aba297bc67ca5946b58fe5a105fe64cb656c211e
Kirill Lykov <lykov.kirill@gmail.com>
2026-02-21 15:59:09 +0000

    enforce AGAVE_XDP_EBPF_PROGRAM 32b alignment

--- xdp-ebpf/src/lib.rs
+++ xdp-ebpf/src/lib.rs
@@ -10,14 +10,5 @@
 
 #[cfg(all(target_os = "linux", not(target_arch = "bpf")))]
 #[unsafe(no_mangle)]
-pub static AGAVE_XDP_EBPF_PROGRAM: [u8; aya::include_bytes_aligned!(concat!(
-    env!("CARGO_MANIFEST_DIR"),
-    "/agave-xdp-prog"
-))
-.len()] = unsafe {
-    core::ptr::read(
-        aya::include_bytes_aligned!(concat!(env!("CARGO_MANIFEST_DIR"), "/agave-xdp-prog"))
-            .as_ptr()
-            .cast(),
-    )
-};
+pub static AGAVE_XDP_EBPF_PROGRAM: &[u8] =
+    aya::include_bytes_aligned!(concat!(env!("CARGO_MANIFEST_DIR"), "/agave-xdp-prog"));
