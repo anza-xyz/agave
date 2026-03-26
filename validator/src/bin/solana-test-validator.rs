b6c25fd6e0fad17e7519c3f56812f98f00b63b24
OliverNChalk <11343499+OliverNChalk@users.noreply.github.com>
2026-03-26 15:20:39 -0500

    build: align test validator shutdown sequence

--- validator/src/bin/solana-test-validator.rs
+++ validator/src/bin/solana-test-validator.rs
@@ -132,6 +132,9 @@ fn main() {
         None
     };
     agave_logger::initialize_logging(logfile);
+    // NB: This hook is used to shutdown the validator, so don't remove it even if you
+    // don't want metrics.
+    solana_metrics::set_panic_hook("solana-test-validator", None);
 
     info!("{} {}", crate_name!(), solana_version::version!());
     info!("Starting validator with: {:#?}", std::env::args_os());
