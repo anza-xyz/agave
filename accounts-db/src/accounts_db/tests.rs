35bb4abfc84feb279285aa51b61d62d8496ef66a
Rory Harris <rory.harris@anza.xyz>
2026-03-31 12:09:10 -0700

    Updating tests to use db scan rather than index scan

--- accounts-db/src/accounts_db/tests.rs
+++ accounts-db/src/accounts_db/tests.rs
@@ -1357,15 +1357,17 @@ fn test_clean_old_with_both_normal_and_zero_lamport_accounts() {
     let mut found_accounts = HashSet::new();
     let index_key = IndexKey::SplTokenMint(mint_key);
     let bank_id = 0;
-    accounts.accounts_index.index_scan_accounts(
-        &Ancestors::default(),
-        accounts.accounts_index.max_root_inclusive(),
-        index_key,
-        |key, _| {
-            found_accounts.insert(*key);
-        },
-        &ScanConfig::default(),
-    );
+    accounts
+        .index_scan_accounts(
+            &Ancestors::default(),
+            bank_id,
+            index_key,
+            |account| {
+                found_accounts.insert(*account.unwrap().0);
+            },
+            &ScanConfig::default(),
+        )
+        .unwrap();
     assert_eq!(found_accounts.len(), 2);
     assert!(found_accounts.contains(&pubkey1));
     assert!(found_accounts.contains(&pubkey2));
@@ -1432,13 +1434,17 @@ fn test_clean_old_with_both_normal_and_zero_lamport_accounts() {
 
     // Secondary index should have purged `pubkey1` as well
     let mut found_accounts = vec![];
-    accounts.accounts_index.index_scan_accounts(
-        &Ancestors::default(),
-        accounts.accounts_index.max_root_inclusive(),
-        IndexKey::SplTokenMint(mint_key),
-        |key, _| found_accounts.push(*key),
-        &ScanConfig::default(),
-    );
+    accounts
+        .index_scan_accounts(
+            &Ancestors::default(),
+            bank_id,
+            IndexKey::SplTokenMint(mint_key),
+            |account| {
+                found_accounts.push(*account.unwrap().0);
+            },
+            &ScanConfig::default(),
+        )
+        .unwrap();
     assert_eq!(found_accounts, vec![pubkey2]);
 }
 
