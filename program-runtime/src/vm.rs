7cc51abe9c4c1a3a3c9048788abe3cd52d4caa79
Boris Astardzhiev <boris.astardzhiev@gmail.com>
2026-04-02 14:21:56 +0300

    Nagisa comment: Remove unnecessary allow(unused_assignments) lint suppression

--- program-runtime/src/vm.rs
+++ program-runtime/src/vm.rs
@@ -212,7 +212,6 @@ pub fn execute<'a, 'b: 'a>(
 
     let mut create_vm_time = Measure::start("create_vm");
     let execution_result = {
-        #[cfg_attr(feature = "sbpf-debugger", allow(unused_assignments))]
         let mut execution_mode = ExecutionMode::PreferJit;
         #[cfg(feature = "sbpf-debugger")]
         let (debug_port, debug_metadata) = if invoke_context.debug_port.is_some() {
