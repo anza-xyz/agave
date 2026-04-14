cd93c3f119a2d7579d027fdd6d3bd842c44051b5
david <david@vacovideo.com>
2026-04-14 09:55:52 -0700

    Reduce default inflation schedule by 10x

--- genesis/src/main.rs
+++ genesis/src/main.rs
@@ -44,7 +44,7 @@ use {
     solana_runtime::{
         bank::VAT_TO_BURN_PER_EPOCH,
         genesis_utils::{add_genesis_epoch_rewards_account, add_genesis_stake_config_account},
-        stake_utils,
+        inflation_schedule, stake_utils,
     },
     solana_sdk_ids::system_program,
     solana_signer::Signer,
@@ -801,6 +801,7 @@ fn main() -> Result<(), Box<dyn error::Error>> {
         ticks_per_slot,
         poh_config,
         fee_rate_governor,
+        inflation: inflation_schedule::default_inflation(),
         rent,
         epoch_schedule,
         cluster_type,
@@ -810,7 +811,7 @@ fn main() -> Result<(), Box<dyn error::Error>> {
     if let Ok(raw_inflation) = value_t!(matches, "inflation", String) {
         let inflation = match raw_inflation.as_str() {
             "pico" => Inflation::pico(),
-            "full" => Inflation::full(),
+            "full" => inflation_schedule::full_inflation(),
             "none" => Inflation::new_disabled(),
             _ => unreachable!(),
         };
