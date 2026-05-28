73f42103517edd87aac3b5e974a77525026d7444
Joe Caulfield <jcaulfield135@gmail.com>
2026-05-28 07:51:29 +0000

    svm: conformance: repackage instr harness

--- svm/src/conformance/mod.rs
+++ svm/src/conformance/mod.rs
@@ -2,8 +2,7 @@
 
 #[cfg(feature = "conformance")]
 pub mod account_state;
-pub mod context;
 #[cfg(feature = "conformance")]
 pub mod feature_set;
-pub mod harness;
+pub mod instr;
 pub mod programs;
