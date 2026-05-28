4ff93cdae4a3d0986d775ea7ff81cfe297f204aa
Joe Caulfield <jcaulfield135@gmail.com>
2026-05-28 08:56:53 +0000

    svm: conformance: extract fd_hash into shared module

--- svm/src/conformance/mod.rs
+++ svm/src/conformance/mod.rs
@@ -6,6 +6,8 @@ pub mod callback;
 #[cfg(feature = "conformance")]
 pub mod elf_loader;
 #[cfg(feature = "conformance")]
+pub mod fd_hash;
+#[cfg(feature = "conformance")]
 pub mod feature_set;
 pub mod instr;
 pub mod programs;
