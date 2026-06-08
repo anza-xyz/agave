64dcb067d68ef67a494dc5bc161a1ece1b56f50e
Tao Zhu <tao@anza.xyz>
2026-06-08 16:42:46 +0000

    restore set-identity and progress.increment_num_bank_switches(slot)

--- core/src/replay_stage.rs
+++ core/src/replay_stage.rs
@@ -1035,6 +1035,15 @@ impl ReplayStage {
                 }
 
                 if migration_status.is_alpenglow_enabled() {
+                    if my_pubkey != cluster_info.id() {
+                        identity_keypair = cluster_info.keypair();
+                        let my_old_pubkey = my_pubkey;
+                        my_pubkey = identity_keypair.pubkey();
+                        migration_status.set_pubkey(my_pubkey);
+
+                        warn!("Identity changed from {my_old_pubkey} to {my_pubkey}");
+                    }
+
                     process_soft_dead_slots(
                         &my_pubkey,
                         &blockstore,
@@ -2502,6 +2511,7 @@ impl ReplayStage {
 
             // Switch the blockstore data atomically, handles slot meta chaining so generate new bank forks can proceed
             blockstore.switch_block_from_alternate(slot, location)?;
+            progress.increment_num_bank_switches(slot);
             info!("{my_pubkey}: Switched {slot} from {location:?}");
         }
 
