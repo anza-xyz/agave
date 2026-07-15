95aa0b8cff7dd84a51050ed25b68442fd7e7c344
Alexander Meißner <AlexanderMeissner@gmx.net>
2026-07-15 13:42:14 +0000

    Adds TransactionAccounts::is_in_replay flag.

--- runtime/src/bank/builtins/core_bpf_migration/mod.rs
+++ runtime/src/bank/builtins/core_bpf_migration/mod.rs
@@ -165,6 +165,7 @@ impl Bank {
                 compute_budget.max_instruction_stack_depth,
                 compute_budget.max_instruction_trace_length,
                 1,
+                true,
             );
 
             struct MockCallback {}
