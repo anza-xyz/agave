5ffd5657839f1e2bcb55ddd22b06d4519652f1be
Joe Caulfield <jcaulfield135@gmail.com>
2026-07-25 21:56:35 +0000

    svm-conformance: init crate

---
+++ svm-conformance/src/lib.rs
@@ -0,0 +1,4 @@
+//! Conformance harnesses for the Solana SVM.
+//!
+//! Houses the `sol_compat_*` FFI entrypoints that Firedancer's conformance
+//! tooling links against to exercise Agave's execution layer.
