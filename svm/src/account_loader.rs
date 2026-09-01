caee28cf2b8121e6cc7051f16881272c0fff1292
Alexander Meißner <AlexanderMeissner@gmx.net>
2026-09-01 13:41:38 +0000

    Removes last_modification_slot from AccountLoader::loaded_accounts.

--- svm/src/account_loader.rs
+++ svm/src/account_loader.rs
@@ -185,7 +185,7 @@ pub struct NoOpTransaction {
 // account states mid-batch.
 #[cfg_attr(feature = "dev-context-only-utils", qualifiers(pub))]
 pub(crate) struct AccountLoader<'a, CB: TransactionProcessingCallback> {
-    loaded_accounts: AHashMap<Pubkey, (AccountSharedData, Slot)>,
+    loaded_accounts: AHashMap<Pubkey, AccountSharedData>,
     callbacks: &'a CB,
     pub(crate) feature_set: &'a SVMFeatureSet,
 }
@@ -206,7 +206,7 @@ impl<'a, CB: TransactionProcessingCallback> AccountLoader<'a, CB> {
         if let Some(slot_history) =
             account_overrides.and_then(|overrides| overrides.get(&slot_history::id()))
         {
-            loaded_accounts.insert(slot_history::id(), (slot_history.clone(), 0));
+            loaded_accounts.insert(slot_history::id(), slot_history.clone());
         }
 
         Self {
@@ -256,19 +256,18 @@ impl<'a, CB: TransactionProcessingCallback> AccountLoader<'a, CB> {
     pub(crate) fn load_account(&mut self, account_key: &Pubkey) -> Option<AccountSharedData> {
         match self.do_load(account_key) {
             // Exists, from AccountLoader.
-            (Some((account, _last_modification_slot)), false) => Some(account),
+            (Some(account), false) => Some(account),
             // Not allocated, but has an AccountLoader placeholder already.
             (None, false) => None,
             // Exists in accounts-db. Store it in AccountLoader for future loads.
-            (Some((account, last_modification_slot)), true) => {
-                self.loaded_accounts
-                    .insert(*account_key, (account.clone(), last_modification_slot));
+            (Some(account), true) => {
+                self.loaded_accounts.insert(*account_key, account.clone());
                 Some(account)
             }
             // Does not exist and has never been seen.
             (None, true) => {
                 self.loaded_accounts
-                    .insert(*account_key, (AccountSharedData::default(), 0));
+                    .insert(*account_key, AccountSharedData::default());
                 None
             }
         }
@@ -277,20 +276,20 @@ impl<'a, CB: TransactionProcessingCallback> AccountLoader<'a, CB> {
     // Internal helper for core loading logic to prevent code duplication. Returns a bool
     // indicating whether an accounts-db lookup was performed, which allows wrappers with
     // &mut self to insert the account. Wrappers with &self ignore it.
-    fn do_load(&self, account_key: &Pubkey) -> (Option<(AccountSharedData, Slot)>, bool) {
-        if let Some((account, slot)) = self.loaded_accounts.get(account_key) {
+    fn do_load(&self, account_key: &Pubkey) -> (Option<AccountSharedData>, bool) {
+        if let Some(account) = self.loaded_accounts.get(account_key) {
             // If lamports is 0, a previous transaction deallocated this account.
             // We return None instead of the account we found so it can be created fresh.
             // We *never* remove accounts, or else we would fetch stale state from accounts-db.
             let option_account = if account.lamports() == 0 {
                 None
             } else {
-                Some((account.clone(), *slot))
+                Some(account.clone())
             };
 
             (option_account, false)
-        } else if let Some((account, slot)) = self.callbacks.get_account_shared_data(account_key) {
-            (Some((account, slot)), true)
+        } else if let Some((account, _slot)) = self.callbacks.get_account_shared_data(account_key) {
+            (Some(account), true)
         } else {
             (None, true)
         }
@@ -299,11 +298,11 @@ impl<'a, CB: TransactionProcessingCallback> AccountLoader<'a, CB> {
     pub(crate) fn update_accounts_for_failed_tx(
         &mut self,
         rollback_accounts: &RollbackAccounts,
-        current_slot: Slot,
+        _current_slot: Slot,
     ) {
         for (account_address, account) in rollback_accounts {
             self.loaded_accounts
-                .insert(*account_address, (account.clone(), current_slot));
+                .insert(*account_address, account.clone());
         }
     }
 
@@ -312,7 +311,7 @@ impl<'a, CB: TransactionProcessingCallback> AccountLoader<'a, CB> {
         message: &impl SVMMessage,
         transaction_accounts: &[KeyedAccountSharedData],
         touched_flags: &[bool],
-        current_slot: Slot,
+        _current_slot: Slot,
     ) {
         for (i, (address, account)) in (0..message.account_keys().len()).zip(transaction_accounts) {
             if !message.is_writable(i) {
@@ -332,8 +331,7 @@ impl<'a, CB: TransactionProcessingCallback> AccountLoader<'a, CB> {
                 continue;
             }
 
-            self.loaded_accounts
-                .insert(*address, (account.clone(), current_slot));
+            self.loaded_accounts.insert(*address, account.clone());
         }
     }
 }
@@ -344,7 +342,7 @@ impl<'a, CB: TransactionProcessingCallback> AccountLoader<'a, CB> {
 // In practice, all accounts we load this way will already be in our accounts store.
 impl<CB: TransactionProcessingCallback> TransactionProcessingCallback for AccountLoader<'_, CB> {
     fn get_account_shared_data(&self, pubkey: &Pubkey) -> Option<(AccountSharedData, Slot)> {
-        self.do_load(pubkey).0
+        self.do_load(pubkey).0.map(|account| (account, 0))
     }
 }
 
@@ -2796,10 +2794,9 @@ mod tests {
         account_loader.load_account(&hit_address);
         let actual_hit_account = account_loader.loaded_accounts.get(&hit_address);
 
-        assert_eq!(actual_hit_account.as_ref().unwrap().0, expected_hit_account);
-        assert_eq!(actual_hit_account.as_ref().unwrap().1, 1);
+        assert_eq!(actual_hit_account.unwrap(), &expected_hit_account);
         assert!(Arc::ptr_eq(
-            &actual_hit_account.unwrap().0.data_clone(),
+            &actual_hit_account.unwrap().data_clone(),
             &expected_hit_account.data_clone()
         ));
 
@@ -2807,10 +2804,9 @@ mod tests {
         account_loader.load_account(&hit_address);
         let actual_hit_account = account_loader.loaded_accounts.get(&hit_address);
 
-        assert_eq!(actual_hit_account.as_ref().unwrap().0, expected_hit_account);
-        assert_eq!(actual_hit_account.as_ref().unwrap().1, 1);
+        assert_eq!(actual_hit_account.unwrap(), &expected_hit_account);
         assert!(Arc::ptr_eq(
-            &actual_hit_account.unwrap().0.data_clone(),
+            &actual_hit_account.unwrap().data_clone(),
             &expected_hit_account.data_clone()
         ));
 
@@ -2823,7 +2819,7 @@ mod tests {
             .clone();
 
         assert!(!Arc::ptr_eq(
-            &expected_miss_account.0.data_clone(),
+            &expected_miss_account.data_clone(),
             &expected_hit_account.data_clone()
         ));
 
@@ -2833,8 +2829,8 @@ mod tests {
 
         assert_eq!(actual_miss_account, Some(&expected_miss_account));
         assert!(Arc::ptr_eq(
-            &actual_miss_account.unwrap().0.data_clone(),
-            &expected_miss_account.0.data_clone()
+            &actual_miss_account.unwrap().data_clone(),
+            &expected_miss_account.data_clone()
         ));
     }
 }
