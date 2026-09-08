06f1cd64cafe79fb25da30dd3434a9d02e2c3e6e
Alexander Lindenbaum <lindenbaum.alexander@gmail.com>
2026-09-08 13:08:53 -0400

    improve error handling in bcl path

--- core/src/block_creation_loop.rs
+++ core/src/block_creation_loop.rs
@@ -1034,7 +1034,7 @@ fn handle_parent_ready(
                 TransactionHistoryPurgeSource::LeaderWindow,
                 TransactionHistoryPurgeInput::Bcl(Arc::clone(&accumulated_txs)),
             )
-            .expect("failed to enqueue UpdateParent transaction-history purge");
+            .map_err(|err| PohRecorderError::PurgeTransactionHistory(slot, err))?;
     }
 
     if let Some(sender) = &ctx.entry_notification_sender
