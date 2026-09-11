5ee05b8c4a9bf3b9ef696b2e4fa5e38962ede89a
Akhi Singhania <akhi3030@gmail.com>
2026-09-11 15:34:16 +0000

    bls-sigverify: report stats once a second

--- bls-sigverify/src/bls_sigverifier.rs
+++ bls-sigverify/src/bls_sigverifier.rs
@@ -166,14 +166,13 @@ impl SigVerifier {
             .thread_name(|i| format!("solSigVerBLS{i:02}"))
             .build()
             .unwrap();
-        let root_slot = sharable_banks.root().slot();
         Self {
             migration_status,
             ban_sender,
             channels,
             sharable_banks,
             highest_parent_ready,
-            stats: SigVerifierStats::new(root_slot),
+            stats: SigVerifierStats::default(),
             verified_certs: HashSet::new(),
             vote_pool: VotePool::default(),
             last_checked_root_slot: 0,
@@ -221,9 +220,6 @@ impl SigVerifier {
             }
             self.stats.maybe_report(self.sharable_banks.root().slot());
         }
-        let elapsed = self.stats.elapsed_since_last_report();
-        self.stats
-            .do_report(self.sharable_banks.root().slot(), elapsed);
     }
 
     #[cfg(test)]
@@ -937,7 +933,7 @@ mod tests {
             ConsensusMessage::Vote(vote_message2),
             ctx.validator_keypairs[vote_rank2].node_keypair.pubkey(),
         )];
-        ctx.verifier.stats = SigVerifierStats::new(ctx.verifier.sharable_banks.root().slot());
+        ctx.verifier.stats = SigVerifierStats::default();
         ctx.verifier
             .verify_and_send_datagrams(messages_to_datagrams(
                 &messages2,
@@ -967,7 +963,7 @@ mod tests {
             ConsensusMessage::Vote(vote_message3),
             ctx.validator_keypairs[vote_rank3].node_keypair.pubkey(),
         )];
-        ctx.verifier.stats = SigVerifierStats::new(ctx.verifier.sharable_banks.root().slot());
+        ctx.verifier.stats = SigVerifierStats::default();
         ctx.verifier
             .verify_and_send_datagrams(messages_to_datagrams(
                 &messages3,
@@ -1782,7 +1778,7 @@ mod tests {
             ctx.verifier.cluster_info.my_shred_version(),
         );
 
-        ctx.verifier.stats = SigVerifierStats::new(ctx.verifier.sharable_banks.root().slot());
+        ctx.verifier.stats = SigVerifierStats::default();
         ctx.verifier.verify_and_send_datagrams(datagrams2).unwrap();
         expect_no_receive(&ctx.pool_receiver);
         assert_eq!(ctx.verifier.stats.num_verified_certs_received.0, 1);
