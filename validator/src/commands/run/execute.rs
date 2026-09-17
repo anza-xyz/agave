e838ac172c1d14e7a78e2a0a806e9fbbef57e19e
Edvard Fagerholm <edvard.fagerholm@gmail.com>
2026-09-17 10:41:26 +0000

    validator: simplify config file deserialization

--- validator/src/commands/run/execute.rs
+++ validator/src/commands/run/execute.rs
@@ -122,7 +122,7 @@ pub fn execute(
     // Print the built-in policy before RunArgs parsing, which loads identity
     // material and canonicalizes the ledger directory.
     if matches.is_present("print_default_config") {
-        print!("{}", config_file::default_config_toml());
+        print!("{}", config_file::DEFAULT_CONFIG);
         return Ok(());
     }
     // Debugging panics is easier with a backtrace
