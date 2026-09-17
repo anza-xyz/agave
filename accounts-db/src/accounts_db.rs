5e6abb0cb2e4f7d7aab7e236f99a605d4432e7e2
Rory Harris <rory.harris@anza.xyz>
2026-09-16 17:49:43 -0700

    Pre-size the reclaims bin

--- accounts-db/src/accounts_db.rs
+++ accounts-db/src/accounts_db.rs
@@ -1047,14 +1047,12 @@ impl AccountsDb {
         &self,
         pubkey: &Pubkey,
         max_clean_root_inclusive: Slot,
-    ) -> ReclaimsWithNewestSlot<AccountInfo> {
+        reclaims: &mut ReclaimsWithNewestSlot<AccountInfo>,
+    ) {
         let mut clean_rooted = Measure::start("clean_old_root-ms");
-        let mut reclaims = ReclaimsWithNewestSlot::new();
-        let removed_from_index = self.accounts_index.clean_rooted_entries(
-            pubkey,
-            &mut reclaims,
-            max_clean_root_inclusive,
-        );
+        let removed_from_index =
+            self.accounts_index
+                .clean_rooted_entries(pubkey, reclaims, max_clean_root_inclusive);
         clean_rooted.stop();
         if removed_from_index {
             self.clean_accounts_stats
@@ -1065,7 +1063,6 @@ impl AccountsDb {
         self.clean_accounts_stats
             .clean_old_root_us
             .fetch_add(clean_rooted.as_us(), Ordering::Relaxed);
-        reclaims
     }
 
     /// Reclaim older states of accounts older than max_clean_root_inclusive for AccountsDb bloat mitigation.
@@ -1521,10 +1518,15 @@ impl AccountsDb {
                 let mut missing = 0;
                 let mut useful = 0;
                 let mut purges_old_accounts_local = 0;
-                let mut reclaims_local = ReclaimsWithNewestSlot::new();
                 // Take the bin so its allocation is freed by this thread once the bin is
                 // scanned, rather than serially after every bin completes.
                 let candidates_bin = mem::take(&mut *candidates_bin.write().unwrap());
+
+                // Sized for one reclaim per candidate. During startup this should be fairly
+                // accurate, but during normal operation, resizes will likely be needed
+                let mut reclaims_local =
+                    ReclaimsWithNewestSlot::with_capacity(candidates_bin.len());
+
                 for candidate_pubkey in candidates_bin {
                     let mut should_collect_reclaims = false;
                     self.accounts_index.scan(
@@ -1585,8 +1587,10 @@ impl AccountsDb {
                         ScanFilter::All,
                     );
                     if should_collect_reclaims {
-                        reclaims_local.extend(
-                            self.collect_reclaims(&candidate_pubkey, max_clean_root_inclusive),
+                        self.collect_reclaims(
+                            &candidate_pubkey,
+                            max_clean_root_inclusive,
+                            &mut reclaims_local,
                         );
                     }
                 }
