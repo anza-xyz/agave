848c288eca39cee55048bee6163df5ecc901818c
Akhi Singhania <akhi3030@gmail.com>
2026-09-19 16:37:12 +0000

    use cfg attr

--- bls-sigverify/src/lib.rs
+++ bls-sigverify/src/lib.rs
@@ -1,5 +1,7 @@
 #![cfg(feature = "agave-unstable-api")]
 
+use qualifier_attr::qualifiers;
+
 pub mod bls_cert_sigverify;
 pub mod bls_sigverifier;
 pub mod bls_vote_sigverify;
@@ -7,13 +9,9 @@ mod errors;
 pub mod generated_cert_types;
 pub mod rewards;
 pub mod stats;
-#[cfg(not(feature = "dev-context-only-utils"))]
+#[cfg_attr(feature = "dev-context-only-utils", qualifiers(pub))]
 mod unverified_votes_batch;
-#[cfg(feature = "dev-context-only-utils")]
-pub mod unverified_votes_batch;
 mod utils;
-#[cfg(not(feature = "dev-context-only-utils"))]
+#[cfg_attr(feature = "dev-context-only-utils", qualifiers(pub))]
 mod verified_batch;
-#[cfg(feature = "dev-context-only-utils")]
-pub mod verified_batch;
 mod vote_pool;
